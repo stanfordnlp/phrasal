b64a573481b36e8c6c33a2a0b39948acbc69348f
danielcer <danielcer@stanford.edu>
2008-06-03 01:07:28 +0000

    minor bug fix. -d
    svn path=/trunk/javanlp/; revision=20438

--- src/mt/tune/UnsmoothedMERT.java
+++ src/mt/tune/UnsmoothedMERT.java
@@ -92,9 +92,9 @@ public class UnsmoothedMERT {
 			dE.multiplyBy(sumExpL/cnt);
 			dE.subtractAll(sumExpLF);
 			l2normalize(dE);
-			dEDiff = wtSsd(dE, oldDe);
+			dEDiff = wtSsd(oldDe, dE);
 			
-			System.err.printf("Batch: %e dEDiff: %e\n", dEDiff, dEDiff);
+			System.err.printf("Batch: %d dEDiff: %e\n", batch, dEDiff);
 			System.err.printf("E(loss) = %e\n", sumExpL/cnt);
 			System.err.printf("E(loss*f):\n%s\n\n", new ClassicCounter<String>(sumExpLF).divideBy(cnt));
 			System.err.printf("E(f):\n%s\n\n", new ClassicCounter<String>(sumExpF).divideBy(cnt));
