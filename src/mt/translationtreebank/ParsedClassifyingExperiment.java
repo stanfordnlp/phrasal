cf70f8208e35bcdd9df57ebecd67477327b8828e
pichuan <pichuan@stanford.edu>
2008-11-16 00:42:19 +0000

    working on the Parsed experiment
    svn path=/trunk/javanlp/; revision=23339

--- src/mt/translationtreebank/ParsedClassifyingExperiment.java
+++ src/mt/translationtreebank/ParsedClassifyingExperiment.java
@@ -12,17 +12,35 @@ import java.util.*;
 class ParsedClassifyingExperiment {
   public static void main(String[] args) throws IOException {
     Boolean reducedCategory = true;
-    List<TreePair> treepairs = ExperimentUtils.readAnnotatedTreePairs(reducedCategory);
+    List<TreePair> treepairs = ExperimentUtils.readAnnotatedTreePairs(reducedCategory, "projects/mt/src/mt/translationtreebank/data/ctb_parsed/bracketed/");
     List<Map<Pair<Integer,Integer>, Integer>> maps = new ArrayList<Map<Pair<Integer,Integer>, Integer>>();
 
 
     for (TreePair validSent : treepairs) {
       for(int deIdxInSent : validSent.NPwithDEs_deIdx_set) {
         Pair<Integer, Integer> chNPrange = validSent.NPwithDEs_deIdx.get(deIdxInSent);
-        String np = validSent.oracleChNPwithDE(deIdxInSent);
-        np = np.trim();
-        String label = validSent.NPwithDEs_categories.get(deIdxInSent);
-        String predictedType = "";
+        Pair<Integer, Integer> parsedChNPrange = validSent.parsedNPwithDEs_deIdx.get(deIdxInSent);
+
+        Tree npTree = TranslationAlignment.getTreeWithEdges(validSent.chTrees.get(0), chNPrange.first, chNPrange.second+1);
+        if (validSent.chParsedTrees.get(0) == null) {
+          throw new RuntimeException("here");
+        }
+        if ( parsedChNPrange==null) {
+          throw new RuntimeException("here");
+        }
+        Tree t = validSent.chParsedTrees.get(0);
+        System.err.println(parsedChNPrange);
+        int f = parsedChNPrange.first;
+        int s = parsedChNPrange.second+1;
+        Tree parsedNpTree = 
+          TranslationAlignment.getTreeWithEdges(t,f,s);
+
+        /*
+        npTree.pennPrint(System.out);
+        System.out.println();
+        parsedNpTree.pennPrint(System.err);
+        System.err.println();
+        */
       }
     }
   }
