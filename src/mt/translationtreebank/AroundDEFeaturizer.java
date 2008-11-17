f86e3a4b7f3be5a825b85b5b5b8e5050f5236365
pichuan <pichuan@stanford.edu>
2008-11-17 22:39:46 +0000

    work on a new Featurizer
    svn path=/trunk/javanlp/; revision=23380

---
+++ src/mt/translationtreebank/AroundDEFeaturizer.java
@@ -0,0 +1,52 @@
+package mt.translationtreebank;
+
+import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.tregex.*;
+import edu.stanford.nlp.stats.*;
+import edu.stanford.nlp.classify.*;
+import edu.stanford.nlp.ling.*;
+import java.util.*;
+import java.io.*;
+
+class AroundDEFeaturizer implements Featurizer {
+  public List<String> extractFeatures(int deIdxInSent, TreePair validSent, Properties props) {
+    String dePosStr   = props.getProperty("dePos", "false");
+    String revisedStr   = props.getProperty("revised", "false");
+    Boolean dePos = Boolean.parseBoolean(dePosStr);
+    Boolean revised = Boolean.parseBoolean(revisedStr);
+
+    Pair<Integer, Integer> chNPrange = validSent.parsedNPwithDEs_deIdx.get(deIdxInSent);
+    Tree chTree = validSent.chParsedTrees.get(0);
+    Tree chNPTree = TranslationAlignment.getTreeWithEdges(chTree,chNPrange.first, chNPrange.second+1);
+    int deIdxInPhrase = deIdxInSent-chNPrange.first;
+    Tree maskedChNPTree = ExperimentUtils.maskIrrelevantDEs(chNPTree, deIdxInPhrasex);
+    Sentence<TaggedWord> npYield = maskedChNPTree.taggedYield();
+
+    // make feature list
+    List<String> featureList = new ArrayList<String>();
+    StringBuilder sb = new StringBuilder();
+    if (dePos) {
+      sb.clear();
+      String deTag = npYield.get(deIdxInPhrase).tag();
+      sb.append("dePos:").append(deTag);
+      System.err.println("adding\t"+sb.toString());
+      featureList.add(sb.toString());
+    
+      if (revised) {
+        if ("DEC".equals(deTag)) {
+          if (ExperimentUtils.hasVApattern(maskedChNPTree))
+            featureList.add("hasVA");
+        } else if ("DEG".equals(deTag)) {
+          if (ExperimentUtils.hasADJPpattern(maskedChNPTree))
+            featureList.add("hasADJP");
+          if (ExperimentUtils.hasQPpattern(maskedChNPTree))
+            featureList.add("hasQP");
+          if (ExperimentUtils.hasNPPNpattern(maskedChNPTree))
+            featureList.add("hasNPPN");
+        }
+      }
+    }
+  }
+}
+
