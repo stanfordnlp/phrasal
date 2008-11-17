162c79838df381b562700fb14c1f9271469d8df0
pichuan <pichuan@stanford.edu>
2008-11-17 22:26:51 +0000

    a class for dumping out all the parsed NPs and correct NPs
    svn path=/trunk/javanlp/; revision=23378

---
+++ src/mt/translationtreebank/OutputAllTrees.java
@@ -0,0 +1,34 @@
+package mt.translationtreebank;
+
+import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.tregex.*;
+import edu.stanford.nlp.stats.*;
+import edu.stanford.nlp.classify.*;
+import edu.stanford.nlp.ling.*;
+import java.util.*;
+import java.io.*;
+
+class OutputAllTrees {
+  public static void main(String args[]) throws Exception {
+    List<TreePair> treepairs;
+    Boolean reducedCategory = true;
+    Boolean nonOracleTree = true;
+    treepairs = ExperimentUtils.readAnnotatedTreePairs(reducedCategory, nonOracleTree);
+    for(TreePair validSent : treepairs) {
+      for (int deIdxInSent : validSent.NPwithDEs_deIdx_set) {
+        Pair<Integer, Integer> chNPrange = validSent.NPwithDEs_deIdx.get(deIdxInSent);
+        Tree chTree = validSent.chTrees.get(0);
+        Tree chNPTree = TranslationAlignment.getTreeWithEdges(chTree,chNPrange.first, chNPrange.second+1);
+        chNPTree.pennPrint(System.out);
+        System.out.println();
+
+        Pair<Integer, Integer> parsed_chNPrange = validSent.parsedNPwithDEs_deIdx.get(deIdxInSent);
+        Tree parsed_chTree = validSent.chParsedTrees.get(0);
+        Tree parsed_chNPTree = TranslationAlignment.getTreeWithEdges(parsed_chTree,parsed_chNPrange.first, parsed_chNPrange.second+1);
+        parsed_chNPTree.pennPrint(System.err);
+        System.err.println();
+      }
+    }
+  }
+}
