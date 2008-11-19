244571fcbacd32af3d7c3b5618a5ed1173a6998e
mgalley <mgalley@stanford.edu>
2008-11-19 05:43:56 +0000

    Phrase penalty feature (constant) can now be moved out from the phrase table.
    svn path=/trunk/javanlp/; revision=23391

---
+++ src/mt/decoder/feat/PhrasePenaltyFeaturizer.java
@@ -0,0 +1,49 @@
+package mt.decoder.feat;
+
+import java.util.List;
+
+import mt.base.ConcreteTranslationOption;
+import mt.base.FeatureValue;
+import mt.base.Featurizable;
+import mt.base.Sequence;
+
+/**
+ * Moses phrase penalty generated here so that you don't have to read it countless times 
+ * from the phrase table.
+ * 
+ * @author Michel Galley
+ *
+ * @param <TK>
+ */
+public class PhrasePenaltyFeaturizer<TK> implements IncrementalFeaturizer<TK,String>, IsolatedPhraseFeaturizer<TK, String>  {
+	static public String FEATURE_NAME = "TM:phrasePenalty";
+	static private final double MOSES_PHRASE_PENALTY = Math.log(2.718);
+
+	@Override
+	public FeatureValue<String> featurize(Featurizable<TK,String> f) {
+		return new FeatureValue<String>(FEATURE_NAME, MOSES_PHRASE_PENALTY); 
+	}
+
+	@Override
+	public List<FeatureValue<String>> listFeaturize(Featurizable<TK,String> f) {
+		return null;
+	}
+
+	@Override
+	public FeatureValue<String> phraseFeaturize(Featurizable<TK, String> f) {
+		return featurize(f);
+	}
+
+	@Override
+	public List<FeatureValue<String>> phraseListFeaturize(
+			Featurizable<TK, String> f) {
+		return null;
+	}
+
+	@Override
+	public void initialize(List<ConcreteTranslationOption<TK>> options,
+			Sequence<TK> foreign) {
+	}
+
+	public void reset() { }
+}
