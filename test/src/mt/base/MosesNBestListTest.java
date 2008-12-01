db44d59dac2c0502abf8c91d9ad2a51e95b04be2
rkarthik <rkarthik@stanford.edu>
2008-12-01 21:13:07 +0000

    Added testng unit tests to few of the classes in the mt.base package.
    svn path=/trunk/javanlp/; revision=23521

---
+++ test/src/mt/base/MosesNBestListTest.java
@@ -0,0 +1,58 @@
+package mt.base;
+
+import java.io.*;
+import org.testng.annotations.*;
+
+/**
+ * 
+ * @author Karthik Raghunathan
+ *
+ */
+public class MosesNBestListTest{
+	
+		@DataProvider (name = "improperInputFiles")
+			public Object[][] improperInputFiles() throws IOException {
+	    	return new Object[][] {
+		  new Object[] { "testng/src/mt/base/improperIDs.test" },
+	          new Object[] { "testng/src/mt/base/outOfOrderIDs.test" },
+	          new Object[] { "testng/src/mt/base/improperFeatures.test" }
+//	    	  new Object[] { "improperIDs.test" },
+//	       	  new Object[] { "outOfOrderIDs.test" },
+//	       	  new Object[] { "improperFeatures.test" }
+	    	};    	
+	    }
+	
+	 	@Test
+	 	public void testConstructor() throws IOException	{
+	 		MosesNBestList nbestList = new MosesNBestList("testng/src/mt/base/properSample.test");
+//	 		MosesNBestList nbestList = new MosesNBestList("properSample.test");
+	 		
+	 		//  Features[3]: LM
+	 		//  Features[11]: TM:phi(t|f)
+	 		
+	 		assert(nbestList.nbestLists().size() == 20);			
+
+	 		assert(nbestList.nbestLists().get(0).get(0).features.get(3).value == -43.621000);
+			assert(nbestList.nbestLists().get(0).get(0).features.get(11).value == -36.746000);
+			assert(nbestList.nbestLists().get(0).get(0).translation.toString().equals("a computer into the means of taiwan ."));
+			
+			assert(nbestList.nbestLists().get(1).get(0).features.get(3).value == -66.212000);
+			assert(nbestList.nbestLists().get(1).get(0).features.get(11).value == -11.933000);
+      assert(nbestList.nbestLists().get(1).get(0).translation.toString().equals("5 afp following are closing ("));			
+	 	} 	
+		
+	 	@Test
+	 	public void testToString() throws IOException	{
+	 		MosesNBestList nbestList = new MosesNBestList("testng/src/mt/base/properSample.test");
+//	 		MosesNBestList nbestList = new MosesNBestList("properSample.test");
+	 			 		
+	 		assert(nbestList.toString().length() == 2817412);
+	 		assert(nbestList.toString().trim().endsWith("UnknownWord:0.000000"));
+			assert(nbestList.toString().startsWith("Moses N-Best List"));			
+	 	} 	
+	 	
+	 	@Test(expectedExceptions = RuntimeException.class, dataProvider = "improperInputFiles")
+	 	public void testRuntimeException(String nbestListFilename) throws IOException {
+	 		MosesNBestList nbestList = new MosesNBestList(nbestListFilename);
+	 	}
+}
