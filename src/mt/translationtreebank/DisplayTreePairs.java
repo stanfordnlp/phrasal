a418bf10154b0e74fbc21184c913b23d6b8b4959
pichuan <pichuan@stanford.edu>
2008-12-02 00:55:19 +0000

    svn path=/trunk/javanlp/; revision=23536

---
+++ src/mt/translationtreebank/DisplayTreePairs.java
@@ -0,0 +1,28 @@
+package mt.translationtreebank;
+
+import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.tregex.*;
+import edu.stanford.nlp.stats.*;
+import edu.stanford.nlp.classify.*;
+import edu.stanford.nlp.ling.*;
+import java.io.*;
+import java.util.*;
+
+
+class DisplayTreePairs {
+  public static void main(String args[]) throws Exception {
+    boolean reducedCategory = true;
+    boolean nonOracleTree = true;
+    List<TreePair> treepairs = ExperimentUtils.readAnnotatedTreePairs(reducedCategory, nonOracleTree);
+
+    for(TreePair validSent : treepairs) {
+      for (int deIdxInSent : validSent.NPwithDEs_deIdx_set) {
+        //Pair<Integer,Integer> npRange = validSent.NPwithDEs_deIdx.get(deIdxInSent);
+        String np = validSent.oracleChNPwithDE(deIdxInSent);
+        np = np.trim();
+        System.out.println(np);
+      }
+    }
+  }
+}
