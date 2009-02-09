00a2cd663226f73e25e09c9a4484391fe2484b60
pichuan <pichuan@stanford.edu>
2009-02-09 06:10:02 +0000

    not done debugging yet. current problem is that typde dependencies seems to have duplicates,etc
    svn path=/trunk/javanlp/; revision=24202

--- src/mt/discrimreorder/AlignmentMatrix.java
+++ src/mt/discrimreorder/AlignmentMatrix.java
@@ -31,16 +31,31 @@ public class AlignmentMatrix {
     tDeps = new String[f.length][f.length];
 
     try {
-      GrammaticalStructure gs = new ChineseGrammaticalStructure(t);
-      Collection<TypedDependency> typedDeps = gs.allTypedDependencies();
+      Filter<String> puncWordFilter = Filters.acceptFilter();
+      GrammaticalStructure gs = new ChineseGrammaticalStructure(t, puncWordFilter);
+      Collection<TypedDependency> typedDeps = gs.typedDependencies(false);
+      if (typedDeps.size() != f.length-2-1) {
+        System.err.print("f=");
+        System.err.println(StringUtils.join(f, " "));
+        System.err.println("DEPs=");
+        for(TypedDependency d : typedDeps) {
+          System.err.println(d);
+        }
+        throw new RuntimeException();
+      }
       if (t.yield().size()+2 != f.length) {
         throw new RuntimeException();
       }
       // The index of dep is 1-based (instead of starting at 0).
       // So it matches well with the AlignmentMatrix, where 0 is <s> anyway.
       for (TypedDependency dep : typedDeps) {
+        System.err.println("DEP="+dep);
+      }
+      for (TypedDependency dep : typedDeps) {
         d2g[dep.dep().index()] = dep.gov().index();
+        System.err.printf("d2g[%d]=%d\n", dep.dep().index(), dep.gov().index());
         tDeps[dep.gov().index()][dep.dep().index()] = dep.reln().getShortName();
+        System.err.printf("tDeps[%d][%d]=%s\n", dep.gov().index(), dep.dep().index(), dep.reln().getShortName());
       }
     } catch (Exception e) {
       e.printStackTrace();
