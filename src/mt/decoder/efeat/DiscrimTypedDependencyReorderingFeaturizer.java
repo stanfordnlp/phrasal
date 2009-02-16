5edc84d7b64791027d41aa5e1ef9b0a081bf98a0
pichuan <pichuan@stanford.edu>
2009-02-16 02:06:12 +0000

    cache the feature score to speed up
    svn path=/trunk/javanlp/; revision=24269

--- src/mt/decoder/efeat/DiscrimTypedDependencyReorderingFeaturizer.java
+++ src/mt/decoder/efeat/DiscrimTypedDependencyReorderingFeaturizer.java
@@ -49,6 +49,7 @@ public class DiscrimTypedDependencyReorderingFeaturizer implements IncrementalFe
   private LineNumberReader pathReader = null;
   private List<TwoDimensionalMap<Integer,Integer,String>> pathMaps = null;
   private LinearClassifier lc = null;
+  private TwoDimensionalMap<CoverageSet, CoverageSet, Double> featureCache;
 
   private int WINDOW = 1;
 
@@ -104,12 +105,15 @@ public class DiscrimTypedDependencyReorderingFeaturizer implements IncrementalFe
   @Override
   public void initialize(List<ConcreteTranslationOption<IString>> options, Sequence<IString> foreign) { 
     //System.err.println("DiscrimTypedDependencyReorderingFeaturizer: initialize.");
+    featureCache = new TwoDimensionalMap<CoverageSet, CoverageSet, Double>();
+
   } 
 
   int sentId = -1;
   public void reset() { 
     //System.err.println("DiscrimTypedDependencyReorderingFeaturizer: reset.");
     sentId++;
+    featureCache = new TwoDimensionalMap<CoverageSet, CoverageSet, Double>();
     //System.err.println("sentId="+sentId);
   }
 
@@ -117,59 +121,74 @@ public class DiscrimTypedDependencyReorderingFeaturizer implements IncrementalFe
   public FeatureValue<String> featurize(Featurizable<IString, String> f) { 
     List<String> features = new ArrayList<String>();
 
-    TwoDimensionalMap<Integer,Integer,String> pathMap = pathMaps.get(sentId);
+    CoverageSet cur = f.hyp.foreignCoverage;
+    CoverageSet pre;
+    if (f.prior != null)
+      pre = f.prior.hyp.foreignCoverage;
+    else
+      pre = new CoverageSet();
 
-    CoverageSet fCoverage = f.hyp.foreignCoverage;
-    int flen = f.foreignPhrase.size();
-    int prevflen = 1;
-    int prevelen = 1;
-    int currC = f.foreignPosition;
-    int prevC = -1;
-    int prevE = -1;
-    if (f.prior != null) {
-      prevC = f.prior.foreignPosition;
-      prevflen = f.prior.foreignPhrase.size();
-      prevE = f.prior.translationPosition;
-      prevelen = f.prior.translatedPhrase.size();
-    }
-    /*
-    System.err.printf("----\n");
-    System.err.printf("Partial translation (pos=%d): %s\n", f.translationPosition, f.partialTranslation);
-    System.err.printf("Foreign sentence (pos=%d): %s\n", f.foreignPosition, f.foreignSentence);
-    System.err.printf("Coverage: %s (size=%d)\n", fCoverage, f.hyp.foreignCoverage.length());
-    System.err.printf("%s(%d) => %s(%d)\n", f.foreignPhrase, f.foreignPosition, f.translatedPhrase, f.translationPosition);
-
-    if (f.prior == null) System.err.printf("Prior <s> => <s>\n");
-    else System.err.printf("Prior %s(%d) => %s(%d)\n",        f.prior.foreignPhrase, f.prior.foreignPosition, f.prior.translatedPhrase, f.prior.translationPosition);
-    //System.err.printf("j = %d, j' = %d, len1 = %d, len2 = %d\n", prevC, currC, prevflen, flen);
-    System.err.printf("i = %d, j = %d, j' = %d, lenE = %d, lenC = %d\n, lenC2 = %d\n", prevE, prevC, currC, prevelen, prevflen, flen);
-    */
-    //System.err.println(pathMap);
-    List<String> feats = extractWordFeatures(f.foreignSentence, prevC, prevflen, currC, flen,
-                                             f.partialTranslation, prevE, prevelen, pathMap);
-    /*
-    for(String feat : feats) {
-      System.err.println(" feat += "+feat);
-    }
-    */
-    features.addAll(feats);
-
-    Datum<TrainingExamples.ReorderingTypes,String> d = new BasicDatum(features);
-    ClassicCounter<TrainingExamples.ReorderingTypes> logPs = lc.logProbabilityOf(d);
-    double logP;
-    if (prevC < currC) {
-      logP = logPs.getCount(TrainingExamples.ReorderingTypes.ordered);
-    } else if (prevC > currC) {
-      logP = logPs.getCount(TrainingExamples.ReorderingTypes.distorted);
+    Double featVal = featureCache.get(cur, pre);
+    if (featVal != null) {
+      //System.err.println("pichuan: get val from cache");
+      return new FeatureValue<String>(FEATURE_NAME, featVal);
     } else {
-      throw new RuntimeException();
+      TwoDimensionalMap<Integer,Integer,String> pathMap = pathMaps.get(sentId);
+
+      CoverageSet fCoverage = f.hyp.foreignCoverage;
+      int flen = f.foreignPhrase.size();
+      int prevflen = 1;
+      int prevelen = 1;
+      int currC = f.foreignPosition;
+      int prevC = -1;
+      int prevE = -1;
+      if (f.prior != null) {
+        prevC = f.prior.foreignPosition;
+        prevflen = f.prior.foreignPhrase.size();
+        prevE = f.prior.translationPosition;
+        prevelen = f.prior.translatedPhrase.size();
+      }
+      /*
+        System.err.printf("----\n");
+        System.err.printf("Partial translation (pos=%d): %s\n", f.translationPosition, f.partialTranslation);
+        System.err.printf("Foreign sentence (pos=%d): %s\n", f.foreignPosition, f.foreignSentence);
+        System.err.printf("Coverage: %s (size=%d)\n", fCoverage, f.hyp.foreignCoverage.length());
+        System.err.printf("%s(%d) => %s(%d)\n", f.foreignPhrase, f.foreignPosition, f.translatedPhrase, f.translationPosition);
+        
+        if (f.prior == null) System.err.printf("Prior <s> => <s>\n");
+        else System.err.printf("Prior %s(%d) => %s(%d)\n",        f.prior.foreignPhrase, f.prior.foreignPosition, f.prior.translatedPhrase, f.prior.translationPosition);
+        //System.err.printf("j = %d, j' = %d, len1 = %d, len2 = %d\n", prevC, currC, prevflen, flen);
+        System.err.printf("i = %d, j = %d, j' = %d, lenE = %d, lenC = %d\n, lenC2 = %d\n", prevE, prevC, currC, prevelen, prevflen, flen);
+      */
+      //System.err.println(pathMap);
+      List<String> feats = extractWordFeatures(f.foreignSentence, prevC, prevflen, currC, flen,
+                                               f.partialTranslation, prevE, prevelen, pathMap);
+      /*
+        for(String feat : feats) {
+        System.err.println(" feat += "+feat);
+        }
+      */
+      features.addAll(feats);
+
+      Datum<TrainingExamples.ReorderingTypes,String> d = new BasicDatum(features);
+      ClassicCounter<TrainingExamples.ReorderingTypes> logPs = lc.logProbabilityOf(d);
+      double logP;
+      if (prevC < currC) {
+        logP = logPs.getCount(TrainingExamples.ReorderingTypes.ordered);
+      } else if (prevC > currC) {
+        logP = logPs.getCount(TrainingExamples.ReorderingTypes.distorted);
+      } else {
+        throw new RuntimeException();
+      }
+      /*
+        System.err.printf("log p(%s|d) = %g\n",
+        TrainingExamples.ReorderingTypes.ordered,
+        logP_ordered);
+      */
+      //System.err.println("pichuan: insert val to cache");
+      featureCache.put(cur, pre, logP);
+      return new FeatureValue<String>(FEATURE_NAME, logP);
     }
-    /*
-    System.err.printf("log p(%s|d) = %g\n",
-                      TrainingExamples.ReorderingTypes.ordered,
-                      logP_ordered);
-    */
-    return new FeatureValue<String>(FEATURE_NAME, logP);
   }
 
   @Override
