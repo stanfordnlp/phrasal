47a6f9504771a65ebbfba77c8bc8cfb7f19780a4
pichuan <pichuan@stanford.edu>
2009-02-24 21:28:52 +0000

    add code for handling multi target
    svn path=/trunk/javanlp/; revision=24419

--- src/mt/discrimreorder/TrainingExamples.java
+++ src/mt/discrimreorder/TrainingExamples.java
@@ -24,6 +24,24 @@ public class TrainingExamples {
 
   Counter<String> extractExamples(AlignmentMatrix matrix) {
     Counter<String> classCounter = new IntCounter<String>();
+
+    int maxEinMatrix[] = null;
+
+    if (dealWithMultiTarget) {
+      maxEinMatrix = new int[matrix.f.length];
+      for(int fi = 0; fi < matrix.f.length; fi++) {
+        boolean set = false;
+        for(int ei = matrix.e.length-1; ei >= 0; ei--) {
+          if (matrix.fe[fi][ei]) {
+            maxEinMatrix[fi] = ei;
+            set = true;
+            break;
+          }
+        }
+        if (!set) maxEinMatrix[fi] = -1;
+      }
+    }
+
     for(int ei = 0; ei < matrix.e.length-1; ei++) {
       int ei_prime = ei+1;
       Set<Integer> ei_fs = new TreeSet<Integer>();
@@ -49,20 +67,34 @@ public class TrainingExamples {
         continue;
       }
 
-      int minf_ei = Collections.min(ei_fs);
-      int maxf_ei = Collections.max(ei_fs);
-      int minf_eiprime = Collections.min(eiprime_fs);
-      int maxf_eiprime = Collections.max(eiprime_fs);
-
-      if (maxf_ei < minf_eiprime) {
-        example = new TrainingExample(ei, maxf_ei, minf_eiprime, ReorderingTypes.ordered);
-        classCounter.incrementCount("ordered");
-      } else if (maxf_eiprime < minf_ei) {
-        example = new TrainingExample(ei, minf_ei, maxf_eiprime, ReorderingTypes.distorted);
-        classCounter.incrementCount("distorted");
-      } else {
-        classCounter.incrementCount("mixed");
+      boolean skip = false;
+      if (dealWithMultiTarget) {
+        for (int f : ei_fs) {
+          if (maxEinMatrix[f] > ei) {
+            classCounter.incrementCount("mixed");
+            skip = true;
+            break;
+          }
+        }
       }
+
+      if (!skip) {
+        int minf_ei = Collections.min(ei_fs);
+        int maxf_ei = Collections.max(ei_fs);
+        int minf_eiprime = Collections.min(eiprime_fs);
+        int maxf_eiprime = Collections.max(eiprime_fs);
+        
+        if (maxf_ei < minf_eiprime) {
+          example = new TrainingExample(ei, maxf_ei, minf_eiprime, ReorderingTypes.ordered);
+          classCounter.incrementCount("ordered");
+        } else if (maxf_eiprime < minf_ei) {
+          example = new TrainingExample(ei, minf_ei, maxf_eiprime, ReorderingTypes.distorted);
+          classCounter.incrementCount("distorted");
+        } else {
+          classCounter.incrementCount("mixed");
+        }
+      }
+
       if (example != null) examples.add(example);
     }
     return classCounter;
