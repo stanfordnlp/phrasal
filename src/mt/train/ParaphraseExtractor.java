0d8a5577cd96a95ac0114da004410f1b6979393a
mgalley <mgalley@stanford.edu>
2009-02-25 01:46:33 +0000

    paraphrases can now be scored using PMI.
    svn path=/trunk/javanlp/; revision=24427

--- src/mt/train/ParaphraseExtractor.java
+++ src/mt/train/ParaphraseExtractor.java
@@ -6,10 +6,7 @@ import java.util.*;
 import java.io.PrintStream;
 import java.io.IOException;
 
-import edu.stanford.nlp.util.StringUtils;
-import edu.stanford.nlp.util.Beam;
-import edu.stanford.nlp.util.IString;
-import edu.stanford.nlp.util.IStrings;
+import edu.stanford.nlp.util.*;
 
 /**
  * Prints a monolingual (source-language) phrase table instead of bilingual,
@@ -31,6 +28,9 @@ public class ParaphraseExtractor extends CombinedFeatureExtractor {
 
   public static final String INV_MODEL_PROPERTY = "InvModel";
   public static final boolean INV_MODEL = Boolean.parseBoolean(System.getProperty(INV_MODEL_PROPERTY, "false"));
+
+  public static final String PMI_PROPERTY = "PMI";
+  public static final boolean PMI = Boolean.parseBoolean(System.getProperty(PMI_PROPERTY, "false"));
 
   class PhraseHyp { int i; double d; public PhraseHyp(int i, double d) { this.i=i; this.d=d; } }
 
@@ -58,29 +58,53 @@ public class ParaphraseExtractor extends CombinedFeatureExtractor {
 
   @Override
   public boolean write(PrintStream oStream, boolean noAlign) {
+
+    assert(extractors.size() == 1);
+    PharaohFeatureExtractor e = (PharaohFeatureExtractor) extractors.get(0);
+
     Map<Integer, Beam<PhraseHyp>> kbest_ef = getKBest(BEAM_SZ,false);
     Map<Integer,Beam<PhraseHyp>> kbest_fe = getKBest(BEAM_SZ,true);
+
     if(oStream == null)
         oStream = System.out;
+    
     for(int fKey : kbest_ef.keySet()) {
+
       if(alTemps.getF(fKey) == null)
         continue;
+      
       String phrase = keyToPlainString(fKey);
       oStream.print(phrase);
       oStream.print("\n");
+
       Map<Integer,Double> scores = new HashMap<Integer,Double>();
+
+      double totalFCount = totalFCount();
+
       for(PhraseHyp eh : kbest_ef.get(fKey)) {
         for(PhraseHyp fh : kbest_fe.get(eh.i)) {
-          double score = 0.0;
-          if(scores.get(fh.i) != null)
-            score = scores.get(fh.i);
-          score += eh.d*fh.d;
-          scores.put(fh.i,score);
+          int fKey2 = fh.i;
+          double sum = (scores.get(fKey2) != null) ? scores.get(fh.i) : 0.0;
+          // Compute (or update): P(f' | f) = \sum_c P(f' | e) P(e | f)
+          double score = eh.d*fh.d;
+          // Compute (or update): PMI(f,f') = P(f,f') / P(f)P(f') = P(f'|f)P(f) / P(f)P(f') =  P(f'|f)/P(f'),
+          // i.e., above score divited by P(f'):
+          if(PMI) {
+            double denom = e.fCounts.get(fKey2)/totalFCount;
+            score /= denom;
+          }
+          scores.put(fh.i, sum+score);
         }
       }
+
+
       Beam<PhraseHyp> nbest = new Beam<PhraseHyp>(NBEST_SZ,beamCmp);
-      for(int fKey2 : scores.keySet())
-        nbest.add(new PhraseHyp(fKey2,scores.get(fKey2)));
+
+      for(int fKey2 : scores.keySet()) {
+        double s = scores.get(fKey2);
+        nbest.add(new PhraseHyp(fKey2, PMI ? Math.log(s) : s));
+      }
+      
       for(PhraseHyp hyp : nbest.asSortedList()) { 
         String paraphrase = keyToPlainString(hyp.i);
         if(SKIP_IDENTICAL && phrase.equals(paraphrase))
@@ -92,37 +116,64 @@ public class ParaphraseExtractor extends CombinedFeatureExtractor {
         oStream.print("\n");
       }
     }
+    
     return true;
   }
 
+  /**
+   * For each phrase f, get k-best e according P(e|f).
+   * @param k Size of k-best list.
+   * @param inv If true, return k-best f according to P(f|e).
+   */
   private Map<Integer,Beam<PhraseHyp>> getKBest(int k, boolean inv) {
     Map<Integer, Beam<PhraseHyp>> m = new Int2ObjectOpenHashMap<Beam<PhraseHyp>>();
+
+    assert(extractors.size() == 1);
+    PharaohFeatureExtractor e = (PharaohFeatureExtractor) extractors.get(0);
+
     for(int idx=0; idx<alTemps.size(); ++idx) {
+
       alTemps.reconstructAlignmentTemplate(alTemp, idx);
+      
       int fKey = inv ? alTemp.getEKey() : alTemp.getFKey();
       int eKey = inv ? alTemp.getFKey() : alTemp.getEKey();
+
       if(m.get(fKey) == null)
-        m.put(fKey,new Beam<PhraseHyp>(k,beamCmp));
+        m.put(fKey, new Beam<PhraseHyp>(k,beamCmp));
       Beam<PhraseHyp> beam = m.get(fKey);
-      assert(extractors.size() == 1);
-      AbstractFeatureExtractor e = extractors.get(0);
+
       Object scores = e.score(alTemp);
       assert(scores.getClass().isArray());
       double[] scoreArray = (double[]) scores;
+
       if(scoreArray.length != 2)
         throw new RuntimeException("Score array of unexpected length: "+scoreArray.length+" != 2.");
+      
       double score = scoreArray[(inv ^ INV_MODEL) ? 0 : 1];
       beam.add(new PhraseHyp(eKey,score));
     }
+    
     return m;
   }
 
+  private int totalFCount() {
+    assert(extractors.size() == 1);
+    PharaohFeatureExtractor e = (PharaohFeatureExtractor) extractors.get(0);
+    int total = 0;
+    for(int c : e.fCounts)
+      total += c;
+    return total;
+  }
+
   public static void main(String[] args) throws IOException {
+    
     Properties prop = StringUtils.argsToProperties(args);
-    prop.put(EXTRACTORS_OPT,"mt.train.PharaohFeatureExtractor");
-    prop.put(ONLY_ML_OPT,"true");
-    prop.put(SPLIT_SIZE_OPT,"1");
+    prop.put(EXTRACTORS_OPT, "mt.train.PharaohFeatureExtractor");
+    prop.put(ONLY_ML_OPT, "true");
+    prop.put(SPLIT_SIZE_OPT, "1");
+    
     AbstractPhraseExtractor.setPhraseExtractionProperties(prop);
+
     try {
       ParaphraseExtractor e = new ParaphraseExtractor(prop);
       e.extractAll();
