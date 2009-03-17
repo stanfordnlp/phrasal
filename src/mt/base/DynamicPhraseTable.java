d99c2392d341356e5a972291933c3b13b346ea65
cerd <cerd@stanford.edu>
2009-03-17 05:25:04 +0000

    tokyo cabinet - fast dbm for dynamic phrase table. -d
    svn path=/trunk/javanlp/; revision=24725

--- src/mt/base/DynamicPhraseTable.java
+++ src/mt/base/DynamicPhraseTable.java
@@ -3,31 +3,20 @@
  */
 package mt.base;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 import mt.decoder.feat.IsolatedPhraseFeaturizer;
 import mt.decoder.util.Scorer;
 
 import edu.stanford.nlp.util.IString;
 import edu.stanford.nlp.util.IStrings;
+import edu.stanford.nlp.stats.ClassicCounter;
+import edu.stanford.nlp.stats.Counters;
 
 import java.util.LinkedList;
 
-import com.sleepycat.je.Cursor;
-import com.sleepycat.je.Database;
-import com.sleepycat.je.DatabaseConfig;
-import com.sleepycat.je.DatabaseEntry;
-import com.sleepycat.je.Environment;
-import com.sleepycat.je.EnvironmentConfig;
-import com.sleepycat.je.LockMode;
-import com.sleepycat.je.OperationStatus;
+import tokyocabinet.*;
 
 
 /**
@@ -38,11 +27,10 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 	static final boolean DEBUG = false;
 	static final int MAX_ABSOLUTE_DISTORTION = 12;
 	
-	Database db;
-	Environment dbEnv;
+	BDB bdb;
 	
 	Set<String> currentSequence; 
-	IBMModel1 model1S2T, model1T2S;
+	//IBMModel1 model1S2T, model1T2S;
 	
 	public DynamicPhraseTable(
 			IsolatedPhraseFeaturizer<IString, FV> phraseFeaturizer, Scorer<FV> scorer, String phraseTableName, String model1S2T, String model1T2S) {
@@ -50,8 +38,6 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 		currentSequence = new HashSet<String>();
 		initdb(phraseTableName);
 		try {
-		this.model1S2T = IBMModel1.load(model1S2T);
-		this.model1T2S = IBMModel1.load(model1T2S);
 		} catch (Exception e) { throw new RuntimeException(e); }		
 	}
 	
@@ -64,28 +50,21 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 
 	private void initdb(String phraseTableName) {
 		try {			
-			EnvironmentConfig envConfig = new EnvironmentConfig();
-			envConfig.setAllowCreate(true);
- 
 			File f = new File(phraseTableName);
 			if (!f.exists()) {
 				f.mkdir();
 			}
 			
-			dbEnv = new Environment(new File(phraseTableName), envConfig);
-
-			// Open the database. Create it if it does not already exist.
-			DatabaseConfig dbConfig = new DatabaseConfig();
-			dbConfig.setAllowCreate(true);
-			dbConfig.setDeferredWrite(true);
-			dbConfig.setSortedDuplicates(true);
-			db = dbEnv.openDatabase(null, "dpt", dbConfig);
+			bdb = new BDB();
+			if (!bdb.open(phraseTableName, BDB.OREADER))
+				throw new RuntimeException();
+				
 		} catch (Exception e) {
 			throw new RuntimeException(e);			
 		}	
 	}
 	
-	public final int phraseLengthLimit = 5;
+	static public final int phraseLengthLimit = 5;
 	
 	@Override
 	public String getName() {
@@ -93,52 +72,43 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 	}
 
 
+	public static final String[] labs = new String[]{"pc(e|f)", "pc(f|e)", "lex(e|f)", "lex(f|e)"};
+
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<TranslationOption<IString>> getTranslationOptions(
 			Sequence<IString> sequence) {		
+		try {
 		List<TranslationOption<IString>> opts = new LinkedList<TranslationOption<IString>>();
 	
 		IString noConst = new IString("noConst");
-		
-		try {
+		RawSequence<IString> rawSequence = new RawSequence<IString>(sequence);
+	
+		List<byte[]> listByteOpts = (List)bdb.getlist(sequence.toString().getBytes("UTF-8"));
 
-			// warning code that dan r. would definitely not like ahead
-			Cursor cursor = db.openCursor(null, null);
-			
-			DatabaseEntry key = new DatabaseEntry(sequence.toString().getBytes("UTF-8"));
-			DatabaseEntry data = new DatabaseEntry();
-			int pairsRetrived = 0;
-			for (OperationStatus status = cursor.getSearchKey(key, data, LockMode.READ_UNCOMMITTED); 
-			status == OperationStatus.SUCCESS; status = cursor.getNextDup(key, data, LockMode.READ_UNCOMMITTED)) { 
-				String transS = new String(data.getData(), "UTF-8");
-				RawSequence<IString> transSeq = new RawSequence<IString>(IStrings.toIStringArray(transS.split("\\s+")));
-				String mappingKey = sequence+"=:=>"+transS;
-				//System.err.printf("Sequences: %s => %s Feature rep: %s\n", sequence, transSeq, featRep);
-				if (model1S2T != null && model1T2S != null) {
-					opts.add(new TranslationOption<IString>(
-							new float[]{(float)-1.0, (float)model1S2T.score(sequence, transSeq), (float)model1T2S.score(transSeq, sequence)}, 
-							new String[]{"PhrPen", "lex(f|e)", "lex(e|f)"}, 
-							new RawSequence<IString>(transSeq), new RawSequence<IString>(sequence), noConst, currentSequence.contains(mappingKey)));
-				} else {
-					opts.add(new TranslationOption<IString>(new float[]{(float)-1.0}, new String[]{"PhrPen"}, 
-							new RawSequence<IString>(transSeq), new RawSequence<IString>(sequence), noConst, currentSequence.contains(mappingKey)));
-				}
-				if (currentSequence.contains(mappingKey)) {
-					// System.err.printf("mapping key found '%s'\n", mappingKey);
-					pairsRetrived++; 
-				} else {
-					// System.err.printf("mapping key not found '%s'\n", mappingKey);
-				}
-			}
-			
-			// System.err.printf("Sequence specific pairs retrived: '%s' : %d\n", sequence, pairsRetrived);
-			// System.err.printf("currentSequence size: %d\n", currentSequence.size());
-			cursor.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		if (listByteOpts == null) return opts;
+
+		for (byte[] byteOpts : listByteOpts) {
+				DataInputStream distrm = new DataInputStream(new ByteArrayInputStream(byteOpts));
+				String trans = distrm.readUTF();
+				float  pcEgF  = distrm.readFloat();
+				float  pcFgE  = distrm.readFloat();
+				float  pLexEgF = distrm.readFloat();
+				float  pLexFgE = distrm.readFloat();
+				RawSequence<IString> transSeq = new RawSequence<IString>(IStrings.toIStringArray(trans.split("\\s+")));
+				String mappingKey = sequence+"=:=>"+trans;
+				opts.add(new TranslationOption<IString>(
+								new float[]{pcEgF, pcFgE, pLexEgF, pLexFgE},
+							  labs,
+								transSeq,
+								rawSequence,										
+								null,
+								currentSequence.contains(mappingKey)
+								));	
+		}	
 		
 		return opts;
+		} catch (Exception e) { throw new RuntimeException(e); }
 	}
 
 	@Override
@@ -148,8 +118,8 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 	
 	public void close() {
 		try {
-		db.close();
-		dbEnv.close();
+		//db.close();
+		//dbEnv.close();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -157,17 +127,10 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 	
 	@Override
 	public void setCurrentSequence(Sequence<IString> foreign, List<Sequence<IString>> tranList) {
-		//System.err.printf("****DYN**** PHRASE GENERATOR SETTING CURRENT SEQ\n");
 		currentSequence = new HashSet<String>();
-		//System.err.printf("foreign: %s\n", foreign);
 		if (tranList == null) return;
-		//System.err.println("Setting Current Sequence Pair:");
-		//System.err.printf("source: %s\n", foreign);
-		//System.err.printf("target: %s\n", tranList.get(0));
 		int pairSpecificPhrases = 0;
-		try {
 		for (Sequence<IString> trans : tranList) {
-		//System.err.printf("tran: %s\n", trans);
 		for (int fStart = 0; fStart < foreign.size(); fStart++) {
 		for (int fEnd = fStart; fEnd < foreign.size() && fEnd < fStart + phraseLengthLimit; fEnd++) {
 		Sequence<IString> fPhrase = foreign.subsequence(fStart, fEnd+1);
@@ -182,74 +145,217 @@ public class DynamicPhraseTable<FV> extends AbstractPhraseGenerator<IString,FV> 
 			Sequence<IString> tPhrase = trans.subsequence(tStart, tEnd+1);
 			String featRep = fPhrase+"=:=>"+tPhrase;
 			currentSequence.add(featRep);
-			DatabaseEntry key = new DatabaseEntry(fPhrase.toString().getBytes("UTF-8"));
-			DatabaseEntry data = new DatabaseEntry(tPhrase.toString().getBytes("UTF-8"));
-			if (db.put(null, key, data) != OperationStatus.SUCCESS) throw new RuntimeException();
 			// System.err.printf("putting '%s=:=>%s'\n", fPhrase, tPhrase);
 			pairSpecificPhrases++;
 			phraseSpecificTranslations++;
 		}
 		}
-		/*
-		System.err.printf("phrase specific inserts: '%s' : %d\n", fPhrase, phraseSpecificTranslations);
-		Cursor cursor = db.openCursor(null, null);
-		
-		DatabaseEntry key = new DatabaseEntry(fPhrase.toString().getBytes("UTF-8"));
-		DatabaseEntry data = new DatabaseEntry();
-		int pairsRetrived = 0;
-		
-		for (OperationStatus status = cursor.getSearchKey(key, data, LockMode.READ_UNCOMMITTED); 
-		status == OperationStatus.SUCCESS; status = cursor.getNextDup(key, data, LockMode.READ_UNCOMMITTED)) {
-			String foreignS = new String(key.getData(), "UTF-8");
-			String transS = new String(data.getData(), "UTF-8");
-			System.err.printf("retrived '%s=:=>%s'\n", foreignS, transS);
-			pairsRetrived++;
-		}
-		System.err.printf("Pairs retreived: %d\n", pairsRetrived);
-		*/
 		}				
 		}
 		}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+	}
+
+	static private int extractFromSequence(BDB tmpF2E, BDB tmpE2F, BDB tmpFPhrases, Sequence<IString> foreign, Sequence<IString> trans) throws Exception {
+		int pairSpecificPhrases = 0;
+		for (int fStart = 0; fStart < foreign.size(); fStart++) {
+		for (int fEnd = fStart; fEnd < foreign.size() && fEnd < fStart + phraseLengthLimit; fEnd++) {
+		Sequence<IString> fPhrase = foreign.subsequence(fStart, fEnd+1);
+		int phraseSpecificTranslations = 0;
+
+		int tEquivFStart = (int)((fStart/(double)foreign.size())*trans.size());	
+		for (int tStart = Math.max(0, tEquivFStart-MAX_ABSOLUTE_DISTORTION); 
+             tStart < Math.min(trans.size(), tEquivFStart+MAX_ABSOLUTE_DISTORTION); 
+						 tStart++) {
+		for (int tEnd = tStart; tEnd < trans.size() && tEnd < tStart + phraseLengthLimit; tEnd++) {
+				
+			Sequence<IString> tPhrase = trans.subsequence(tStart, tEnd+1);
+			byte[] fBytes = fPhrase.toString().getBytes("UTF-8");
+			byte[] tBytes = tPhrase.toString().getBytes("UTF-8");
+			tmpF2E.putdup(fBytes, tBytes);
+			tmpE2F.putdup(tBytes, fBytes);
+			tmpFPhrases.put(fBytes, new byte[0]);
+			// System.err.printf("putting '%s=:=>%s'\n", fPhrase, tPhrase);
+			pairSpecificPhrases++;
+			phraseSpecificTranslations++;
 		}
+		}
+		}				
+		}
+		return pairSpecificPhrases;
+	} 
+
+	static public void extractDB(String fCorpus, String eCorpus, String tcFile, IBMModel1 model1F2E, IBMModel1 model1E2F)
+		throws Exception {
+		BufferedReader fReader = new BufferedReader(new FileReader(fCorpus));
+		BufferedReader eReader = new BufferedReader(new FileReader(eCorpus));
+		BDB tmpF2E = new BDB();
+		String fileTmpF2E = "tmp."+fCorpus+".2."+eCorpus;
+		String fileTmpE2F = "tmp."+eCorpus+".2."+fCorpus;
+		String fileTmpFPhrases = "tmp."+fCorpus+".phrases";
+
+		System.err.printf("tmp f2e: %s\n", fileTmpF2E);
+		System.err.printf("tmp e2f: %s\n", fileTmpE2F);
+		System.err.printf("tmp fPhrases: %s\n", fileTmpFPhrases);
+
+		if (!tmpF2E.open(fileTmpF2E, BDB.OWRITER | BDB.OTRUNC | BDB.OCREAT ))
+			throw new RuntimeException();
+		BDB tmpE2F = new BDB();
+		if (!tmpE2F.open(fileTmpE2F, BDB.OWRITER | BDB.OTRUNC | BDB.OCREAT ))
+			throw new RuntimeException();
+
+		BDB tmpFPhrases = new BDB();
+		if (!tmpFPhrases.open(fileTmpFPhrases, BDB.OWRITER | BDB.OTRUNC | BDB.OCREAT ))
+			throw new RuntimeException();
+
+
+		int lineno = -1;
+		long phrasesExtracted = 0;
+		for (String fLine = fReader.readLine(), eLine = eReader.readLine();
+				 fLine != null && eLine != null;
+			 	 fLine = fReader.readLine(), eLine = eReader.readLine()) { lineno++;
+				Sequence<IString> fSeq = new RawSequence<IString>(IStrings.toIStringArray(fLine.split("\\s+")));
+				Sequence<IString> eSeq = new RawSequence<IString>(IStrings.toIStringArray(eLine.split("\\s+")));
+				
+				phrasesExtracted += extractFromSequence(tmpF2E, tmpE2F, tmpFPhrases, fSeq, eSeq);
+				if (lineno % 100 == 0) { 
+					System.err.printf("lineno > %d (phrases: %d)\n", lineno, phrasesExtracted); }
+		}
+		System.err.printf("Phrases Extracted pre-filtering: %d\n", phrasesExtracted);
+
+		// syncing before close prevents weird hanging behavior
+		System.err.printf("Closing tmpF2E\n");
+		tmpF2E.sync(); tmpF2E.close();	 
+		System.err.printf("Closing tmpE2F\n");
+		tmpE2F.sync(); tmpE2F.close();
+		System.err.printf("Closing tmpFPhrases\n");
+		tmpFPhrases.sync(); tmpFPhrases.close();
+	
+		tmpF2E = new BDB();
+		tmpE2F = new BDB();
+		tmpFPhrases = new BDB();
+
+		tmpF2E.open(fileTmpF2E, BDB.OREADER);
+		tmpE2F.open(fileTmpE2F, BDB.OREADER);
+		tmpFPhrases.open(fileTmpFPhrases, BDB.OREADER);			
 		
-		/*
-		System.err.printf("Current sequence mapping keys\n");
-		for (String mappingKey : currentSequence) {
-			System.out.printf("'%s'\n", mappingKey);
+		String fileProbE2F = "tmp.prob."+eCorpus+".2."+fCorpus;
+		System.err.printf("fileProbE2F: %s\n", fileProbE2F);
+		BDB tmpProbE2F = new BDB();
+		if (!tmpProbE2F.open(fileProbE2F, BDB.OWRITER | BDB.OTRUNC | BDB.OCREAT | BDB.OREADER))
+			throw new RuntimeException();
+
+		BDB dbPT = new BDB();
+		if (!dbPT.open(tcFile, BDB.OWRITER | BDB.OTRUNC | BDB.OCREAT)) 
+			throw new RuntimeException();
+
+		BDBCUR fcur = new BDBCUR(tmpFPhrases);
+    fcur.first();
+    byte[] key;
+		int lowCntsRemoved = 0;
+		int ptSize = 0;
+    while((key = fcur.key()) != null){	
+			String fphrase = new String(key, "UTF-8");
+			System.err.printf("%s\n", fphrase);
+			List<byte[]> transByteList = (List)tmpF2E.getlist(fcur.key());
+			ClassicCounter<String> pF2E = new ClassicCounter<String>();
+			
+			for (byte[] transByte : transByteList) {
+				String trans = new String(transByte, "UTF-8");
+				pF2E.incrementCount(trans);
+			} 
+
+			Set<String> lowCnt = new HashSet<String>();
+			for (Map.Entry<String,Double> entry : pF2E.entrySet()) {
+				if (entry.getValue() < 3) {
+					lowCnt.add(entry.getKey());
+				}
+			}
+
+			double oldTotalCnt = pF2E.totalCount();
+
+			for (String lowCntKey : lowCnt) {
+				System.err.println("Skipping: "+fphrase+" ||| "+lowCntKey+" ||| "+pF2E.getCount(lowCntKey));
+				pF2E.remove(lowCntKey);
+			}
+
+			lowCntsRemoved += lowCnt.size();
+
+			if (pF2E.size() == 0) {
+				fcur.next();
+				continue;
+			}	
+
+			Counters.divideInPlace(pF2E, oldTotalCnt);
+
+			ClassicCounter<String> pE2F = new ClassicCounter<String>();
+
+			
+			for (String trans : pF2E.keySet()) {
+				String probStr = tmpProbE2F.get(fphrase+":::"+trans);
+				if (probStr == null) {
+					List<byte[]> forByteList = (List)tmpE2F.getlist(trans.getBytes("UTF-8"));
+					ClassicCounter<String> allE2F = new ClassicCounter<String>();
+					for (byte[] forByte : forByteList) {
+						 String eForeign = new String(forByte, "UTF-8");
+						 allE2F.incrementCount(eForeign+":::"+trans);
+					}
+					double allE2Ftc = allE2F.totalCount();
+					for (Map.Entry<String,Double> entry : allE2F.entrySet()) {
+						if (entry.getValue() < 3) continue; 
+						tmpProbE2F.put(entry.getKey(), (new Double(entry.getValue()/allE2Ftc)).toString());
+					}
+					probStr = tmpProbE2F.get(fphrase+":::"+trans);
+				} else {
+					System.err.println("ProbStr Match!!!!! >" +fphrase+":::"+trans);
+				} 
+				pE2F.setCount(trans, Double.parseDouble(probStr));
+			}
+
+			for (String trans : pF2E.keySet()) {
+        float pcEgF = (float)Math.log(pF2E.getCount(trans)); // pcEgF
+        float pcFgE = (float)Math.log(pE2F.getCount(trans)); // pcFgE
+				RawSequence<IString> fphraseSeq = new RawSequence<IString>(IStrings.toIStringArray(fphrase.split("\\s+")));
+				RawSequence<IString> transSeq = new RawSequence<IString>(IStrings.toIStringArray(trans.split("\\s+")));
+				float pLexEgF = (float)model1F2E.score(fphraseSeq, transSeq); // pLexEgF
+				float pLexFgE = (float)model1E2F.score(transSeq, fphraseSeq); // pLexFgE
+
+				System.err.printf("Inserting %s ||| %s %f %f %f %f\n", fphrase, trans, 
+							Math.exp(pcEgF), Math.exp(pcFgE), 
+							Math.exp(pLexEgF),  Math.exp(pLexFgE));
+				
+				ByteArrayOutputStream bostrm = new ByteArrayOutputStream();
+				DataOutputStream dostrm = new DataOutputStream(bostrm);
+
+				dostrm.writeUTF(trans);
+        dostrm.writeFloat(pcEgF);
+        dostrm.writeFloat(pcFgE);
+				dostrm.writeFloat(pLexEgF);
+				dostrm.writeFloat(pLexFgE);
+				dostrm.flush();
+				dbPT.putdup(fphrase.getBytes("UTF-8"), bostrm.toByteArray());
+				ptSize++;
+			}	
+
+			fcur.next();
 		}
-		System.err.printf("Pair specific phrases generated: %d\n", pairSpecificPhrases);
-		System.err.printf("currentSequence size: %d\n", currentSequence.size());
-		*/
+		System.err.printf("Low cnts removed: %d\n", lowCntsRemoved);
+		System.err.printf("Phrase table size: %d\n", ptSize);
+		tmpProbE2F.sync(); tmpProbE2F.close();
+		dbPT.sync(); dbPT.close();
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-	  DynamicPhraseTable<String> dpt = new DynamicPhraseTable<String>(null, null, args[0]);
-	  
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for (String foreign = br.readLine(); foreign != null && !foreign.equals(""); foreign = br.readLine()) {
-			String trans = br.readLine();
-			if (trans == null || trans.equals("")) break;
-		
-			RawSequence<IString> foreignSeq = new RawSequence<IString>(IStrings.toIStringArray(foreign.split("\\s+")));
-			List<Sequence<IString>> transList = new ArrayList<Sequence<IString>>();
-			transList.add(new RawSequence<IString>(IStrings.toIStringArray(trans.split("\\s+"))));
-			dpt.setCurrentSequence(foreignSeq, transList);
-			for (int fStart = 0; fStart < foreignSeq.size(); fStart++) {
-				for (int fEnd = fStart; fEnd < foreignSeq.size() && fEnd < fStart + dpt.phraseLengthLimit; fEnd++) {
-					RawSequence<IString> foreignPhr = new RawSequence(foreignSeq.subsequence(fStart, fEnd+1));
-					List<TranslationOption<IString>> options = dpt.getTranslationOptions(foreignPhr);
-					System.out.printf("%s =>\n", foreignPhr);
-					for (TranslationOption<IString> option : options) {
-						System.out.printf("\t%s %s %s\n", option.translation, option.forceAdd, option.phraseScoreNames[0]);
-					}
-				}
-			}
-		}
-		dpt.close();
+		String strModel1F2E = args[0].split(":")[1];
+		System.out.printf("Loading F2E: %s\n", strModel1F2E);
+		IBMModel1 model1F2E = IBMModel1.load(strModel1F2E);
+
+		String strModel1E2F = args[1].split(":")[1];
+		System.out.printf("Loading E2F: %s\n", strModel1E2F);
+		IBMModel1 model1E2F = IBMModel1.load(strModel1E2F);
+		extractDB(args[0].split(":")[0], args[1].split(":")[0], args[2],
+					model1F2E, model1E2F);
 	}
 }
