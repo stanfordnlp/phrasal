fa33a740db07c0d536251d8545b3a5bfb2f46f5c
mgalley <mgalley@stanford.edu>
2009-07-28 20:08:03 +0000

    added sanity check
    svn path=/trunk/javanlp/; revision=26093

--- src/mt/tune/UnsmoothedMERT.java
+++ src/mt/tune/UnsmoothedMERT.java
@@ -709,6 +709,8 @@ public class UnsmoothedMERT extends Thread {
           if (score < minReachableScore)
             minReachableScore = score;
         }
+				if(nbestlist.size() == 0)
+					throw new RuntimeException(String.format("Nbest list of size zero at %d. Perhaps PseudoMoses ran out of memory?\n", lI));
         System.err.printf("l %d - min reachable score: %f (orig size: %d)\n",
                 lI, minReachableScore, nbestlist.size());
         for (ScoredFeaturizedTranslation<IString, String> trans : nbestlist) {
