5cfd439671c1fe7d9c6543a081116bb027b05ca2
cerd <cerd@stanford.edu>
2009-09-07 23:25:41 +0000

    java versions of some of the Turk command line data prep tools
    svn path=/trunk/javanlp/; revision=26406

---
+++ src/mt/tools/turk/Detokenizer.java
@@ -0,0 +1,22 @@
+package mt.tools.turk;
+
+import edu.stanford.nlp.process.PTBTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+/**
+ * Command line utility for detokenizing MT output.
+ *
+ * @author Daniel Cer
+ */
+class Detokenizer {
+  static public void main(String args[]) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    for (String line = reader.readLine(); 
+         line != null; line = reader.readLine()) {
+      String detok = PTBTokenizer.ptb2Text(line);
+      System.out.println(detok);
+    }   
+  }
+}
---
+++ src/mt/tools/turk/MixReferences.java
@@ -0,0 +1,47 @@
+package mt.tools.turk;
+
+import static java.lang.System.*;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/**
+ * Randomly mix together lines from different
+ * reference files
+ *
+ * @author Daniel Cer
+ */
+public class MixReferences {
+  static public void main(String argv[]) throws IOException { 
+    if (argv.length == 0) {
+      out.println("Usage:\n\tjava ...MixReferences (ref0) (ref1) ....\n");
+      exit(-1);
+    }
+    List<List<String>> refs = new ArrayList<List<String>>();
+    for (String filename  : argv) {
+      List<String> reflines = new ArrayList<String>();
+      refs.add(reflines);
+      BufferedReader reader = new BufferedReader(new FileReader(filename));
+      for (String line = reader.readLine(); line != null;
+          line = reader.readLine()) {
+        reflines.add(line);
+      }
+    }
+    for (int i = 0; i < refs.size()-1; i++) {
+      if (refs.get(i).size() != refs.get(i+1).size()) {
+        throw new RuntimeException(
+          String.format("ref file size mismatch %s(%d) vs. %s(%d)\n", 
+          argv[i], refs.get(i).size(), argv[i+1], refs.get(i+1).size()));
+      }
+    } 
+    Random r = new Random(1); // make results repeatable
+    for (int i = 0; i < refs.get(0).size(); i++) {
+      int selectId = r.nextInt(refs.size());
+      out.println(selectId + ": "+refs.get(selectId).get(i));
+    }
+  }
+}
