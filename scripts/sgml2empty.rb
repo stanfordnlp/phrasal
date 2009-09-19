62a9c4bfee41218ca52ac7a71fae0229f507ef8e
spenceg <spenceg@stanford.edu>
2009-09-19 16:06:14 +0000

    Added william's sgml stripping script
    svn path=/trunk/javanlp/; revision=26451

---
+++ scripts/sgml2empty.rb
@@ -0,0 +1,23 @@
+#!/bin/env ruby -w
+
+## quick and dirty! first arg should be the sysid you want extracted,
+## or nothing for all.
+
+sysid = ARGV.shift
+
+punc = '.,"()-'
+skip = false
+
+ARGF.each do |l|
+  case l
+  when /<doc .* sysid="(.*?)"/i
+    skip = true unless sysid.nil? || sysid == $1
+  when /<\/doc>/i
+    skip = false
+  when /<seg id=.*?> *(.*?) *<\/seg>/i
+    next if skip
+    text = $1
+## not sure what this is for---some kind of limited english tokenization, it looks like
+    puts text #.downcase.gsub(/(\S)'s/, '\1 \'s').gsub(/(\S)([#{punc}])/, '\1 \2').gsub(/([#{punc}])(\S)/, '\1 \2').gsub(/\s+/, " ")
+  end
+end
