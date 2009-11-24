b819735ea56276bfe6edd5fe93962057db92c760
spenceg <spenceg@stanford.edu>
2009-11-24 21:56:58 +0000

    Fix build error with change of method name from prob --> logProb.
    svn path=/trunk/javanlp/; revision=27086

--- src/mt/train/discrimdistortion/DistortionModel.java
+++ src/mt/train/discrimdistortion/DistortionModel.java
@@ -10,9 +10,13 @@ import edu.stanford.nlp.util.Pair;
 public class DistortionModel implements Serializable {
 	private static final long serialVersionUID = 8119388926319744131L;
 
-	public static enum Feature { Word, RelPosition, CurrentTag, SourceLen, LeftTag, RightTag }
+	public static enum Feature { Word, RelPosition, CurrentTag, SourceLen, LeftTag, RightTag, ArcTag }
 	public static enum FeatureType { Binary, Real };
+
 	public static enum Class { C1, C2, C3, C4, C5, C6, C7, C8, C9}
+	public static final Class FIRST_CLASS = Class.C1;
+	public static final Class LAST_CLASS = Class.C9;
+	public static final Class MONOTONE = Class.C5;
 
 	public Index<DistortionModel.Feature> featureIndex = null;
 	public Index<DistortionModel.Class> classIndex = null;
@@ -22,6 +26,13 @@ public class DistortionModel implements Serializable {
 	public Map<DistortionModel.Feature,Integer> featureOffsets = null;
 	public Map<DistortionModel.Feature,Integer> featureDimensions = null;
 	public Map<DistortionModel.Feature,DistortionModel.FeatureType> featureTypes = null;
+	
+	public boolean isOutbound = false;
+	
+	public boolean useBeginEndMarkers = false;
+  public String START_OF_SENTENCE = "<S>"; //Some defaults
+  public String END_OF_SENTENCE = "</S>";
+  public String DELIM_POS = "SS";
 
 	//WSGDEBUG - Should make this configurable, eventually
 	//NOTE the stylistic justification: you probably wouldn't use the same word twice
@@ -36,7 +47,7 @@ public class DistortionModel implements Serializable {
 	 * @param isOOV
 	 * @return
 	 */
-	public double prob(Datum datum, DistortionModel.Class thisC, boolean isOOV) {
+	public double logProb(Datum datum, DistortionModel.Class thisC, boolean isOOV) {
 		double[] logScores = new double[DistortionModel.Class.values().length];
 		for(DistortionModel.Class c : DistortionModel.Class.values())
 			logScores[c.ordinal()] = modelScore(datum, c, isOOV);
@@ -100,21 +111,10 @@ public class DistortionModel implements Serializable {
 	public int getFeatureDimension() { return (featureIndex == null) ? 0 : featureIndex.size(); }
 	
 	
-	public static final float[] classRightBounds = {-50.0f,-22.09f,-11.84f,-6.69f,-3.38f,
-	  -1.0f, 0.64f, 3.45f, 8.68f, 32.0f, 65.0f, 100.0f };
-	public static final Class FIRST_CLASS = Class.C1;
-	public static final Class LAST_CLASS = Class.C9;
-	public static final Class MONOTONE = Class.C5;
-//	public static final int NULL_VALUE = -5000;
-	
-	//Expects relative movement as a percentage (e.g., 100%)
 	public static Class discretizeDistortion(int relMovement) {
 	
-//	  if(relMovement == NULL_VALUE)
-//	    return Class.NULL;
-	  
 		//10 class implementation (28 Oct 2009)
-	  if(relMovement <= -7)    //By construction
+	  if(relMovement <= -7)
 	    return Class.C1;
 	  else if(relMovement <= -4)
 	    return Class.C2;
@@ -132,97 +132,6 @@ public class DistortionModel implements Serializable {
 	    return Class.C8;
 	  
 	  return Class.C9;
-		
-//    if (relMovement == 0.0)
-//      return Class.Zero;
-//    
-////    10 class implementation (28 Oct 2009)
-//    if(relMovement < -19.82f)    //By construction
-//      return Class.C1;
-//    if(relMovement < -11.80)
-//      return Class.C2;
-//    else if(relMovement < -7.31f)
-//      return Class.C3;
-//    else if(relMovement < -4.27f)
-//      return Class.C4;
-//    else if(relMovement < -1.94f)
-//      return Class.C5;
-//    else if(relMovement < 0.0f)
-//      return Class.C6;
-//    else if(relMovement < 1.94f)
-//      return Class.C7;
-//    else if(relMovement < 4.27f)
-//      return Class.C8;
-//    else if(relMovement < 7.31f)
-//      return Class.C9;
-//    else if(relMovement < 11.81f)
-//      return Class.C10;
-//    else if(relMovement < 19.82f)  //By construction
-//      return Class.C11;
-//    return Class.C12;
-//		
-				
-       //9 class (this works well except for right movement
-//		if(relMovement < -50.0f) //By construction
-//			return Class.C1;
-//		else if(relMovement < -21.1f)
-//			return Class.C2;
-//		else if(relMovement < -11.0f)
-//			return Class.C3;
-//		else if(relMovement < -5.98f)
-//			return Class.C4;
-//		else if(relMovement < -2.70f)
-//			return Class.C5;
-//		else if(relMovement < -0.22f)
-//			return Class.C6;
-//		else if(relMovement < 1.57f)
-//			return Class.C7;
-//		else if(relMovement < 5.29f)
-//			return Class.C8;
-//		else if(relMovement < 14.9f)
-//			return Class.C9;
-//		else if(relMovement < 50.0f) //By construction
-//			return Class.C10;
-//		return Class.C11;
-		
-		
-		//17 class justification:
-		// 1) 15 bins using the bin selection procedure
-		// 2) 2 construction bins to collect crap. Bounds set empirically
-//		if(relMovement < -50.0f) //By construction
-//			return Class.C1;
-//		else if(relMovement < -29.04f)
-//			return Class.C2;
-//		else if(relMovement < -18.26f)
-//			return Class.C3;
-//		else if(relMovement < -12.42f)
-//			return Class.C4;
-//		else if(relMovement < -8.64f)
-//			return Class.C5;
-//		else if(relMovement < -5.95f)
-//			return Class.C6;
-//		else if(relMovement < -3.86f)
-//			return Class.C7;
-//		else if(relMovement < -2.13f)
-//			return Class.C8;
-//		else if(relMovement < -0.61f)
-//			return Class.C9;
-//		else if(relMovement < 0.19f)
-//			return Class.C10;
-//		else if(relMovement < 1.60f)
-//			return Class.C11;
-//		else if(relMovement < 3.57f)
-//			return Class.C12;
-//		else if(relMovement < 6.45f)
-//			return Class.C13;
-//		else if(relMovement < 11.86f)
-//			return Class.C14;
-//		else if(relMovement < 29.30f)
-//			return Class.C15;
-//		else if(relMovement < 65.0f) //By construction
-//			return Class.C16;
-//		return Class.C17;
-
 	}
 	
 	@Override
