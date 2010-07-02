cf6a7eba19cfc69a27564810d1b362fe88a0002a
mgalley <mgalley@stanford.edu>
2010-07-02 03:41:59 +0000

    fixed copyrights
    svn path=/trunk/javanlp/; revision=30651

--- src/edu/stanford/nlp/mt/syntax/ghkm/RuleExtractor.java
+++ src/edu/stanford/nlp/mt/syntax/ghkm/RuleExtractor.java
@@ -1,29 +1,29 @@
-//Phrasal -- A Statistical Machine Translation Toolkit
-//for Exploring New Model Features.
-//Copyright (c) 2007-2010 Leland Stanford Junior University
-
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either version 2
-//of the License, or (at your option) any later version.
-
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-//For more information, bug reports, fixes, contact:
-//Christopher Manning
-//Dept of Computer Science, Gates 1A
-//Stanford CA 94305-9010
-//USA
-//Support/Questions: java-nlp-user@lists.stanford.edu
-//Licensing: java-nlp-support@lists.stanford.edu
-//http://nlp.stanford.edu/software/phrasal
+// Phrasal -- A Statistical Machine Translation Toolkit
+// for Exploring New Model Features.
+// Copyright (c) 2007-2010 The Board of Trustees of
+// The Leland Stanford Junior University. All Rights Reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// For more information, bug reports, fixes, contact:
+//    Christopher Manning
+//    Dept of Computer Science, Gates 1A
+//    Stanford CA 94305-9010
+//    USA
+//    java-nlp-user@lists.stanford.edu
+//    http://nlp.stanford.edu/software/phrasal
 
 package edu.stanford.nlp.mt.syntax.ghkm;
 
