d1b6cd1acf630306f08cf414a0a043d27b2d68bb
danielcer <danielcer@stanford.edu>
2010-08-31 06:32:42 +0000

    bug fix for when absolute paths are used on Windows (e.g. paths like "C:\users\daniel\mtexperiments")
    svn path=/trunk/javanlp/; revision=31686

--- src/edu/stanford/nlp/mt/base/FactoryUtil.java
+++ src/edu/stanford/nlp/mt/base/FactoryUtil.java
@@ -15,16 +15,14 @@ public class FactoryUtil {
 		Map<String,String> paramPairs = new HashMap<String, String>();
 	
 		for (int i = 1; i < specs.length; i++) {
-			String[] fields = specs[i].split(":");
-			String key, value;
+			String[] fields = specs[i].split(":", 2);
+			String key = null, value = null;
 			if (fields.length == 1) {
 				key = fields[0];
 				value = "";
 			} else if (fields.length == 2) {				
 				key = fields[0];
 				value = fields[1];
-			} else {
-				throw new RuntimeException(String.format("Invalid parameter pair '%s'", specs[i]));
 			}
 			paramPairs.put(key, value);
 		}
