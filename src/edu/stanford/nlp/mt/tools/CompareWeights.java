908c8c842e26984b89caa04b91df83e90b6812b6
danielcer <danielcer@stanford.edu>
2010-09-09 02:29:06 +0000

    a couple more ways to compare weight vectors: sum-square-error and cosine
    svn path=/trunk/javanlp/; revision=31786

--- src/edu/stanford/nlp/mt/tools/CompareWeights.java
+++ src/edu/stanford/nlp/mt/tools/CompareWeights.java
@@ -8,9 +8,12 @@ import java.io.ObjectInputStream;
 import java.util.HashSet;
 import java.util.Set;
 
+import scala.actors.threadpool.Arrays;
+
 
 import edu.stanford.nlp.stats.ClassicCounter;
 import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.stats.Counters;
 
 public class CompareWeights {
    // TODO find a good permanent home for this method
@@ -35,9 +38,30 @@ public class CompareWeights {
       return wts;
    }
    
+   enum CompareType {MAX_COOR_ABS, COSINE, SUM_SQUARE_ERROR};
+   
+   static public void usage() {
+      System.err.println("Usage:\n\tjava ...CompareWeights [-cosine|-sse] wts1 wts2\n\n\tDefault: Returns maximum absolute difference in a single weight value");      
+   }
+   
    static public void main(String[] args) throws Exception {
+      CompareType compareType = CompareType.MAX_COOR_ABS;
+   
+      if (args.length == 0) { 
+         usage();
+         System.exit(-1);
+      }
+      
+      if ("-cosine".equals(args[0])) {
+         compareType = CompareType.COSINE;
+         args = (String[])Arrays.copyOfRange(args, 1, args.length);
+      } else if ("-sse".equals(args[0])) {
+         compareType = CompareType.SUM_SQUARE_ERROR;
+         args = (String[])Arrays.copyOfRange(args, 1, args.length);
+      }
+      
       if (args.length != 2) {
-         System.err.println("Usage:\n\tjava ...CompareWeights wts1 wts2\n\nReturns: maximum absolute difference in a single weight value");
+         usage();         
          System.exit(-1);
       }
       
@@ -46,12 +70,24 @@ public class CompareWeights {
       Counter<String> wts2 = readWeights(args[1]);
       allWeights.addAll(wts1.keySet());
       allWeights.addAll(wts2.keySet());
-      
-      double maxDiff = Double.NEGATIVE_INFINITY;
-      for (String wt : allWeights) {
-         double absDiff = Math.abs(wts1.getCount(wt) - wts2.getCount(wt));
-         if (absDiff > maxDiff) maxDiff = absDiff;
+     
+      if (compareType == CompareType.MAX_COOR_ABS) {         
+         double maxDiff = Double.NEGATIVE_INFINITY;
+         for (String wt : allWeights) {
+            double absDiff = Math.abs(wts1.getCount(wt) - wts2.getCount(wt));
+            if (absDiff > maxDiff) maxDiff = absDiff;
+         }
+         System.out.println(maxDiff);
+      } else if (compareType == CompareType.COSINE){
+         double dotProd = Counters.cosine(wts1, wts2);
+         System.out.println(dotProd);
+      } else if (compareType == CompareType.SUM_SQUARE_ERROR) {
+         double sse = 0;
+         for (String wt : allWeights) {
+            double diff = wts1.getCount(wt) - wts2.getCount(wt);
+            sse += diff*diff;            
+         }
+         System.out.println(sse);
       }
-      System.out.println(maxDiff);
    }
 }
