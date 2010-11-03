95e78ed71adcd4deb85319ca9ca38e7a92af31f9
spenceg <spenceg@stanford.edu>
2010-11-03 23:58:04 +0000

    Removed MERT dependency for weight normalization.
    svn path=/trunk/javanlp/; revision=32574

--- src/edu/stanford/nlp/mt/tools/CompareWeights.java
+++ src/edu/stanford/nlp/mt/tools/CompareWeights.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import edu.stanford.nlp.stats.ClassicCounter;
 import edu.stanford.nlp.stats.Counter;
 import edu.stanford.nlp.stats.Counters;
-import edu.stanford.nlp.mt.tune.MERT;
 
 public class CompareWeights {
   // TODO find a good permanent home for this method
@@ -77,8 +76,8 @@ public class CompareWeights {
 
     if (compareType == CompareType.MAX_COOR_ABS) {
       double maxDiff = Double.NEGATIVE_INFINITY;
-      MERT.normalize(wts1);
-      MERT.normalize(wts2);
+      Counters.multiplyInPlace(wts1, 1.0 / Counters.L1Norm(wts1));
+      Counters.multiplyInPlace(wts2, 1.0 / Counters.L1Norm(wts2));
       for (String wt : allWeights) {
         double absDiff = Math.abs(wts1.getCount(wt) - wts2.getCount(wt));
         if (absDiff > maxDiff)
