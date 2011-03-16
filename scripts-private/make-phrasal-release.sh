a7999852b39d9429c24aafcf3bf64b41517a415e
cerd <cerd@stanford.edu>
2011-03-16 23:54:21 +0000

    changes for offsite builds
    svn path=/trunk/javanlp/; revision=34831

--- scripts-private/make-phrasal-release.sh
+++ scripts-private/make-phrasal-release.sh
@@ -5,6 +5,11 @@ echo "Making Phrasal release tar ball"
 rm -rf phrasal.$1
 mkdir phrasal.$1
 
+mkdir dir src/edu/stanford/nlp/lm
+cp ../more/src/edu/stanford/nlp/lm/* src/edu/stanford/nlp/lm
+
 jar -cf phrasal.$1/phrasal.$1.jar -C classes edu
 cp -r src scripts README.txt LICENSE.txt phrasal.$1
 tar --exclude .svn -czf phrasal.$1.tar.gz phrasal.$1
+
+rm -rf src/edu/stanford/nlp/lm
