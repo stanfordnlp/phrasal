22b7343ffdd752c2392a4c0ed568be9a69d9b6dc
cerd <cerd@stanford.edu>
2011-03-17 02:21:21 +0000

    include user build.xml in phrasal releases
    svn path=/trunk/javanlp/; revision=34840

--- scripts-private/make-phrasal-release.sh
+++ scripts-private/make-phrasal-release.sh
@@ -5,11 +5,14 @@ echo "Making Phrasal release tar ball"
 rm -rf phrasal.$1
 mkdir phrasal.$1
 
-mkdir dir src/edu/stanford/nlp/lm
+
+mkdir src/edu/stanford/nlp/lm
 cp ../more/src/edu/stanford/nlp/lm/* src/edu/stanford/nlp/lm
 
 jar -cf phrasal.$1/phrasal.$1.jar -C classes edu
 cp -r src scripts README.txt LICENSE.txt phrasal.$1
+cp userbuild.xml  phrasal.$1/build.xml
+
 tar --exclude .svn -czf phrasal.$1.tar.gz phrasal.$1
 
 rm -rf src/edu/stanford/nlp/lm
