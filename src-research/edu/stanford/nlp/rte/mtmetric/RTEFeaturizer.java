d8f17b2d9dd2e7a15c586b8aeea8a66ead18ae9c
danielc <danielc@stanford.edu>
2011-04-19 07:36:09 +0000

    RTE pipeline non-verbose by default.
    svn path=/trunk/javanlp/; revision=35600

--- src-research/edu/stanford/nlp/rte/mtmetric/RTEFeaturizer.java
+++ src-research/edu/stanford/nlp/rte/mtmetric/RTEFeaturizer.java
@@ -6,14 +6,27 @@ import edu.stanford.nlp.rte.Global;
 import edu.stanford.nlp.rte.MockProblem;
 import edu.stanford.nlp.rte.NoLearningExperiment;
 import edu.stanford.nlp.rte.RTEPipeline;
+import edu.stanford.nlp.util.Index;
+import edu.stanford.nlp.util.OAIndex;
 import edu.stanford.nlp.util.Timing;
 import edu.stanford.nlp.stats.ClassicCounter;
 import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.stats.CountersRealVectors;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.math.linear.Array2DRowRealMatrix;
+import org.apache.commons.math.linear.ArrayRealVector;
+import org.apache.commons.math.linear.DecompositionSolver;
+import org.apache.commons.math.linear.LUDecompositionImpl;
+import org.apache.commons.math.linear.RealMatrix;
+import org.apache.commons.math.linear.RealVector;
 
 /**
  * 
@@ -82,6 +95,10 @@ class RTEFeaturizer {
     BufferedReader reader = 
       new BufferedReader(new InputStreamReader(System.in));
     PrintStream pstrm = new PrintStream("rte.featurized");
+    List<Counter<String>> dataPts = new ArrayList<Counter<String>>(); 
+    List<Double>  scores = new ArrayList<Double>();
+    Index<String> featureIndex = new OAIndex<String>();
+    
     for (String line = reader.readLine(); line != null; 
          line = reader.readLine()) {
       String[] fields = line.split(" \\|\\|\\| ");
@@ -97,11 +114,62 @@ class RTEFeaturizer {
         results = new ClassicCounter<String>();
       }
       results.incrementCount("SmoothBLEU", BLEUMetric.computeLocalSmoothScore(mt, Arrays.asList(ref), 4));
+      results.incrementCount("biasFeature");
+      
+      dataPts.add(results);
+      scores.add(score);
+      for (String feature : results.keySet()) {
+        featureIndex.add(feature);
+      }
       System.err.printf("Results: %s\n", results);
       pstrm.printf("%s ||| %s ||| %f\n", id, results, score); 
     } 
+    // long term - this really shouldn't be here
+    
     long dur = System.currentTimeMillis() - startTime;
-    System.err.printf("Time: %.3f s\n", dur/1000.);    
+    System.err.printf("RTE Time: %.3f s\n", dur/1000.);
+    
+    RealMatrix A = new Array2DRowRealMatrix(dataPts.size(), featureIndex.size());
+    RealVector b = new ArrayRealVector(dataPts.size());
+    
+    int row = 0;
+    for (Counter<String> datum : dataPts) {
+       for (Map.Entry<String, Double> entry : datum.entrySet()) {
+         int idx = featureIndex.indexOf(entry.getKey());
+         double val = entry.getValue();
+         A.setEntry(row, idx, val);
+         b.setEntry(row, scores.get(row));
+         row++;
+       }
+    }
+    DecompositionSolver s = new LUDecompositionImpl(A).getSolver();
+    RealVector x = s.solve(b);
+    RealVector p = A.operate(x);
+    
+    System.err.println("Linear regression");
+    System.err.println("=================");
+    Counter<String> wts = CountersRealVectors.fromRealVector(x, featureIndex);
+    
+    for (Map.Entry<String, Double> entry : wts.entrySet()) {
+      System.err.printf("%s: %e\n", entry.getKey(), entry.getValue());
+    }
+    System.err.println("=================");
+    
+    row = 0;
+    double sse = 0;
+    double correctSSE = 0;
+    for (Counter<String> datum : dataPts) {
+      double hypScore = 0; 
+      for (Map.Entry<String, Double> entry : datum.entrySet()) {
+        hypScore += wts.getCount(entry.getKey()) * entry.getValue();
+      }
+      double error = hypScore - scores.get(row);
+      System.err.printf("%d: predicted: %d actual: %d\n error: %d\n", hypScore, scores.get(row), error);
+      sse += error*error;
+      correctSSE += (p.getEntry(row) - b.getEntry(row))*(p.getEntry(row) - b.getEntry(row));
+      row++;
+    } 
+    System.err.printf("MSE: %e (MSSE amc: %e)\n", sse/row, correctSSE/row);
   }
 }
 
