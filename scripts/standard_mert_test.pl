5176a17a66be6fe1136c0ac30c7b8b8b65d0627e
cerd <cerd@stanford.edu>
2011-06-08 22:13:47 +0000

    update integration test to account for current (improved) system performance
    svn path=/trunk/javanlp/; revision=36406

--- scripts/standard_mert_test.pl
+++ scripts/standard_mert_test.pl
@@ -3,10 +3,14 @@
 use File::Compare;
 
 $start_time = time;
+
 $date_tag=`date +%Y-%m-%d`; chomp $date_tag;
-`(cd  /u/nlp/data/mt_test/mert/; $ENV{"JAVANLP_HOME"}/projects/mt/scripts/phrasal-mert.pl dev2006.fr.lowercase.h10  dev2006.en.lowercase.h10 bleu base.ini > phrasal-mert.$date_tag.log)`;
+$proc_tag=$ARGV[0];
+
+`rm -rf /u/nlp/data/mt_test/mert/phrasal-mert.$proc_tag`;
+`(cd  /u/nlp/data/mt_test/mert/; $ENV{"JAVANLP_HOME"}/projects/mt/scripts/phrasal-mert.pl --nbest=500 --working-dir=phrasal-mert.$proc_tag dev2006.fr.lowercase.h10 dev2006.en.lowercase.h10 bleu base.ini > phrasal-mert.$proc_tag.$date_tag.log 2>&1)`;
 $total_time = time - $start_time;
-if (compare("/u/nlp/data/mt_test/mert/pmert-dir/phrasal.17.trans", "/u/nlp/data/mt_test/mert/expected-pmert-dir/phrasal.17.trans") == 0) {
+if (compare("/u/nlp/data/mt_test/mert/phrasal-mert.$proc_tag/phrasal.24.trans", "/u/nlp/data/mt_test/mert/expected-pmert-dir/phrasal.24.trans") == 0) {
 	print "Test Success (Time: $total_time s).\n";
 	exit 0;
 } else {
