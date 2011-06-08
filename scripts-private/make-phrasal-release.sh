69c1adf00525be506f35f9ecd8ee2539ceffefe7
cerd <cerd@stanford.edu>
2011-06-08 22:22:28 +0000

    Significanly improved Phrasal distribution build script automatically: 1) checks that repository is up to date, 2) checks that there is no uncommitted changes, 3) checks that offsite users can build the code using the most recent release of the CoreNLP jars, 4) runs decoder and MERT training integration test using released CoreNLP jars, 5) archives distribution under /u/svnroot/branches/phrasal-releases
    svn path=/trunk/javanlp/; revision=36408

--- scripts-private/make-phrasal-release.sh
+++ scripts-private/make-phrasal-release.sh
@@ -2,11 +2,52 @@
 
 echo "Making Phrasal release tar ball"
 
+cd $JAVANLP_HOME
+revHead=`svn info -r HEAD | grep -i "Last Changed Rev"`
+revCheckout=`svn info | grep -i "Last Changed Rev"`
+svnStatus=`svn status`
+cd -
+
+if [ "$revHead" = "$revCheckout" ]; then
+   echo "PASS: Repository checkout is current"
+   echo "$revCheckout"
+   `date`
+else
+   echo "FAIL: Repository checkout is NOT current"
+   echo "$revCheckout != $revHead"
+   echo "Please svn update before making a distribution"
+   exit -1
+fi
+
+if [ "$svnStatus" = "" ]; then
+   echo "PASS: no uncommitted changes detected"
+else
+   echo "FAIL: uncommitted changes detected"
+   echo $svnStatus
+   exit -1
+fi
+
+cd $JAVANLP_HOME
+ant all
+if [ $? = 0 ]; then
+  echo "PASS: repository builds succuessfully"
+else
+  echo "FAIL: repository has build errors"
+  exit -1
+fi 
+cd -
+
+svn info  file:///u/nlp/svnroot/branches/phrasal-releases/$1 >/dev/null 2>&1
+if [ $? = 0 ]; then
+echo "Removing old $1 distribution branch from svn/branches/phrasal-releases"
+svn delete file:///u/nlp/svnroot/branches/phrasal-releases/$1 -m 'remaking Stanford Phrasal distribution $1 (this happens when something went wrong the first time around)'
+fi
+
+echo "Archiving distribution under svnroot/branches/phrasal-releases/$1"
+svn copy file:///u/nlp/svnroot/trunk/javanlp file:///u/nlp/svnroot/branches/phrasal-releases/$1 -m 'release branch for Stanford Phrasal distribution $1'
+
 rm -rf phrasal.$1
 mkdir phrasal.$1
-
-
-mkdir src/edu/stanford/nlp/lm
 cp ../more/src/edu/stanford/nlp/lm/* src/edu/stanford/nlp/lm
 
 cp -r src scripts README.txt LICENSE.txt phrasal.$1
@@ -19,25 +60,41 @@ mkdir `pwd`/phrasal.$1/classes
 mkdir `pwd`/phrasal.$1/lib-nodistrib
 
 export CLASSPATH=.
-CORENLP=`ls -dt /u/nlp/distrib/stanford-corenlp-2011-0*[0-9] | head -1`
+export CORENLP=`ls -dt /u/nlp/distrib/stanford-corenlp-2011-0*[0-9] | head -1`
 
 (cd  phrasal.$1/; ./scripts/first-build.sh all)
+if [ $? = 0 ]; then
+   echo "PASS: User distribution builds successfully"
+else
+   echo "FAIL: User distribution has build errors"
+   exit -1
+fi
+
 jar -cf phrasal.$1/phrasal.$1.jar -C phrasal.$1/classes edu
-export CLASSPATH=$CLASSPATH:`pwd`/phrasal.$1/classes
 
 echo "Running phrasal integration test" 
-/user/cerd/scr/dev/javanlp/projects/mt/scripts/standard_mert_test.pl
+export CLASSPATH=$CLASSPATH:`pwd`/phrasal.$1/classes
+for jarFile in $CORENLP/*.jar; do
+  export CLASSPATH=$CLASSPATH:$jarFile
+done
 
-if [ $? != 0 ]; then
-  echo "Phrasal integration test failed!\n\n"
+/user/cerd/scr/dev/javanlp/projects/mt/scripts/standard_mert_test.pl distro.$1
+
+if [ $? = 0 ]; then
+  echo "PASS: Phrasal integration test"
+else
+  echo "FAIL: Phrasal integration test\n\n"
   echo "Log file in /u/nlp/data/mt_test/mert:\n\n"
   cat `ls -t  /u/nlp/data/mt_test/mert/*.log | head -1`
   exit -1;
+  echo "End of log dump for FAIL: Phrasal integration test"
 fi
 
-rm -rf phrasal.$1/classes/*
+#rm -rf phrasal.$1/classes/*
 rm -rf phrasal.$1/lib-nodistrib/*
 
 tar --exclude .svn -czf phrasal.$1.tar.gz phrasal.$1
 
-rm -rf src/edu/stanford/nlp/lm
+if [ $? = 0]; then
+  echo "SUCCESS: Stanford Phrasal distribution phrasal.$1.tar.gz successfully built"
+fi
