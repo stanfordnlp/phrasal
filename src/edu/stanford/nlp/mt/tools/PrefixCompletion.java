d0db4ae0033b80fe606b2d3be49080ed7a26157b
danielcer <danielcer@stanford.edu>
2011-06-20 21:31:13 +0000

    MT project should not depend on Scala libraries.
    svn path=/trunk/javanlp/; revision=36657

--- src/edu/stanford/nlp/mt/tools/PrefixCompletion.java
+++ src/edu/stanford/nlp/mt/tools/PrefixCompletion.java
@@ -14,11 +14,11 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import scala.actors.threadpool.Arrays;
 
 //import java.io.BufferedReader;
 //import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
