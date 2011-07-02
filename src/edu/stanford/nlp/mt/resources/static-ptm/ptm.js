0147502d2a9f096919b91aa5811d2ea99661c5b1
spenceg <spenceg@stanford.edu>
2011-07-02 22:55:20 +0000

    Re-write of ptm library. Library now conforms to the module design pattern. Changed to json interface.
    svn path=/trunk/javanlp/; revision=36760

--- src/edu/stanford/nlp/mt/resources/static-ptm/ptm.js
+++ src/edu/stanford/nlp/mt/resources/static-ptm/ptm.js
@@ -1,349 +1,207 @@
+// Predictive Translation Memory 
+//
+// Setup using the module pattern:
+// http://addyosmani.com/resources/essentialjsdesignpatterns/book/#designpatternsjavascript
 
-//This is the default jQuery documentReady function
-//that comes up with onLoad()
-$(document).ready(function(){
+(function(window){
+
+  //Register ptm callbacks with the UI when it is ready
+  $(document).ready(function(){
 
     //Configure the autocomplete box, even though it is initially hidden
-    //
     $( "#ptm-input_" ).autocomplete({
       minLength: 2,
       search: function(event,ui){
-//        var source = $("textarea#ptm-input_").val();
-//        var endsWithWhitespace=new RegExp("\\s$");
-//        console.log("Ends with whitespace:" + endsWithWhitespace.test(source));  
-//        return endsWithWhitespace.test(source);
-          return true;
+        var source = $("textarea#ptm-input_").val();
+        var endsWithWhitespace=new RegExp("\\s$");
+        console.log("Ends with whitespace:" + endsWithWhitespace.test(source));  
+        return endsWithWhitespace.test(source);
       },
       
       source: function(request,response){
-        usrHandler.autoCompleteReq(request,response);
+        ptm.autoCompleteReq(request,response);
       },
       
       select: function( event, ui ) {
         
         console.log( "Selected:" + ui.item);
         if(ui.item){
-          usrHandler.autoCompleteSelect(ui.item.label);
+          ptm.autoCompleteSelect(ui.item.label);
         }
       },
     });
     
-    //Configure the handler for completing translation
-    //
-    $( '#form-ptm-input_' ).submit(function(event){
-      event.preventDefault();
-      usrHandler.doneWithTranslation();
-      return false;
-    });
-
     //Configure the form that will initiate translation
-    //
+    //Suppress the form POST
     $('#lang-form_').submit(function(event){
       event.preventDefault();
-      usrHandler.initTranslation();
+      ptm.initTranslation();
+      return false;
+    });   
+
+    //Configure the handler for completing translation
+    //Suppress the form POST
+    $( '#form-ptm-input_' ).submit(function(event){
+      event.preventDefault();
+      ptm.doneWithTranslation();
       return false;
     });
-    
-});
 
-var autoResp;
-
-var usrHandler = {
-  initTranslation: function(){
-
-    var sourceLang = $("select#src-list_ option:selected").val();
-    var targetLang = $("select#tgt-list_").val();
-    var source = $("textarea#src-input_").val();
-    
-    var ptmMsg = new ptmInitC(sourceLang,targetLang,source);
-    console.log("ptmInit");
-    console.log(ptmMsg);
-    var msg = {
-      ptmInit: JSON.stringify(ptmMsg),
-    };
-    
-    console.log(msg);
-    
-    $.ajax({
-          url: "http://jack.stanford.edu:8080",
-          dataType: "jsonp",
-          data: msg,
-    });
-      
-  },
-  
-  autoCompleteReq: function(request,response){
-  
-    var sourceLang = $("select#src-list_ option:selected").val();
-    var targetLang = $("select#tgt-list_").val();
-    var source = $("textarea#src-input_").val();
-    
-    var ptmMsg = new ptmPredictC(sourceLang,targetLang,source,request.term);
-    console.log("ptmPredict");
-    console.log(ptmMsg);
-    var msg = {
-      ptmPredict: JSON.stringify(ptmMsg),
-    };
-    console.log(msg);
-    
-    $.ajax({
-          url: "http://jack.stanford.edu:8080",
-          dataType: "jsonp",
-          data: msg,
-//          success: function(data){
-//            console.log("Autocomplete returned from server");
-//            console.log(data);
-//            response( $.map( data, function( item ) {
-//              return {
-//                label: item.name,
-//                value: item.name
-//              }
-//            }));
-//          },
-    });
-    
-    autoResp = response;
-  },
-  
-  autoCompleteSelect: function(completion){
-    console.log("autoCompleteSelect");
-    console.log("Completion:" + completion);
-
-    $("textarea#ptm-input_").val(completion);
-    
-    var sourceLang = $("select#src-list_ option:selected").val();
-    var targetLang = $("select#tgt-list_").val();
-    var source = $("textarea#src-input_").val();
-    
-    var ptmMsg = new ptmUserSelectionC(sourceLang,targetLang,source,completion);
-
-    console.log("ptmUserSelection");
-    console.log(ptmMsg);
-    var msg = {
-      ptmUserSelection: JSON.stringify(ptmMsg),
-    };
-    console.log(msg);
-    
-    $.ajax({
-          url: "http://jack.stanford.edu:8080",
-          dataType: "jsonp",
-          data: msg,
-    });
-    
-    //TODO: Remove
-    fn(jump);
-  },
-  
-  doneWithTranslation: function(){
-    var sourceLang = $("select#src-list_ option:selected").val();
-    var targetLang = $("select#tgt-list_").val();
-    var source = $("textarea#src-input_").val();
-    var tgt =  $("textarea#ptm-input_").val();
-    
-    var ptmMsg = new ptmDoneC(sourceLang,targetLang,source,tgt);
-    console.log("ptmDone");
-    console.log(ptmMsg);
-    var msg = {
-      ptmDone: JSON.stringify(ptmMsg),
-    };
-    console.log(msg);
-    
-    $.ajax({
-          url: "http://jack.stanford.edu:8080",
-          dataType: "jsonp",
-          data: msg,
-    });
-  },
-};
-
-//
-//
-// UI Widgets
-
-
-
-//////////////////////
-// PTM jsonp x-domain calls. These handlers will be executed upon receipt from
-// the jetty server.
-//////////////////////
-
-
-//
-// Returns the 1-best translation
-//
-function ptmInitResponse(data){
-  console.log(data);
-  
-  //Clear what's in there now.
-  $("#oov-input_").html('');
-  
-  //Populate the OOV list dynamically
-  for(var i in data.OOVs){
-    var htmlStr = '<div class=\"oov-pair\">';
-    htmlStr = htmlStr + '<form class="form-oov" name="form-oov-' + i + '">';
-    htmlStr = htmlStr + '<span class="oov-src-text" id="form-oov-src-' + i +  '">' + data.OOVs[i] + '</span>';
-    htmlStr = htmlStr + '<input type="text" class="oov-tgt" />';
-    htmlStr = htmlStr + '<input type="submit" value="X" />'
-    htmlStr = htmlStr + '</form></div>';
-    
-    console.log(htmlStr);
-    $("#oov-input_").append(htmlStr);
-  }
-  
-  $('.form-oov').submit(function(event){
-    event.preventDefault();
-    $(this).slideUp();
-    
-    //TODO: Send the OOV messages
-    
-    //Open the PTM pane when ready
-    //TODO: Weird...this should be 0?
-    console.log($('.form-oov:visible').length);
-    if ($('.form-oov:visible').length === 1){
-      console.log('In here');
-      $('#ptm_').show();
-    }
-    
-    return false;
   });
-};
 
-//
-// ACK response to ptmOOV
-//
-function ptmOOVResponse(data){
-  console.log("OOV response:" + data);
-};
 
+// The main ptm namespace
 //
-//
-//
-function ptmPredictResponse(data){
-  console.log("ptmPredictResponse");
-  var sourceArr = [];
-  for(var i in data.predictions){
-    var item = {
-      label: data.prefix + ' ' + data.predictions[i],
-      value: data.prefix,
-    };
-    sourceArr.push(item);
-    console.log(item);
-  }
+var ptm = (function() {
   
-//  console.log(sourceArr);
-//  console.log(autoResp);
-  autoResp(sourceArr);
-};
-
-//
-// 
-//
-function ptmUserSelectionResponse(data){
-  //Send out which one was selected
-};
-
-//
-//
-//
-function ptmDoneResponse(data){
-  //Notify server that the translation is finished
-};
-
-
-////////////////////////
-// PTM messages
-////////////////////////
-
-//
-// Requests a 1-best translation from the server
-//
-function ptmInitC(sourceLang,targetLang,source){
-  this.sourceLang = sourceLang;
-  this.targetLang = targetLang;
-  this.source = source;
-};
-
-//
-// Sends a user-solicited phrase pair to the server
-//
-function ptmOOVC(sourceLang,targetLang,sourcePhrase,targetPhrase){
-  this.sourceLang = sourceLang;
-  this.targetLang = targetLang;
-  this.sourcePhrase = sourcePhrase;
-  this.targetPhrase = targetPhrase;
-};
-
-//
-// Message that feeds the autocomplete with MT completions
-//
-function ptmPredictC(sourceLang,targetLang,source,prefix){
-  this.sourceLang = sourceLang;
-  this.targetLang = targetLang;
-  this.source = source;
-  this.prefix = prefix;
-};
-
-//
-// Indicates an autocomplete selection by the user
-//
-function ptmUserSelectionC(sourceLang,targetLang,source,prefix){
-  this.sourceLang = sourceLang;
-  this.targetLang = targetLang;
-  this.source = source;
-  this.prefix = prefix;
-//  this.completion = completion;
-};
-
-//
-// User has completed translation. This is a new reference
-//
-function ptmDoneC(sourceLang,targetLang,source,finishedTarget){
-  this.sourceLang = sourceLang;
-  this.targetLang = targetLang;
-  this.source = source;
-  this.finishedTarget = finishedTarget;
-};
-
-
-
-///////////////////
-// Old Deprecated stuff for reference
-///////////////////
-
-function someFunc(data){
-  console.log(data);
-};
-
-var oneBest = {
-
-  processSrc: function() {
-    var requestJSON = {
-//      sourceLang: $("select#src-list_ option:selected").val(),
-//      targetLang: $("select#tgt-list_").val(),
-      source: $("textarea#src-input_").val(),
-      prefix: "1 2"
-    };
-    console.log(requestJSON);
-    //TODO: The AJAX call here
-    $.ajax({
-          url: "http://jack.stanford.edu:8080",
-          dataType: "jsonp",
-          data: requestJSON,
-//wsg: For x-domain requests, use jsonp, which will be executed upon
-//     receipt. For dataType: json, register a success() handler.
-//          success: function(data) {
-//              alert('Handler');
-//              console.log(data);
-//              oneBest.processMtResult(data);
-//          }
-//         error: function(jqxhr, textstatus, errorThrown) {
-//           console.log(textstatus);
-//           console.log(errorThrown);
-//         }
-        });
-  },
+  var serverURL = "http://jack.stanford.edu:8080";
   
-  processMtResult: function(data) {
+  //Initialize any variables here
+  var ui = {
+    srcLang: function(){ return $("select#src-list_ option:selected").val(); },
+    tgtLang: function(){ return $("select#tgt-list_").val(); },
+    src: function(){     return $("textarea#src-input_").val(); },
+    tgt: function(){     return $("textarea#ptm-input_").val(); },
+  };
   
-  }
-};
+  //Callbacks from the server to render data to the interface
+  var serveData = {
+    //TODO: Want to remove the CSS/HTML from here...but how?
+    oovResponse: function(data) {
+      console.log("oovResponse");
+      console.log(data);
+  
+      //Clear what's in there now.
+      $("#oov-input_").html('');
+      
+      //Populate the OOV list dynamically
+      for(var i in data.OOVs){
+        var htmlStr = '<div class=\"oov-pair\">';
+        htmlStr = htmlStr + '<form class="form-oov" name="form-oov-' + i + '">';
+        htmlStr = htmlStr + '<span class="oov-src-text" id="form-oov-src-' + i +  '">' + data.OOVs[i] + '</span>';
+        htmlStr = htmlStr + '<input type="text" class="oov-tgt" />';
+        htmlStr = htmlStr + '<input type="submit" value="X" />'
+        htmlStr = htmlStr + '</form></div>';
+        
+        console.log(htmlStr);
+        $("#oov-input_").append(htmlStr);
+      }
+      
+      $('.form-oov').submit(function(event){
+        event.preventDefault();
+        $(this).slideUp();
+        
+        //TODO: Send the OOV messages
+        
+        //TODO: Move this into the UI functionality
+        console.log($('.form-oov:visible').length);
+        if ($('.form-oov:visible').length === 1){
+          console.log('In here');
+          $('#ptm_').show();
+        }
+      });
+    },
+  };
+  
+  
+  //Callbacks to be registered with the UI/client
+  //
+  //
+  var fn = {
+    //Initializes translation from the interface
+    initTranslation: function(){
+
+      var ptmMsg = {
+        sourceLang: ui.srcLang(),
+        targetLang: ui.tgtLang(),
+        source: ui.src(),
+      };
+      
+      console.log("POST: ptmInit");
+      console.log(ptmMsg);
+      console.log(serverURL);
+      
+      $.ajax({
+            url: serverURL,
+            dataType: "json",
+            data: {ptmInit: JSON.stringify(ptmMsg), },
+            success: serveData.oovResponse,
+      });     
+    },
+    
+    //User request for an autocomplete
+    autoCompleteReq: function(request,response){
+  
+      var ptmMsg = {
+        sourceLang: ui.srcLang(),
+        targetLang: ui.tgtLang(),
+        source: ui.src(),
+      };
+      console.log("POST: ptmPredict");
+      console.log(ptmMsg);
+    
+      //Register the callback here since we'll send it directly
+      //to the autocomplete box
+      $.ajax({
+            url: serverURL,
+            dataType: "json",
+            data: { ptmPredict: JSON.stringify(ptmMsg), },
+            success: function(data){
+              response( $.map( data, function( item ) {
+                return { label: item.name, value: item.name }
+              }));
+            },
+      });
+    },
+        
+    //When the user selects a completion
+    autoCompleteSelect: function(completion){
+
+      //Set the autocomplete box on the interface
+      //TODO: Move this into a separate interface component      
+      $("textarea#ptm-input_").val(completion);
+      
+      var ptmMsg = {
+        sourceLang: ui.srcLang(),
+        targetLang: ui.tgtLang(),
+        source: ui.src(),
+      };
+      console.log("POST: ptmUserSelection");
+      console.log(ptmMsg);
+      
+      $.ajax({
+            url: serverURL,
+            dataType: "json",
+            data: {ptmUserSelection: JSON.stringify(ptmMsg),},
+      });
+    },
+    
+    //User has finished the translation.
+    //Submit to server
+    doneWithTranslation: function(){
+      
+      var ptmMsg = {
+        sourceLang: ui.srcLang(),
+        targetLang: ui.tgtLang(),
+        source: ui.src(),
+        finishedTarget: ui.tgt(),
+      };
+      console.log("POST: ptmDone");
+      console.log(ptmMsg);
+      
+      $.ajax({
+            url: serverURL,
+            dataType: "json",
+            data: { ptmDone: JSON.stringify(ptmMsg), },
+      });
+    },
+    
+  };
+  
+  return fn;
+})();
+
+//Map ptm to the global namespace
+window.ptm = ptm;
+
+})(window);
