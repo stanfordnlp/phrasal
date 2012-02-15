ccee76f721f673f3ead0ae7742ffec4e43856de5
spenceg <spenceg@stanford.edu>
2012-02-15 00:57:50 +0000

    Added script for extracting an unfiltered phrase table using moses.
    svn path=/trunk/javanlp/; revision=39144

---
+++ scripts/unfiltered-ptable.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Extracts an unfiltered phrase table using Moses
+#
+#
+# Author: Spence Green
+#
+MOSES=/u/nlp/packages/moses
+TRAIN=$MOSES/scripts/training/train-model.perl 
+ALIGN=grow-diag
+
+echo $TRAIN
+
+# Extract ptable:
+perl $TRAIN --root-dir . --f ar --e en --corpus corpus --factor-delimiter="|||" --alignment $ALIGN --alignment-file corpus --first-step 4 --last-step 7 --reordering=distance,msd-bidirectional-fe 
