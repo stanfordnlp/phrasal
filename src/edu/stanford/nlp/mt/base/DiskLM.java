fd1d53ba905c752eac54d291909306fcb8c72860
cerd <cerd@stanford.edu>
2012-03-22 09:00:39 +0000

    x15 decoding (/translation-time) speed improvement over JDBM key value store using custom backend. If we add in memory caching of frequent n-grams, this might be getting fast enough that it would actually be usable for experiments.
    svn path=/trunk/javanlp/; revision=39462

--- src/edu/stanford/nlp/mt/base/DiskLM.java
+++ src/edu/stanford/nlp/mt/base/DiskLM.java
@@ -6,30 +6,72 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
+import java.util.BitSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.zip.GZIPInputStream;
-
-import net.kotek.jdbm.*;
 
 public class DiskLM implements LanguageModel<IString> {
   public static final IString START_TOKEN = new IString("<s>");
   public static final IString END_TOKEN = new IString("</s>");
   public static final IString UNK_TOKEN = new IString("<unk>");
   protected final String name;
-  protected final Map<Integer,Integer> ngramOrder;
-  protected final Map<String, ProbBowPair> ngramProbBows;
-  protected final DB db; 
+  //protected final Map<Integer,Integer> ngramOrder;
+  //protected final Map<String, ProbBowPair> ngramProbBows;
+  protected final RandomAccessFile lmfh;
+  protected final long entries;
   protected final int order;
   static public final boolean verbose = false;
+    
+  protected final BitSet usedPositions;
+  public static final long MAGIC_NUMBER = 0xDA11CE4111L;
   
-  public DiskLM(String filename) {
+  // Header: 
+  //   magic number (long)  (8)
+  //   version (int)        (4)
+  //   order   (int)        (4)
+  //   Entries (long)       (8)
+  //
+  // Array of []
+  //    extended hashCode  : long  (8)
+  //    ptrSymbol : long (8)
+  //   float : prob     (4)
+  //   float : bow      (4)
+  //                    24
+  // Symbols
+  //
+  // UTF-8 encoded strings
+  // ....
+  
+  public DiskLM(String filename) throws IOException {
+    lmfh = new RandomAccessFile(filename, "r");
+    long magicNumber = lmfh.readLong();
+    if (magicNumber != MAGIC_NUMBER) {
+      throw new RuntimeException(String.format("Bad LM (magic number %x != %x)\n", magicNumber, MAGIC_NUMBER));  
+    }
+    int version = lmfh.readInt(); 
+    if (version != 1) throw new RuntimeException(String.format("Unknown version: %d != 1\n", version));
+    order = lmfh.readInt();
+    entries = lmfh.readLong();
+
+    usedPositions = new BitSet((int)entries); // todo - this will break on LMs with more than 2 billion entries
     this.name = String.format("DiskLM(%s)", filename);
-    db = new DBMaker(filename).build();
-    ngramOrder = db.getHashMap("ngramOrder");
-    ngramProbBows = db.getHashMap("ngramProbBows");
-    order = ngramProbBows.keySet().size();
+    
+    System.err.printf("Initializing %s\n", filename);
+    long setPositions = 0;
+    for (int i = 0; i < entries; i++) {
+      lmfh.seek(24L*i + 24);
+      long extendedHashCode = lmfh.readLong();
+      if (extendedHashCode > 0) {
+        usedPositions.set(i);
+        setPositions++;
+      }
+    }
+
+    System.err.printf("Done (%s, non-zero entries: %d).\n", filename, setPositions);
   }
   
   @Override
@@ -52,6 +94,17 @@ public class DiskLM implements LanguageModel<IString> {
     return order;
   }
 
+  // Use the same algorithm as String.hashCode(), 
+  // but with 64bit longs rather than an 32bit ints
+  public static long extendedHashCode(String string) {
+    long hashCode = 1;
+    int strLength = string.length();
+    for (int i = 0; i < strLength; i++) {
+      hashCode = 31*hashCode + string.charAt(i);
+    }
+    return (hashCode >= 0 ? hashCode : -hashCode);
+  }
+  
   @Override
   public double score(Sequence<IString> sequence) {
     if (ARPALanguageModel.isBoundaryWord(sequence))
@@ -73,6 +126,59 @@ public class DiskLM implements LanguageModel<IString> {
     return score;
   }
 
+  public static long getPutPosition(RandomAccessFile rfh, String entry, long entries) throws IOException {
+    long extendedHashCode = extendedHashCode(entry);
+    long startIdx = extendedHashCode % entries;
+    for (long i = 0; i < entries; i++) {
+      long checkPosition = (i+startIdx) % entries;
+      long seekPos = 24*checkPosition+24;
+      //System.err.printf("extenedHashCode: %d startIdx %d seekPos: %d\n", extendedHashCode, startIdx, seekPos);
+      rfh.seek(seekPos);
+      long entryHash = rfh.readLong();
+      if (entryHash == 0 || entryHash == extendedHashCode) return checkPosition;
+    }
+    return -1;
+  }
+  
+  synchronized public ProbBowPair getProbBow(String entry)  {
+    try {
+    long extendedHashCode = extendedHashCode(entry);
+    long startIdx = extendedHashCode % entries;
+    
+    for (long i = 0; i < entries; i++) {
+       long checkPosition = (i+startIdx) % entries;
+       if (!usedPositions.get((int)checkPosition)) return null;
+       lmfh.seek(24*checkPosition+24);
+       long entryHash = lmfh.readLong();
+       
+       if (entryHash == 0) throw new RuntimeException();
+       if (entryHash != extendedHashCode) continue;
+       
+       if (verbose) {
+         System.err.printf("hash entry(%b:%s): %s (%s)\n", usedPositions.get((int)checkPosition), checkPosition, entryHash, extendedHashCode);
+       }
+       long symbolPosition = lmfh.readLong();
+       
+       float prob = lmfh.readFloat();
+       float bow = lmfh.readFloat();
+       
+       if (verbose) {
+         System.err.printf("symbol pos: %d\n", symbolPosition);
+       }
+       lmfh.seek(symbolPosition);
+       String s = lmfh.readUTF();
+       if (verbose) {
+         System.err.printf("found %s  (%s)", s, entry);
+       }
+       if (s.equals(entry)) {
+         return new ProbBowPair(prob, bow);
+       }
+    }
+    return null;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
   /**
    * 
    * From CMU language model headers:
@@ -90,7 +196,9 @@ public class DiskLM implements LanguageModel<IString> {
     if (verbose) {
       System.err.printf("Looking up %s\n", ngram);
     }
-    ProbBowPair pbp = ngramProbBows.get(ngram);
+    
+    ProbBowPair pbp = getProbBow(ngram);
+   
     if (pbp != null) { // match found
       if (verbose)
         System.err.printf("EM: scoreR: seq: %s logp: %f\n", ngram, pbp.p);
@@ -103,7 +211,7 @@ public class DiskLM implements LanguageModel<IString> {
     }
     int rnxtSpace = ngram.lastIndexOf(" ");
     String prefix = ngram.substring(0, rnxtSpace);
-    ProbBowPair prefixPbp = ngramProbBows.get(prefix);
+    ProbBowPair prefixPbp = getProbBow(prefix);
     double bow;
     if (prefixPbp == null) {
       bow = 0.0;
@@ -122,7 +230,7 @@ public class DiskLM implements LanguageModel<IString> {
   
   @Override
   public boolean releventPrefix(Sequence<IString> sequence) {
-    return ngramProbBows.containsKey(sequence.toString(" "));
+    return getProbBow(sequence.toString(" ")) != null;
   }
   
   
@@ -133,10 +241,7 @@ public class DiskLM implements LanguageModel<IString> {
       diskLMf.delete();
     }
     
-    DB db = new DBMaker(diskLM).build();
-    
-    Map<Integer,Integer> ngramOrder = db.createHashMap("ngramOrder");
-    Map<String, ProbBowPair> ngramProbBows = db.createHashMap("ngramProbBows");
+    Map<Integer,Integer> ngramOrder = new HashMap<Integer,Integer>();
     
     LineNumberReader reader = (arpaLM.endsWith(".gz") ? new LineNumberReader(
         new InputStreamReader(
@@ -150,6 +255,7 @@ public class DiskLM implements LanguageModel<IString> {
     
     String inline;
     int maxOrder = 0;
+    int totalNgrams = 0;
     while ((inline = ARPALanguageModel.readLineNonNull(reader)).startsWith("ngram")) {
       inline = inline.replaceFirst("ngram\\s+", "");
       String[] fields = inline.split("=");
@@ -157,11 +263,22 @@ public class DiskLM implements LanguageModel<IString> {
       int ngramOrderCnt = Integer.parseInt(fields[1].replaceFirst(
           "[^0-9].*$", ""));
       ngramOrder.put(ngramOrderN, ngramOrderCnt);
+      totalNgrams += ngramOrderCnt;
       maxOrder++;
     }
 
     float log10LogConstant = (float) Math.log(10);
 
+    RandomAccessFile lmfh = new RandomAccessFile(diskLM, "rw");
+    lmfh.writeLong(MAGIC_NUMBER);
+    lmfh.writeInt(1); 
+    lmfh.writeInt(ngramOrder.size());
+    lmfh.writeLong(totalNgrams*3L);
+    
+    if (totalNgrams*3L > Integer.MAX_VALUE) throw new RuntimeException();
+
+    long symbolTablePos = 24 + totalNgrams*3L*24;
+    lmfh.setLength(symbolTablePos);
     // read in the n-gram tables one by one
     for (int order = 0; order < maxOrder; order++) {
       System.err.printf("Reading %d %d-grams...\n", ngramOrder.get(order+1),
@@ -171,7 +288,6 @@ public class DiskLM implements LanguageModel<IString> {
       // skip all material upto the next n-gram table header
       while (!ARPALanguageModel.readLineNonNull(reader).startsWith(nextOrderHeader)) {
       }
-      
       // read in table
       while (!(inline = ARPALanguageModel.readLineNonNull(reader)).equals("")) {
         // during profiling, 'split' turned out to be a bottle neck
@@ -191,12 +307,18 @@ public class DiskLM implements LanguageModel<IString> {
         float bow = (tok.hasMoreElements() ? Float.parseFloat(tok.nextToken())
             * log10LogConstant : 0);
         
-        ProbBowPair pbp = new ProbBowPair(prob, bow);
-        ngramProbBows.put(ngram,  pbp);
+        long pos = getPutPosition(lmfh, ngram, totalNgrams*3); 
+        lmfh.seek(24*pos + 24);
+        lmfh.writeLong(extendedHashCode(ngram));
+        lmfh.writeLong(symbolTablePos);
+        lmfh.writeFloat(prob);
+        lmfh.writeFloat(bow);
+        lmfh.seek(symbolTablePos);
+        lmfh.writeUTF(ngram);
+        symbolTablePos = lmfh.getFilePointer();
       }
     }
-    db.commit();
-    db.close();
+    lmfh.close();
   }
   
   public static void main(String[] args) throws IOException {
@@ -211,8 +333,7 @@ public class DiskLM implements LanguageModel<IString> {
   }
 }
 
-class ProbBowPair implements Serializable {
-  private static final long serialVersionUID = 1L;
+class ProbBowPair {
   public final float p;
   public final float bow;
   public ProbBowPair(float p, float bow) {
