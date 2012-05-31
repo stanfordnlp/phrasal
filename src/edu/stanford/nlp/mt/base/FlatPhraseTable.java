88b8ef1d72737336216390bc021deab62355ae35
cerd <cerd@stanford.edu>
2012-05-31 22:44:20 +0000

    treat scores <= 0 as log probabilites (this change was accidently included in the last commit r40347. I just touched the file, so that this change would be explicity mentioned in the svn commit log)
    svn path=/trunk/javanlp/; revision=40349

--- src/edu/stanford/nlp/mt/base/FlatPhraseTable.java
+++ src/edu/stanford/nlp/mt/base/FlatPhraseTable.java
@@ -87,7 +87,9 @@ public class FlatPhraseTable<FV> extends AbstractPhraseGenerator<IString, FV>
         throw new RuntimeException(String.format(
             "Bad phrase table. %s parses as (float) %f", s, f));
       }
+
       float newF =  (f <= 0 ? f : (float) Math.log(f));
+
       if (Float.isNaN(newF)) {
         throw new RuntimeException(String.format(
             "Bad phrase table. %s parses as (float) %f", s, newF));
