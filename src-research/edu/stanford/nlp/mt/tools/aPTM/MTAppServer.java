06dd75496eab6f26d73bc9283f6dbd987099e708
manning <manning@stanford.edu>
2012-07-16 17:44:28 +0000

    Our original StringUtils not Berkeley's!
    svn path=/trunk/javanlp/; revision=40950

--- src-research/edu/stanford/nlp/mt/tools/aPTM/MTAppServer.java
+++ src-research/edu/stanford/nlp/mt/tools/aPTM/MTAppServer.java
@@ -11,36 +11,40 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 
-import edu.berkeley.nlp.util.StringUtils;
+import edu.stanford.nlp.util.StringUtils;
 
 /**
  * Embedded servlet loader for the MT webapp.
  * <p>
  * Implements the Cross-Origin Resource Sharing specification.
- * 
- * @author Spence Green
  *
+ * @author Spence Green
  */
 public class MTAppServer {
 
   public static final String DEBUG_PROPERTY = MTAppServer.class.getSimpleName();
   public static final boolean DEBUG = Boolean.parseBoolean(System.getProperty(
       DEBUG_PROPERTY, "false"));
-  
+
   private static int HTTP_PORT = 8017;
-  
+
   private static boolean MOCK_SERVER = false;
-  
+
   // CORS filter settings
   private static String ALLOWED_URIS = "http://127.0.0.1:8000";
   private static final String ALLOWED_METHODS = "*";
   private static final String ALLOWED_CREDENTIALS = "true";
-  
+
   // MT System ini file
   private static String MT_INI = null;
   private static String MT_WA_MODEL = null;
 
   private static final Map<String,Integer> optionArgDefs = new HashMap<String,Integer>();
+
+
+  private MTAppServer() {} // static
+
+
   static {
     optionArgDefs.put("-p", 1);
     optionArgDefs.put("-u", 1);
@@ -48,18 +52,17 @@ public class MTAppServer {
     optionArgDefs.put("-d", 0);
     optionArgDefs.put("-w", 1);
   }
-  
+
   private static void setOptions(String[] args) {
     final Map<String,String[]> opts = StringUtils.argsToMap(args, optionArgDefs);
     for (String key : opts.keySet()) {
       if (key == null) {
         // Parsed options
-        continue;
       } else if (key.equals("-p")) {
         HTTP_PORT = Integer.valueOf(opts.get(key)[0]);
       } else if (key.equals("-u")) {
         ALLOWED_URIS = opts.get(key)[0];
-      } else if (key.equals("-i")) { 
+      } else if (key.equals("-i")) {
         MT_INI = opts.get(key)[0];
       } else if (key.equals("-w")) {
         MT_WA_MODEL = opts.get(key)[0];
@@ -71,16 +74,16 @@ public class MTAppServer {
       }
     }
   }
-  
+
   private static String usage() {
     StringBuilder sb = new StringBuilder();
     sb.append(String.format("Usage: java %s [OPTS]%n", MTAppServer.class.getName()));
     return sb.toString();
   }
-  
+
   /**
    * Setup the servlet context. This context includes a global CORS filter.
-   * 
+   *
    * @return
    */
   private static ServletContextHandler getContext() {
@@ -102,7 +105,7 @@ public class MTAppServer {
       System.exit(-1);
     }
     setOptions(args);
-    
+
     Server server = new Server(HTTP_PORT);
     ServletContextHandler context = getContext();
     server.setHandler(context);
@@ -111,9 +114,9 @@ public class MTAppServer {
     // TODO(spenceg): If we add more than one, will need to put the servlets on different URIs
     if (MOCK_SERVER) {
       // Add mock servlets here
-      context.addServlet(new ServletHolder(new PhrasalUnifiedServletMock()),"/*");          
+      context.addServlet(new ServletHolder(new PhrasalUnifiedServletMock()),"/*");
     } else {
-      context.addServlet(new ServletHolder(new PhrasalUnifiedServlet(MT_INI, MT_WA_MODEL)),"/*");          
+      context.addServlet(new ServletHolder(new PhrasalUnifiedServlet(MT_INI, MT_WA_MODEL)),"/*");
     }
 
     try {
