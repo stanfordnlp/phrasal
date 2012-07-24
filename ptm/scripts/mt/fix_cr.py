3b2330eb182c47de5f663978d7ed6374d3b768f5
spenceg <spenceg@stanford.edu>
2012-07-24 18:35:22 +0000

    Add whitespace normalization
    svn path=/trunk/javanlp/; revision=41043

--- ptm/scripts/mt/fix_cr.py
+++ ptm/scripts/mt/fix_cr.py
@@ -3,11 +3,17 @@
 # Normalizes EOL character for the current platform.
 #
 import sys
+import re
 import codecs
 
 sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
+# Match various forms of whitespace. Normalize to
+# a single ASCII space character.
+p_ws = re.compile(ur'[ \t\u00A0\u2000-\u200A\u3000]+')
+
 for line in sys.stdin:
-    print line.strip()
+    line = p_ws.sub(' ', line.strip())
+    print line
 
