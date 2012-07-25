dc55dab8efd56843ac85c124b7bf29f876e469cf
spenceg <spenceg@stanford.edu>
2012-07-25 18:30:08 +0000

    Strip control characters. Tokenize each file separately for debugging purposes.
    svn path=/trunk/javanlp/; revision=41058

--- ptm/scripts/mt/tok_google.sh
+++ ptm/scripts/mt/tok_google.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+#
+# Wrapper for the Google tokenizer that ships
+# with the LDC Google 1T European n-grams corpus.
+#
+#
 if [ $# -lt 2 ]; then
   echo Usage: `basename $0` lang file [files]
 	echo 
@@ -9,13 +13,17 @@ fi
 
 lang=$1
 shift
-outfile=corpus."$lang".tok
 
-scriptdir=/u/spenceg/javanlp/projects/mt/ptm/scripts/mt
+#scriptdir=/u/spenceg/javanlp/projects/mt/ptm/scripts/mt
+scriptdir=/home/rayder441/sandbox/javanlp/projects/mt/ptm/scripts/mt
 fixnl="$scriptdir"/fix_cr.py
 tokenizer="$scriptdir/tokenizer.pl --datafile=$scriptdir/tokenizer.data"
 
-# Normalize newlines for the current platform, tokenize,
-# and lowercase.
-cat $* | $fixnl | $tokenizer --language="$lang" | tr A-Z a-z > $outfile
+# Normalize newlines and whitespace for the current platform,
+# strip control characters, tokenize, and lowercase.
+for infile in $*
+do
+    outfile=`basename "$infile"`
+    cat $infile | sed -e 's/[[:cntrl:]]/ /g' | $fixnl | $tokenizer --language="$lang" | tr A-Z a-z > ${outfile}.tok
+done
 
--- ptm/scripts/mt/tok_stanford.sh
+++ ptm/scripts/mt/tok_stanford.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 #
 # Tokenize English and Arabic using appropriate
-# packages from JavaNLP
+# packages from Stanford JavaNLP.
 #
 
 if [ $# -lt 2 ]; then
-	echo Usage: `basename $0` language file [files]
-	echo
-	echo lang = Arabic,English
-	exit -1
+    echo Usage: `basename $0` language file [files]
+    echo
+    echo lang = Arabic,English
+    exit -1
 fi
 
 lang=$1
 shift
-outfile=corpus."$lang".tok
+
+# Whitespace and newline normalizer
+# Do this externally to guard against any differences between
+# the underlying Flex tokenizers.
+scriptdir=/u/spenceg/javanlp/projects/mt/ptm/scripts/mt
+fixnl="$scriptdir"/fix_cr.py
 
 # Arabic word segmenter setup
 AR_MODEL=/scr/spenceg/atb-lex/1-Raw-All.utf8.txt.model.gz
@@ -22,13 +27,19 @@ AR_TOK="java -server -XX:+UseCompressedOops -XX:MaxPermSize=2g -Xmx6g -Xms6g edu
 # English tokenizer setup
 EN_TOK="java -server -XX:+UseCompressedOops -XX:MaxPermSize=2g edu.stanford.nlp.process.PTBTokenizer -preserveLines -lowerCase -options ptb3Escaping=false,asciiQuotes=true"
 
-# Choose language-specific tokenizer
-# These tokenizers perform newline normalization internally.
+# Lowercase ASCII text that appears in the Arabic data.
 if [ $lang == "Arabic" ]; then
-	# Lowercase ASCII text that appears in the Arabic data.
-	cat $* | $AR_TOK | tr A-Z a-z > "$outfile" 
-
+    for infile in $*
+    do
+	outfile=`basename "$infile"`
+	cat $infile | sed -e 's/[[:cntrl:]]/ /g' | $fixnl | $AR_TOK | tr A-Z a-z > ${outfile}.tok 
+    done
+    
 elif [ $lang == "English" ]; then
-	cat $* | $EN_TOK > "$outfile"
+    for infile in $*
+    do
+	outfile=`basename "$infile"`
+	cat $infile | sed -e 's/[[:cntrl:]]/ /g' | $fixnl | $EN_TOK > ${outfile}.tok
+    done
 fi
 
