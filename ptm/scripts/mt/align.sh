4dddb0b6caa5b98514ad1396e35ed510e5cd2cec
spenceg <spenceg@stanford.edu>
2012-08-21 23:54:45 +0000

    Re-factored.
    svn path=/trunk/javanlp/; revision=41317

--- ptm/scripts/mt/align.sh
+++ ptm/scripts/mt/align.sh
@@ -4,51 +4,70 @@
 # Berkeley aligner 2.1 and the Phrasal implementation
 # of symmetrization heuristics.
 #
-if [ $# -ne 3 ]; then
-    echo Usage: `basename $0` src_file_gz tgt_file_gz lines_per_split
+if [ $# -ne 5 ]; then
+    echo Usage: `basename $0` src_file src_lang tgt_file tgt_lang lines_per_split
     echo
-    echo "NOTE: Expects that the bitext is gzip'd."
+		echo src_lang/tgt_lang should be two-letter ISO 639-1 language codes.
     exit -1
 fi
 
-# Uncomment the line below to run on the cluster
+# Uncomment the line below to run locally 
 MEM=15g
-#EXEC=
+#EXEC=echo
 EXEC="nlpsub -m${MEM} -c4"
 
 src_file=$1
-tgt_file=$2
-split_size=$3
+src_lang=$2
+tgt_file=$3
+tgt_lang=$4
+split_size=$5
+
+src_name=$(basename "$src_file")
+src_ext="${src_name##*.}"
+CAT1=cat
+if [ $src_ext == "gz" ]; then
+	CAT1=zcat
+elif [ $src_ext == "bz2" ]; then
+	CAT1=bzcat
+fi
+
+tgt_name=$(basename "$tgt_file")
+tgt_ext="${tgt_name##*.}"
+CAT2=cat
+if [ $tgt_ext == "gz" ]; then
+	CAT2=zcat
+elif [ $tgt_ext == "bz2" ]; then
+	CAT2=bzcat
+fi
+
 
 scriptdir=${JAVANLP_HOME}/projects/mt/ptm/scripts/mt
 mkconf=${scriptdir}/mkconf.py
 conf_template=${scriptdir}/ucb-align.conf
 align=${JAVANLP_HOME}/projects/mt/scripts/align
 
-filename=`basename "$src_file"`
-src_ext="${filename##*.}"
-#filename="${filename%.*}"
-
-filename=`basename "$tgt_file"`
-tgt_ext="${filename##*.}"
-#filename="${filename%.*}"
 
 # Step 1: Split the bitext
-zcat ${src_file} | split -l "$split_size" -d - split."$src_ext".
-zcat ${tgt_file} | split -l "$split_size" -d - split."$tgt_ext".
+echo Counting the bitext and splitting if necessary...
+src_len=`$CAT1 ${src_file} | wc -l`
+echo Bitext size: $src_len lines
+$CAT1 ${src_file} | split -l "$split_size" -d - split."$src_name".
+$CAT2 ${tgt_file} | split -l "$split_size" -d - split."$tgt_name".
+
 
 # Step 2: Align each split
-for src_split in `ls split."$src_ext".*`
+echo Creating alignment jobs...
+for src_split in `ls split."$src_name".*`
 do
     splitnum="${src_split##*.}"
-    modeldir=split.${splitnum}
-    mkdir $modeldir
-    mkdir ${modeldir}/data
+    modeldir=split."$src_lang"-"$tgt_lang".${splitnum}
+    mkdir -p $modeldir
+    mkdir -p ${modeldir}/data
     cd ${modeldir}/data
-    ln -s ../../${src_split} split."$src_ext"
-    ln -s ../../split."$tgt_ext"."$splitnum" split."$tgt_ext"
+    ln -s ../../${src_split} split."$src_lang"
+    ln -s ../../split."$tgt_name"."$splitnum" split."$tgt_lang"
     cd ../../
-    $mkconf $modeldir ${modeldir}/data $src_ext $tgt_ext < $conf_template > ${modeldir}.conf
+    $mkconf $modeldir ${modeldir}/data $src_lang $tgt_lang < $conf_template > ${modeldir}.conf
     $EXEC $align $MEM ${modeldir}.conf 
 done
 
