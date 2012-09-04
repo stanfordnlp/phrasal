b0d76bda8452562e28e0f3f2d8730d0341401c3e
spenceg <spenceg@stanford.edu>
2012-09-04 21:50:10 +0000

    Added 1s pauses and edit distances.
    svn path=/trunk/javanlp/; revision=41380

--- ptm/scripts/wrangle/make_translation_frame.py
+++ ptm/scripts/wrangle/make_translation_frame.py
@@ -17,7 +17,7 @@ import ptm_file_io
 from edit_distance import dameraulevenshtein
 
 # Output format
-TranslationAnovaRow = namedtuple('TranslationAnovaRow', 'time prev_time pause_mean pause_cnt pause_cnt1s event_cnt event_keyboard_cnt event_mouse_cnt event_focus_cnt event_browser_cnt src_id ui_id tgt_len user_id rank')
+TranslationAnovaRow = namedtuple('TranslationAnovaRow', 'time prev_time pause_mean pause_cnt pause_cnt1s pause_ratio pause_ratio1s event_cnt event_keyboard_cnt event_mouse_cnt event_focus_cnt event_browser_cnt src_id ui_id tgt_len user_id edit_distance rank')
 
 # Event delay that indicates a pause (unit: ms)
 # From Joort1996
@@ -38,11 +38,14 @@ def process_action_file(action_file):
     pause_counts = Counter()
     pause_counts2 = Counter()
     pause_means = []
+    pause_ratio = []
+    pause_ratio2 = []
     event_counters = defaultdict(Counter)
 
     last_src_id = 0
     last_event_time = 0
     durations = 0
+    durations2 = 0
     action_rows = ptm_file_io.load_actionlog_events(action_file)
     for row in action_rows:
         src_id = int(row.sourceid)
@@ -52,11 +55,20 @@ def process_action_file(action_file):
             last_src_id = src_id
             last_event_time = 0
             durations = 0
+            durations2 = 0
+            
         elif row.event_name == 'end':
             assert pause_counts[src_id] > 0
             mean = float(durations) / float(pause_counts[src_id])
             pause_means.append(mean)
+            
+            ratio = float(durations) / float(event_time)
+            pause_ratio.append(ratio)
+            ratio1s = float(durations2) / float(event_time)
+            pause_ratio2.append(ratio1s)
+
             time_list.append(event_time)
+
         else:
             event_counters[src_id][event_class] += 1
             pause_duration = event_time - last_event_time
@@ -65,8 +77,9 @@ def process_action_file(action_file):
                 durations += pause_duration
             if pause_duration > MIN_PAUSE_DURATION2:
                 pause_counts2[src_id] += 1
+                durations2 += pause_duration
             last_event_time = event_time
-    return time_list,pause_counts,pause_counts2,pause_means,event_counters
+    return time_list,pause_counts,pause_counts2,pause_means,event_counters,pause_ratio,pause_ratio2
 
 
 def get_edit_distances(tgt_segments, ref_segments):
@@ -105,7 +118,7 @@ def get_rows(directory, user_id, ref_segments, rankings):
     meta_rows = ptm_file_io.load_meta_file(meta_file)
 
     # Action log response variables
-    time_list,pause_counts,pause_counts2,pause_means,event_counters = process_action_file(action_file)
+    time_list,pause_counts,pause_counts2,pause_means,event_counters,pause_ratio,pause_ratio2 = process_action_file(action_file)
 
     # Text clustering
     tgt_edist_list = get_edit_distances(tgt_segments, ref_segments)
@@ -123,6 +136,8 @@ def get_rows(directory, user_id, ref_segments, rankings):
                                   pause_mean=str(pause_means[i]),
                                   pause_cnt=str(pause_counts[i]),
                                   pause_cnt1s=str(pause_counts2[i]),
+                                  pause_ratio=str(pause_ratio[i]),
+                                  pause_ratio1s=str(pause_ratio2[i]),
                                   event_cnt=str(sum(event_counters[i].values())),
                                   event_keyboard_cnt=str(event_counters[i]['keyboard']),
                                   event_mouse_cnt=str(event_counters[i]['mouse']),
@@ -132,6 +147,7 @@ def get_rows(directory, user_id, ref_segments, rankings):
                                   ui_id=ui_id,
                                   tgt_len=tgt_len,
                                   user_id=user_id,
+                                  edit_distance=str(tgt_edist_list[i]),
                                   rank=str(score))
         output_row_list.append(row)
     
