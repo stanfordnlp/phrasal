55e4485e1adb66514ad38251c58b0cce416ba920
spenceg <spenceg@stanford.edu>
2012-12-06 19:25:51 +0000

    Implement same interface as command-line BLEUMetric.
    svn path=/trunk/javanlp/; revision=42367

--- src/edu/stanford/nlp/mt/tools/SmoothBLEU.java
+++ src/edu/stanford/nlp/mt/tools/SmoothBLEU.java
@@ -1,30 +1,54 @@
 package edu.stanford.nlp.mt.tools;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.util.List;
 
+import edu.stanford.nlp.mt.base.IString;
+import edu.stanford.nlp.mt.base.IStrings;
+import edu.stanford.nlp.mt.base.RawSequence;
+import edu.stanford.nlp.mt.base.Sequence;
 import edu.stanford.nlp.mt.metrics.BLEUMetric;
-
-import java.util.Arrays;
+import edu.stanford.nlp.mt.metrics.Metrics;
+import edu.stanford.nlp.mt.metrics.NISTTokenizer;
 
 /**
- * Interactive smooth BLEU utility
+ * Compute smooth BLEU according to Lin and Och (2004).
  * 
  * @author daniel cer
+ * @author Spence Green
  *
  */
 public class SmoothBLEU {
-  public static void main(String[] args) {
-    if (args.length != 3) {
-      System.out.println("Usage:\n\tjava ...SmoothBLEU \"this is ref1|||this is ref2\" \"translation\" (order)\n");
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      System.err
+          .printf("Usage: java %s [-order #] (ref 1) (ref 2) ... (ref n) < candidateTranslations%n", SmoothBLEU.class.getName());
       System.exit(-1);
     }
-      
-    String[] refArr = args[0].split("\\|\\|\\|");
-    
-    List<String> refs = Arrays.asList(refArr);
-    String trans = args[1];
-    int order = Integer.parseInt(args[2]);
-    double smoothBLEU = BLEUMetric.computeLocalSmoothScore(trans, refs, order);
-    System.out.printf("%.3f\n", smoothBLEU*100);
+
+    int BLEUOrder = 4;
+    if (args[0].equals("-order")) {
+      BLEUOrder = Integer.parseInt(args[1]);
+      String[] newArgs = new String[args.length - 2];
+      System.arraycopy(args, 2, newArgs, 0, args.length - 2);
+      args = newArgs;
+    }
+    List<List<Sequence<IString>>> referencesList = Metrics.readReferences(args);
+
+    LineNumberReader reader = new LineNumberReader(new InputStreamReader(
+        System.in));
+
+    int translationId = 0;
+    for (String line; (line = reader.readLine()) != null; ) {
+      line = NISTTokenizer.tokenize(line).trim();
+      Sequence<IString> translation = new RawSequence<IString>(
+          IStrings.toIStringArray(line.split("\\s+")));
+      double score = 
+          BLEUMetric.computeLocalSmoothScore(translation, referencesList.get(translationId), BLEUOrder);
+      System.out.printf("%d\t%.4f%n", translationId, score);
+      ++translationId;
+    }
   }
 }
