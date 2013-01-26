e0ad5550bc05060386e7c5724c983cb511cb1721
Spence Green <spence@spencegreen.com>
2013-01-26 13:08:15 -0800

    Add SparseScorer

---
+++ src/edu/stanford/nlp/mt/decoder/util/SparseScorer.java
@@ -0,0 +1,73 @@
+package edu.stanford.nlp.mt.decoder.util;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import edu.stanford.nlp.mt.base.FeatureValue;
+import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.util.Index;
+import edu.stanford.nlp.util.OAIndex;
+
+/**
+ * A lightweight scorer for high dimensional models.
+ * 
+ * @author Spence Green
+ *
+ */
+public class SparseScorer implements Scorer<String> {
+
+  private final Index<String> featureIndex;
+  private Counter<String> weights;
+  
+  public SparseScorer(Counter<String> featureWts) {
+    this(featureWts, null);
+  }
+
+  /**
+   * Constructor. If featureIndex is null, then a local feature index will be used.
+   * 
+   * @param featureWts
+   * @param featureIndex
+   */
+  public SparseScorer(Counter<String> featureWts, Index<String> featureIndex) {
+    this.featureIndex = featureIndex == null ? new OAIndex<String>() : featureIndex;
+    updateWeights(featureWts);
+  }
+  
+  @Override
+  public double getIncrementalScore(Collection<FeatureValue<String>> features) {
+    double score = 0.0;
+    for (FeatureValue<String> feature : features) {
+      score += feature.value * weights.getCount(feature.name);
+    }
+    return score;
+  }
+
+  @Override
+  public void updateWeights(Counter<String> weights) {
+    this.weights = weights;
+    
+    if (featureIndex.isLocked()) {
+      throw new RuntimeException("Cannot update weight vector after the feature index has been locked!");
+    } else {
+      for (String feature : weights.keySet()) {
+        featureIndex.indexOf(feature, true);
+      }
+    }
+  }
+
+  @Override
+  public void saveWeights(String filename) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean hasNonZeroWeight(String featureName) {
+    return featureIndex.contains(featureName) && weights.getCount(featureName) != 0.0;
+  }
+
+  @Override
+  public Index<String> getFeatureIndex() {
+    return featureIndex;
+  }
+}
