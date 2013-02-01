f154db762b33deedfc59f9ac8f04b1a771638980
Christopher Manning <manning@stanford.edu>
2013-02-01 09:42:30 -0800

    Trivial changes

--- src-research/edu/stanford/nlp/rte/mtmetric/RTEFeaturizer.java
+++ src-research/edu/stanford/nlp/rte/mtmetric/RTEFeaturizer.java
@@ -15,20 +15,19 @@ import edu.stanford.nlp.stats.Counters;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * 
+ *
  * @author John Bauer, Daniel Cer
  *
  */
 public class RTEFeaturizer {
   RTEPipeline pipeline;
   NoLearningExperiment kbeTester;
-  
+
   /**
    * Since the RTE system is not re-entrant, and there is no
    * realistic way to create two of them because of the Global state,
@@ -56,9 +55,9 @@ public class RTEFeaturizer {
     Global.setProperty("parse.malt", "false");
     Global.setProperty("rsrc.malt", "engmalt.linear.mco");
     // this was turned off for the Ontonotes/WordNet dataset
-    Global.setProperty("lex.Nominalization", "on"); 
+    Global.setProperty("lex.Nominalization", "on");
     Global.setProperty("lex.InfoMap", "on");
-    Global.setProperty("lex.WNHypernymy", "on"); 
+    Global.setProperty("lex.WNHypernymy", "on");
     // this was turned off for the Ontonotes/WordNet dataset
     Global.setProperty("lex.JiangConrathWN", "on");
     Global.setProperty("lex.WNHyponymy", "on");
@@ -85,9 +84,9 @@ public class RTEFeaturizer {
     return counter;
   }
 
-  private void addScores(Counter<String> counter, String prefix, 
+  private void addScores(Counter<String> counter, String prefix,
                          String text, String hypothesis) {
-    MockProblem problem = new MockProblem(prefix + "-rte-scoring", 
+    MockProblem problem = new MockProblem(prefix + "-rte-scoring",
                                           text, hypothesis);
     pipeline.processProblem(problem);
 
@@ -95,17 +94,17 @@ public class RTEFeaturizer {
 
     counter.incrementCount(prefix + "-alignment", problem.getAlignmentScore());
     for (FeatureOccurrence feature : problem.getFeatureOccurrences()) {
-      String name = feature.feature.name();     
+      String name = feature.feature.name();
       counter.incrementCount(prefix + "-" + name, feature.value);
     }
   }
-  
+
   Counter<String> defaultWts;
-  
-  public double mtScore(String[] refs, String translation, Counter<String> wts) {
+
+  public static double mtScore(String[] refs, String translation, Counter<String> wts) {
      int bestRef = 0;
      double bestRefScore = Double.NEGATIVE_INFINITY;
-     
+
      // only call RTE on one reference
      for (int i=0; i < refs.length; i++) {
        double refScore = BLEUMetric.computeLocalSmoothScore(translation, Arrays.asList(refs[i]), 4);
@@ -114,15 +113,15 @@ public class RTEFeaturizer {
          bestRef = i;
        }
      }
-     
+
      System.out.printf("Scoring:\n\tref: %s\n\thyp: %s\n", refs[bestRef],translation);
      Counter<String> features = mtFeaturizer(refs[bestRef],translation);
      double score = Counters.dotProduct(wts, features);
      System.out.printf("Score: %e\n", score);
      return score;
   }
-  
-  public Counter<String> mtFeaturizer(String reference, String translation) {
+
+  public static Counter<String> mtFeaturizer(String reference, String translation) {
     Counter<String> results;
     try {
       results = featurizer.featurize(reference, translation);
@@ -139,20 +138,20 @@ public class RTEFeaturizer {
     results.incrementCount("biasFeature");
     return results;
   }
-  
+
   public static void main(String[] args) throws Exception {
     RTEFeaturizer featurizer = initialize(args);
     long startTime = System.currentTimeMillis();
-    BufferedReader reader = 
+    BufferedReader reader =
       new BufferedReader(new InputStreamReader(System.in));
-    List<Counter<String>> dataPts = new ArrayList<Counter<String>>(); 
+    List<Counter<String>> dataPts = new ArrayList<Counter<String>>();
     List<Double>  scores = new ArrayList<Double>();
     Index<String> featureIndex = new OAIndex<String>();
     boolean interactive = System.console() != null;
-    int row = 0;    
+    int row = 0;
 
     if (interactive) System.out.print("\nRTE Eval>");
-    for (String line = reader.readLine(); line != null; 
+    for (String line = reader.readLine(); line != null;
          line = reader.readLine()) {
       try {
         String[] fields = line.split(" \\|\\|\\| ");
@@ -161,7 +160,7 @@ public class RTEFeaturizer {
         String mt  = fields[2];
         double score = Double.parseDouble(fields[3]);
         System.err.printf("Scoring: \n\tref: %s hyp: %s\n", ref, mt);
-        Counter<String> results = featurizer.mtFeaturizer(ref, mt);      
+        Counter<String> results = featurizer.mtFeaturizer(ref, mt);
         dataPts.add(results);
         scores.add(score);
         for (String feature : results.keySet()) {
