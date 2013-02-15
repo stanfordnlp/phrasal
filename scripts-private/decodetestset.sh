704927d4696cd9b4afdbfc65ad4c52eee3b6f847
Spence Green <spence@spencegreen.com>
2013-02-14 21:29:28 -0800

    Script accommodates test tuples

--- scripts-private/decodetestset.sh
+++ scripts-private/decodetestset.sh
@@ -1,35 +1,53 @@
 #!/usr/bin/env bash
+#
+#
 # Author: sidaw
-if [ $# -ne 5 ]; then
-    echo Usage: `basename $0` tuneset name testsetname testsetloc iterationnumber
-    echo e.g. `basename $0` mt06 l1-001-cluster mt04 zhdata/mt04.seg.zh 5
-    exit 0
+#
+if [ $# -lt 6 ]; then
+    echo Usage: `basename $0` tuneset name iteration oldtestset newtestset:filepath "[newtestset:filepath]"
+    echo e.g. `basename $0` mt06 l1-001-cluster 5 mt05 mt04:zhdata/mt04.seg.zh
+    exit -1
 fi
 
+#
+# Take the results of a tuning run and decode a test set
+#
 function decodetest {
-echo decoding on $testset tuned on $tuneset runname  $name
-mv $tuneset.$name.online.final.binwts $tuneset.$name.online.final.binwts.$RANDOM
-ln -s  $tuneset.$name.online.$iter.binwts $tuneset.$name.online.final.binwts
-if [ -e $testset.$tuneset.$name.vars ]; then
-echo var file already exists, using the exisitng one
-else
-echo var file does not exisit, making new one by concatenatiing DECODE_SET and DECODE_SET_NAME to the end
-varname=$testset.$tuneset.$name.vars
-cat test5k.$tuneset.$name.vars > $varname
-echo '' >> $varname
-echo DECODE_SET=$testsetloc >> $varname
-echo DECODE_SET_NAME=$testset >> $varname
-fi
+    echo Decoding on $testset tuned on $tuneset runname $name
+    if [ -L $tuneset.$name.online.final.binwts ]; then
+	rm $tuneset.$name.online.final.binwts
+    fi
+    if [ -e $tuneset.$name.online.final.binwts ]; then
+	mv $tuneset.$name.online.final.binwts $tuneset.$name.online.final.binwts.bak
+    fi
 
-phrasal-train-tune.sh $testset.$tuneset.$name.vars 6-7 $tuneset.$name.ini $name
-mv $testset.$tuneset.$name.bleu $testset.$tuneset.$name.$iter.bleu
+    # Symlink the appropriate iteration
+    ln -s  $tuneset.$name.online.$iter.binwts $tuneset.$name.online.final.binwts
+    
+    varfile=$testset.$tuneset.$name.vars
+    if [ -e $testset.$tuneset.$name.vars ]; then
+	echo var file already exists, using the existing one
+    else
+	echo var file does not exist, making new one
+	cat $oldtestset.$tuneset.$name.vars | grep -v "DECODE_SET" > $varfile
+	echo "DECODE_SET=$testsetloc" >> $varfile
+	echo "DECODE_SET_NAME=$testset" >> $varfile
+    fi
+
+    phrasal-train-tune.sh $varfile 6-7 $tuneset.$name.ini $name
+    mv $testset.$tuneset.$name.bleu $testset.$tuneset.$name.$iter.bleu
 }
-
 
 tuneset=$1
 name=$2
-testset=$3
-testsetloc=$4
-iter=$5
-decodetest
+iter=$3
+oldtestset=$4
+shift 4
 
+# Iterate over test sets
+for tuple in $@; do
+    echo $tuple
+    testset=${tuple%:*}
+    testsetloc=${tuple#*:}
+    decodetest
+done
