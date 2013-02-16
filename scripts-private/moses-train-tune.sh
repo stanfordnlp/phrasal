cf39e3919530cac785dd0a2e7bfbcff723bb028c
Spence Green <spence@spencegreen.com>
2013-02-16 01:10:31 -0800

    From A3

--- scripts-private/moses-train-tune.sh
+++ scripts-private/moses-train-tune.sh
@@ -21,8 +21,8 @@ MOSES=/u/nlp/packages/mosesdecoder
 GIZA=/u/nlp/packages/GIZA++
 
 # TODO: Change these for your language
-CORPUSDIR="$HOME"/training/corpus
-LM=/scr/nlp/data/gale/NIST09/lm/releases/mt_giga3_afp_xin.1234.unk.lm.gz
+CORPUSDIR=/u/spenceg/BOLT-Arabic-all/corpus2/sampled/dedup
+LM=/u/spenceg/BOLT-Arabic-all/lm/5gm-dedup.unk.model.gz
 TUNE_SET="$HOME"/mt06.unk
 REF_PREFIX="$HOME"/refs/mt06/ref
 
@@ -30,21 +30,26 @@ REF_PREFIX="$HOME"/refs/mt06/ref
 # hierarchical re-ordering models.
 mkdir -p $HOME/train/model
 $MOSES/scripts/training/train-model.perl --max-phrase-length 7 \
---external-bin-dir $GIZA --first-step 4 --last-step 9 \
--root-dir $HOME/train -corpus $CORPUSDIR -f ar -e en \
--alignment-file $HOME/training/corpus -alignment grow-diag \
+--external-bin-dir $GIZA --first-step 3 --last-step 9 \
+-root-dir $HOME/train -corpus $CORPUSDIR/corpus -f ar -e en \
+-giza-f2e $CORPUSDIR/giza.ar-en \
+-giza-e2f $CORPUSDIR/giza.en-ar \
+ -alignment grow-diag \
 -lm 0:3:"$LM":0 \
 -reordering hier-mslr-bidirectional-fe
 
-# Model tuning with MIRA
+
+
+
+# Batch mira Model tuning with MIRA
 # For MERT: remove the "--batch-mira" parameter
 # For PRO: replace mira with "--pairwise-ranked"
 rm -rf $HOME/$TUNEDIR
 mkdir -p $HOME/$TUNEDIR
-$MOSES/scripts/training/mert-moses.pl \
---working-dir $HOME/$TUNEDIR \
---decoder-flags="-distortion-limit 5 -threads all" --mertdir $MOSES/bin/ \
---nbest=500 \
---batch-mira --return-best-dev \
-$TUNE_SET $REF_PREFIX \
-$MOSES/bin/moses $HOME/train/model/moses.ini
+#$MOSES/scripts/training/mert-moses.pl \
+#--working-dir $HOME/$TUNEDIR \
+#--decoder-flags="-distortion-limit 5 -threads all" --mertdir $MOSES/bin/ \
+#--nbest=500 \
+#--batch-mira --return-best-dev \
+#$TUNE_SET $REF_PREFIX \
+#$MOSES/bin/moses $HOME/train/model/moses.ini
