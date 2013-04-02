e009d8c94885bdf21fdb2bfcc03c0e1ba0945846
Spence Green <spence@spencegreen.com>
2013-04-02 16:53:33 -0700

    Remove OOV classes from discriminative featurizers; they didn't scale to large data sets

--- src-research/edu/stanford/nlp/mt/decoder/efeat/DiscriminativeAlignmentFeaturizer.java
+++ src-research/edu/stanford/nlp/mt/decoder/efeat/DiscriminativeAlignmentFeaturizer.java
@@ -1,29 +1,22 @@
 package edu.stanford.nlp.mt.decoder.efeat;
 
-import java.io.IOException;
-import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import edu.stanford.nlp.mt.base.CacheableFeatureValue;
 import edu.stanford.nlp.mt.base.ConcreteTranslationOption;
 import edu.stanford.nlp.mt.base.FeatureValue;
 import edu.stanford.nlp.mt.base.Featurizable;
-import edu.stanford.nlp.mt.base.IOTools;
 import edu.stanford.nlp.mt.base.IString;
 import edu.stanford.nlp.mt.base.PhraseAlignment;
 import edu.stanford.nlp.mt.base.Sequence;
 import edu.stanford.nlp.mt.decoder.feat.AlignmentFeaturizer;
 import edu.stanford.nlp.mt.decoder.feat.IncrementalFeaturizer;
 import edu.stanford.nlp.mt.decoder.feat.IsolatedPhraseFeaturizer;
-import edu.stanford.nlp.stats.Counter;
-import edu.stanford.nlp.stats.ThreadsafeCounter;
 import edu.stanford.nlp.util.Index;
 
 /**
@@ -39,80 +32,25 @@ IncrementalFeaturizer<IString, String>, IsolatedPhraseFeaturizer<IString,String>
   private static final String FEATURE_NAME_TGT = "UnAlignTgt";
   private static final String FEATURE_NAME_SRC = "UnAlignSrc";
 
-  private static final double DEFAULT_UNSEEN_THRESHOLD = 2.0;
-
-  private final boolean addUnalignedSourceWords;
-  private final boolean addUnalignedTargetWords;
-  private final double unseenThreshold;
-
-  private Counter<String> featureCounter;
-  private Index<String> featureIndex;
-  
-  private final boolean createOOVClasses;
-
-  private Map<String,String> srcWordToClassMap;
-  private boolean mapSrcWord = false;
-  private Map<String,String> tgtWordToClassMap;
-  private boolean mapTgtWord = false;
+  private final boolean addSourceDeletions;
+  private final boolean addTargetInsertions;
   
   public DiscriminativeAlignmentFeaturizer() { 
-    addUnalignedSourceWords = false;
-    addUnalignedTargetWords = false;
-    unseenThreshold = DEFAULT_UNSEEN_THRESHOLD;
-    createOOVClasses = unseenThreshold > 0.0;
+    addSourceDeletions = false;
+    addTargetInsertions = false;
   }
 
   public DiscriminativeAlignmentFeaturizer(String...args) {
-    addUnalignedSourceWords = args.length > 0 ? Boolean.parseBoolean(args[0]) : false;
-    addUnalignedTargetWords = args.length > 1 ? Boolean.parseBoolean(args[1]) : false;
-    unseenThreshold = args.length > 2 ? Double.parseDouble(args[2]) : DEFAULT_UNSEEN_THRESHOLD;
-    createOOVClasses = unseenThreshold > 0.0;
-    String srcClassFile = args.length > 3 ? args[3] : null;
-    String tgtClassFile = args.length > 4 ? args[4] : null;
-    if (srcClassFile != null) {
-      srcWordToClassMap = loadWordClassMap(srcClassFile);
-      mapSrcWord = true;
-    }
-    if (tgtClassFile != null) {
-      tgtWordToClassMap = loadWordClassMap(tgtClassFile);
-      mapTgtWord = true;
-    }
-  }
-
-  private static Map<String, String> loadWordClassMap(String wordClassFile) {
-    LineNumberReader reader = IOTools.getReaderFromFile(wordClassFile);
-    Map<String,String> map = new HashMap<String,String>(60000);
-    try {
-      for (String line; (line = reader.readLine()) != null;) {
-        String[] toks = line.trim().split("\\s+");
-        if (toks.length == 2) {
-          map.put(toks[0], toks[1]);
-        } else {
-          System.err.printf("%s: Ignoring line %s (line: %d)%n", DiscriminativeAlignmentFeaturizer.class.getName(),
-              line.trim(), reader.getLineNumber());
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException("Could not load: " + wordClassFile);
-    }
-    return map;
+    addSourceDeletions = args.length > 0 ? Boolean.parseBoolean(args[0]) : false;
+    addTargetInsertions = args.length > 1 ? Boolean.parseBoolean(args[1]) : false;
   }
 
   @Override
   public void initialize(Index<String> featureIndex) {
-    this.featureIndex = featureIndex;
-    featureCounter = !featureIndex.isLocked() && createOOVClasses ? 
-        new ThreadsafeCounter<String>(100*featureIndex.size()) : null;
   }
 
   @Override
   public List<FeatureValue<String>> phraseListFeaturize(Featurizable<IString, String> f) {
-    return featurizeRule(f, true);
-  }
-
-  private List<FeatureValue<String>> featurizeRule(
-      Featurizable<IString, String> f, boolean incrementCount) {
     PhraseAlignment alignment = f.option.abstractOption.alignment;
     final int eLength = f.translatedPhrase.size();
     final int fLength = f.foreignPhrase.size();
@@ -127,12 +65,11 @@ IncrementalFeaturizer<IString, String>, IsolatedPhraseFeaturizer<IString,String>
     for (int i = 0; i < eLength; ++i) {
       int[] fIndices = alignment.e2f(i);
       String eWord = f.translatedPhrase.get(i).toString();
-      if (mapTgtWord) eWord = mapToClass(eWord, tgtWordToClassMap);
       
       if (fIndices == null) {
         // Unaligned target word
-        if (addUnalignedTargetWords) {
-          String feature = makeFeatureString(FEATURE_NAME_TGT, eWord, 0, incrementCount);
+        if (addTargetInsertions) {
+          String feature = makeFeatureString(FEATURE_NAME_TGT, eWord);
           features.add(new CacheableFeatureValue<String>(feature, 1.0));
         }
 
@@ -147,7 +84,6 @@ IncrementalFeaturizer<IString, String>, IsolatedPhraseFeaturizer<IString,String>
             f2e.get(fInsertionIndex).add(eWord);
           } else {
             String fWord = f.foreignPhrase.get(fIndex).toString();
-            if (mapSrcWord) fWord = mapToClass(fWord, srcWordToClassMap);
             f2e.get(fInsertionIndex).add(fWord);
           }
           fIsAligned[fIndex] = true;
@@ -159,10 +95,9 @@ IncrementalFeaturizer<IString, String>, IsolatedPhraseFeaturizer<IString,String>
     for (int i = 0; i < fLength; ++i) {
       Set<String> eWords = f2e.get(i);
       String fWord = f.foreignPhrase.get(i).toString();
-      if (mapSrcWord) fWord = mapToClass(fWord, srcWordToClassMap);
       if ( ! fIsAligned[i]) {
-        if (addUnalignedSourceWords) {
-          String feature = makeFeatureString(FEATURE_NAME_SRC, fWord, 0, incrementCount);
+        if (addSourceDeletions) {
+          String feature = makeFeatureString(FEATURE_NAME_SRC, fWord);
           features.add(new CacheableFeatureValue<String>(feature, 1.0));
         }
       } else if (eWords.size() > 0){
@@ -176,39 +111,15 @@ IncrementalFeaturizer<IString, String>, IsolatedPhraseFeaturizer<IString,String>
           if (j != 0) sb.append("-");
           sb.append(alignedWords.get(j));
         }
-        String feature = makeFeatureString(FEATURE_NAME, sb.toString(), fLength, incrementCount);
+        String feature = makeFeatureString(FEATURE_NAME, sb.toString());
         features.add(new CacheableFeatureValue<String>(feature, 1.0));
       }
     }
     return features;
   }
 
-  private static String mapToClass(String word, Map<String, String> wordToClassMap) {
-    return wordToClassMap.containsKey(word) ? wordToClassMap.get(word) : "UNK";
-  }
-
-  private String makeFeatureString(String featureName, String featureSuffix, int fLength, boolean incrementCount) {
-    String featureString = String.format("%s:%s", featureName, featureSuffix);
-    if ( ! createOOVClasses) return featureString;
-    
-    // Collect statistics and detect unseen events
-    if (featureCounter == null) {
-      // Test time
-      if (featureIndex.indexOf(featureString) < 0) {
-        // TODO(spenceg): Elaborate?
-        featureString = String.format("%s:UNK%d", featureName, fLength);
-      }
-
-    } else {
-      // Training time
-      double count = incrementCount ? featureCounter.incrementCount(featureString) 
-          : featureCounter.getCount(featureString);
-      if (count <= unseenThreshold) {
-        // TODO(spenceg): Elaborate?
-        featureString = String.format("%s:UNK%d", featureName, fLength);
-      }
-    }
-    return featureString;
+  private String makeFeatureString(String featureName, String featureSuffix) {
+    return String.format("%s:%s", featureName, featureSuffix);
   }
 
   @Override
--- src-research/edu/stanford/nlp/mt/decoder/efeat/DiscriminativePhraseTable.java
+++ src-research/edu/stanford/nlp/mt/decoder/efeat/DiscriminativePhraseTable.java
@@ -1,23 +1,16 @@
 package edu.stanford.nlp.mt.decoder.efeat;
 
-import java.io.IOException;
-import java.io.LineNumberReader;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import edu.stanford.nlp.mt.base.CacheableFeatureValue;
 import edu.stanford.nlp.mt.base.ConcreteTranslationOption;
 import edu.stanford.nlp.mt.base.FeatureValue;
 import edu.stanford.nlp.mt.base.Featurizable;
-import edu.stanford.nlp.mt.base.IOTools;
 import edu.stanford.nlp.mt.base.IString;
 import edu.stanford.nlp.mt.base.Sequence;
 import edu.stanford.nlp.mt.decoder.feat.IncrementalFeaturizer;
 import edu.stanford.nlp.mt.decoder.feat.IsolatedPhraseFeaturizer;
-import edu.stanford.nlp.stats.Counter;
-import edu.stanford.nlp.stats.ThreadsafeCounter;
 import edu.stanford.nlp.util.Index;
 
 /**
@@ -32,142 +25,52 @@ public class DiscriminativePhraseTable implements IncrementalFeaturizer<IString,
   public static final String TARGET = "trg";
   public static final String SOURCE_AND_TARGET = "s+t";
 
-  private static final double DEFAULT_UNSEEN_THRESHOLD = 2.0;
-
   private final boolean doSource;
   private final boolean doTarget;
-  private final double unseenThreshold;
-
-  private Counter<String> featureCounter;
-  private Index<String> featureIndex;
-  
-  private final boolean createOOVClasses;
-
-  private Map<String,String> srcWordToClassMap;
-  private boolean mapSrcWord = false;
-  private Map<String,String> tgtWordToClassMap;
-  private boolean mapTgtWord = false;
   
   public DiscriminativePhraseTable() {
     doSource = true;
     doTarget = true;
-    unseenThreshold = DEFAULT_UNSEEN_THRESHOLD;
-    createOOVClasses = unseenThreshold > 0.0;
   }
 
   public DiscriminativePhraseTable(String... args) {
     doSource = args.length > 0 ? Boolean.parseBoolean(args[0]) : true;
     doTarget = args.length > 1 ? Boolean.parseBoolean(args[1]) : true;
-    unseenThreshold = args.length > 2 ? Double.parseDouble(args[2]) : DEFAULT_UNSEEN_THRESHOLD;
-    createOOVClasses = unseenThreshold > 0.0;
-    String srcClassFile = args.length > 3 ? args[3] : null;
-    String tgtClassFile = args.length > 4 ? args[4] : null;
-    if (srcClassFile != null) {
-      srcWordToClassMap = loadWordClassMap(srcClassFile);
-      mapSrcWord = true;
-    }
-    if (tgtClassFile != null) {
-      tgtWordToClassMap = loadWordClassMap(tgtClassFile);
-      mapTgtWord = true;
-    }
   }
   
-  private static Map<String, String> loadWordClassMap(String wordClassFile) {
-    LineNumberReader reader = IOTools.getReaderFromFile(wordClassFile);
-    Map<String,String> map = new HashMap<String,String>(60000);
-    try {
-      for (String line; (line = reader.readLine()) != null;) {
-        String[] toks = line.trim().split("\\s+");
-        if (toks.length == 2) {
-          map.put(toks[0], toks[1]);
-        } else {
-          System.err.printf("%s: Ignoring line %s (line: %d)%n", DiscriminativePhraseTable.class.getName(),
-              line.trim(), reader.getLineNumber());
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException("Could not load word2class map: " + wordClassFile);
-    }
-    return map;
-  }
-
   @Override
-  public void initialize(Index<String> featureIndex) {
-    this.featureIndex = featureIndex;
-    featureCounter = !featureIndex.isLocked() && createOOVClasses ? 
-        new ThreadsafeCounter<String>(100*featureIndex.size()) : null;
-  }
+  public void initialize(Index<String> featureIndex) {}
 
   @Override
   public List<FeatureValue<String>> phraseListFeaturize(Featurizable<IString, String> f) {
-    return featurizePhrase(f, true);
-  }
-
-  private List<FeatureValue<String>> featurizePhrase(
-      Featurizable<IString, String> f, boolean incrementCount) {
     List<FeatureValue<String>> fvalues = new LinkedList<FeatureValue<String>>();
 
-    String srcPhrase = mapSrcWord ? mapPhrase(f.foreignPhrase, srcWordToClassMap) :
-      f.foreignPhrase.toString("_");
-    String tgtPhrase = mapTgtWord ? mapPhrase(f.translatedPhrase, tgtWordToClassMap) :
-      f.translatedPhrase.toString("_");
+    String srcPhrase = f.foreignPhrase.toString("_");
+    String tgtPhrase = f.translatedPhrase.toString("_");
     
     if (doSource && doTarget) {
       String suffix = srcPhrase + ">"
           + tgtPhrase;
       fvalues.add(new CacheableFeatureValue<String>(
-          makeFeatureString(FEATURE_NAME, SOURCE_AND_TARGET, suffix, f.foreignPhrase.size(), incrementCount), 
+          makeFeatureString(FEATURE_NAME, SOURCE_AND_TARGET, suffix, f.foreignPhrase.size()), 
           1.0));
 
     } else if (doSource) {
       fvalues.add(new CacheableFeatureValue<String>(
-          makeFeatureString(FEATURE_NAME, SOURCE, srcPhrase, f.foreignPhrase.size(), incrementCount), 
+          makeFeatureString(FEATURE_NAME, SOURCE, srcPhrase, f.foreignPhrase.size()), 
           1.0));
 
     } else if (doTarget) {
       fvalues.add(new CacheableFeatureValue<String>(
-          makeFeatureString(FEATURE_NAME, TARGET, tgtPhrase, f.foreignPhrase.size(), incrementCount), 
+          makeFeatureString(FEATURE_NAME, TARGET, tgtPhrase, f.foreignPhrase.size()), 
           1.0));
     }
     return fvalues;
   }
 
-  private static String mapPhrase(Sequence<IString> phrase,
-      Map<String, String> wordToClassMap) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < phrase.size(); ++i) {
-      if (i > 0) sb.append("_");
-      String word = phrase.get(i).toString();
-      word = wordToClassMap.containsKey(word) ? wordToClassMap.get(word) : "UNK";
-      sb.append(word);
-    }
-    return sb.toString();
-  }
-
   private String makeFeatureString(String featurePrefix, String featureType, String value, 
-      int length, boolean incrementCount) {
-    String featureString = String.format("%s.%s:%s", featurePrefix, featureType, value);
-    if ( ! createOOVClasses) return featureString;
-    
-    // Collect statistics and detect unseen events
-    if (featureCounter == null) {
-      // Test time
-      if (featureIndex.indexOf(featureString) < 0) {
-        // TODO(spenceg): Elaborate?
-        featureString = String.format("%s.%s:UNK%d", featurePrefix, featureType, length);
-      }
-
-    } else {
-      // Training time
-      double count = incrementCount ? featureCounter.incrementCount(featureString) :
-        featureCounter.getCount(featureString);
-      if (count <= unseenThreshold) {
-        // TODO(spenceg): Elaborate?
-        featureString = String.format("%s.%s:UNK%d", featurePrefix, featureType, length);
-      }
-    }
-    return featureString;
+      int length) {
+    return String.format("%s.%s:%s", featurePrefix, featureType, value);
   }
 
   @Override
