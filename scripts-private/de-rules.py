fa8c8c92f9b7cbdf7f4dc04c1837487d2252dbfc
Rob Frederick Voigt Jr <robvoigt@john1.Stanford.EDU>
2013-04-17 17:00:17 -0700

    updated rules for german de-tokenization

--- scripts-private/de-rules.py
+++ scripts-private/de-rules.py
@@ -13,6 +13,15 @@ import codecs
 sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
+
+# load dictionary
+compound_file = codecs.open('/scr/nlp/data/WMT/scripts/compound.dict','r','utf-8')
+compound_dict = {}
+for line in compound_file:
+    pieces = line.split()[0] # comma-separated pieces
+    full = line.split()[1]
+    compound_dict[pieces] = full
+    
 ## TODO: Specific rules
 # recht #kräftig => rechtskräftig
 
@@ -22,6 +31,30 @@ p_all = re.compile(r'(\w) #(\w)', re.U)
 
 for line in sys.stdin:
     # Apply specific rules
+    
+    # catch consecutive pieces broken apart by hashes, replace their span with the full word from the compound_dict
+    split = line.split()
+    pieces = []
+    span = ''
+    for i, word in enumerate(split):
+        if word.startswith('#'):
+            if len(pieces) == 0:
+                pieces.append(split[i-1])
+                span = span + split[i-1] + ' '
+            pieces.append(word[1:])
+            span += word
+        if len(pieces) != 0 and (i == len(split)-1 or not split[i+1].startswith('#')): # reached the end of a set of pieces
+            if ','.join(pieces) in compound_dict:
+                to_replace = re.compile(span, re.U)
+                line = to_replace.sub(compound_dict[','.join(pieces)], line)
+                pieces = []
+                #print >> sys.stderr, 'FOUND ONE'
+            else:
+                #print >> sys.stderr, 'couldnt find ', ','.join(pieces), ' in dict'
+                #print >> sys.stderr, line
+                pieces = []
+
+
     
     # Catch-all rule
     line = p_all.sub(r'\1\2',line)
