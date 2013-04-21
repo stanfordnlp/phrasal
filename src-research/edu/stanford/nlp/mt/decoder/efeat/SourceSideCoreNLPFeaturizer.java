0e20bd637f748846bc14f8bd3c52ac6bcf54d3d9
Spence Green <spence@spencegreen.com>
2013-04-20 22:45:21 -0700

    Re-factored Core featurizer

--- src-research/edu/stanford/nlp/mt/decoder/efeat/SourceSideCoreNLPFeaturizer.java
+++ src-research/edu/stanford/nlp/mt/decoder/efeat/SourceSideCoreNLPFeaturizer.java
@@ -1,7 +1,10 @@
 package edu.stanford.nlp.mt.decoder.efeat;
 
 import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,12 +19,12 @@ import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.util.CoreMap;
-import edu.stanford.nlp.util.Generics;
 import edu.stanford.nlp.util.Index;
 
 import edu.stanford.nlp.mt.base.ConcreteTranslationOption;
 import edu.stanford.nlp.mt.base.FeatureValue;
 import edu.stanford.nlp.mt.base.Featurizable;
+import edu.stanford.nlp.mt.base.IOTools;
 import edu.stanford.nlp.mt.base.IString;
 import edu.stanford.nlp.mt.base.PhraseAlignment;
 import edu.stanford.nlp.mt.base.Sequence;
@@ -42,20 +45,17 @@ import edu.stanford.nlp.mt.decoder.feat.IncrementalFeaturizer;
  * is translated to, if that word is part of the hypothesis, and the
  * feature DEP-reln-targetword is added.
  *
- *@author John Bauer
+ * @author John Bauer
+ * @author Spence Green
+ * 
  */
-public class SourceSideCoreNLPFeaturizer implements IncrementalFeaturizer<IString, String>, AlignmentFeaturizer {
-  
-  /*
-   * The CoreNLP annotation
-   */
-  private final Annotation annotation;
-  
+public class SourceSideCoreNLPFeaturizer implements IncrementalFeaturizer<IString, String>, 
+AlignmentFeaturizer {
+   
   /**
-   * The list of sentences annotations
+   * The list of sentence annotations
    */
-  private final List<CoreMap> sentences;
-  private final int numSentences;
+  private final CoreMap[] sentences;
 
   /**
    * Sentence-specific data structures
@@ -63,46 +63,89 @@ public class SourceSideCoreNLPFeaturizer implements IncrementalFeaturizer<IStrin
   private boolean[] isHead;
   private String[] posTags;
   
+  private final boolean addHeadAlignmentFeature;
+  private final boolean addContentWordDeletionFeature;
+  
+  private final Set<IString> targetFunctionWordList; 
   /**
    * All tag features will start with this prefix
    */
-  public static final String TAG_FEATURE_NAME = "Syn-";
-
-  /**
-   * All dep features will start with this prefix
-   */
-  public static final String DEP_FEATURE_NAME = "DEP-";
+  public static final String FEATURE_PREFIX = "SRC";
 
   public SourceSideCoreNLPFeaturizer(String ... args) {
-    if (args.length == 0) {
-      throw new IllegalArgumentException("Need to provide this Featurizer with a path to a serialized Annotation");
+    if (args.length < 3) {
+      throw new IllegalArgumentException("Required arguments: serialized Annotation, target frequency file, top-k count");
     }
+        
+    // Load CoreNLP annotations
     try {
-      annotation = IOUtils.readObjectFromFile(args[0]);
+      Annotation annotation = IOUtils.readObjectFromFile(args[0]);
+      List<CoreMap> sentenceList = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+      if (sentenceList == null) {
+        throw new RuntimeException("Unusable annotation (no sentences) in " + args[0]);
+      }
+      int numSentences = sentenceList.size();
+      sentences = new CoreMap[numSentences];
+      for (CoreMap annotationSet : sentenceList) {
+        // 1-indexed
+        int id = annotationSet.get(CoreAnnotations.LineNumberAnnotation.class);
+        sentences[id-1] = annotationSet;
+      }
     } catch (ClassNotFoundException e) {
       throw new RuntimeException(e);
     } catch (IOException e) {
       throw new RuntimeIOException(e);
     }
-    sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-    if (sentences == null) {
-      throw new RuntimeException("Unusable annotation (no sentences) in " + args[0]);
+    
+    // Load target frequency annotation
+    String targetFrequencyFile = args[1];
+    int topK = Integer.parseInt(args[2]);
+    addContentWordDeletionFeature = args.length > 3 ? Boolean.parseBoolean(args[3]) : true;
+    addHeadAlignmentFeature = args.length > 4 ? Boolean.parseBoolean(args[4]) : false;
+    targetFunctionWordList = loadList(targetFrequencyFile, topK);
+  }
+
+  /**
+   * Load the target word frequency counts for content word deletion.
+   * 
+   * @param targetFrequencyFile
+   * @param topK
+   * @return
+   */
+  private Set<IString> loadList(String targetFrequencyFile, int topK) {
+    LineNumberReader reader = IOTools.getReaderFromFile(targetFrequencyFile);
+    Set<IString> wordSet = new HashSet<IString>(topK);
+    try {
+      for(String line; (line = reader.readLine()) != null && reader.getLineNumber() < topK;) {
+        String[] toks = line.split("\\s+");
+        assert toks.length == 2;
+        wordSet.add(new IString(toks[0]));
+      }
+      reader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    numSentences = sentences.size();
+    return wordSet;
   }
 
   @Override
   public void reset() {}
 
+  
   /**
-   * Initialize on a new translation.  
+   * Initialize annotations for a new source input. 
    */
   @Override
   public void initialize(int sourceInputId,
-                         List<ConcreteTranslationOption<IString, String>> options, Sequence<IString> foreign, Index<String> featureIndex) {
+                         List<ConcreteTranslationOption<IString, String>> options, 
+                         Sequence<IString> foreign, Index<String> featureIndex) {
     int length = foreign.size();
-    CoreMap currentSentence = sentences.get(sourceInputId);
-    assert length == currentSentence.size();
+    CoreMap currentSentence = sentences[sourceInputId];
+    
+    if(length != currentSentence.size()) {
+      throw new RuntimeException(String.format(
+          "Annotation mismatch at source line %d (%d vs. %d)", sourceInputId, length, currentSentence.size()));
+    }
     
     isHead = new boolean[length];
     // Sanity check
@@ -120,6 +163,51 @@ public class SourceSideCoreNLPFeaturizer implements IncrementalFeaturizer<IStrin
     }
   }
 
+
+  /**
+   * Return a set of features for the tagged sentence.
+   * Each feature will be of the form TAGGER-sourcetag-targetword
+   */  
+  @Override
+  public List<FeatureValue<String>> listFeaturize(Featurizable<IString, String> f) {
+    List<FeatureValue<String>> featureList = new LinkedList<FeatureValue<String>>();
+    PhraseAlignment alignment = f.option.abstractOption.alignment;
+    final int targetPhraseLength = f.targetPhrase.size();
+    final int sourcePhraseLength = f.sourcePhrase.size();
+    
+    List<Set<Integer>> s2tAlignments = new ArrayList<Set<Integer>>(sourcePhraseLength);
+    for (int i = 0; i < sourcePhraseLength; ++i) {
+      s2tAlignments.add(new HashSet<Integer>());
+    }
+    
+    if (addContentWordDeletionFeature) {
+      int numContentDeletions = 0;
+      for (int i = 0; i < targetPhraseLength; ++i) {
+        int[] sourceIndices = alignment.t2s(i);
+        IString targetWord = f.targetPhrase.get(i);
+        for (int j : sourceIndices) {
+          int sourceIndex = f.sourcePosition + j;
+          assert sourceIndex < posTags.length;
+          s2tAlignments.get(j).add(i);
+          ++numContentDeletions;
+          if (targetFunctionWordList.contains(targetWord)) {
+            featureList.add(new FeatureValue<String>(FEATURE_PREFIX + ".fnalign." + posTags[sourceIndex], 1.0));
+            if (isHead[sourceIndex] && addHeadAlignmentFeature) {
+              featureList.add(new FeatureValue<String>(FEATURE_PREFIX + ".headalign." + posTags[sourceIndex], 1.0));  
+            }
+          }
+        }
+      }
+      featureList.add(new FeatureValue<String>(FEATURE_PREFIX + ".fntot", numContentDeletions));
+    }
+    
+    if (addHeadAlignmentFeature) {
+      // TODO Implement DCA per Hwa et al. (2002) ACL paper.
+    }
+    
+    return featureList;
+  }
+  
   /**
    * We care about the features produced by the list of words, so
    * listFeaturize returns results and featurize does not.
@@ -128,139 +216,5 @@ public class SourceSideCoreNLPFeaturizer implements IncrementalFeaturizer<IStrin
   public FeatureValue<String> featurize(Featurizable<IString, String> f) {
     return null;
   }
-  private Set<Integer> warnSet = new HashSet<Integer>();
 
-  private static CoreMap findSentence(List<CoreMap> sentences, int id) {
-    return findSentence(sentences, id, 0, sentences.size() - 1);
-  }
-
-  private static CoreMap findSentence(List<CoreMap> sentences, int id, int start, int end) {
-    if (start > end) {
-      return null;
-    }
-    int midpoint = (start + end) / 2;
-    int currentId = sentences.get(midpoint).get(CoreAnnotations.LineNumberAnnotation.class);
-    if (currentId == id) {
-      return sentences.get(midpoint);
-    } else if (start == end) {
-      return null;
-    } else if (currentId < id) {
-      return findSentence(sentences, id, midpoint + 1, end);
-    } else {
-      return findSentence(sentences, id, start, midpoint - 1);
-    }
-  }
-
-  /**
-   * Return a set of features for the tagged sentence.
-   * Each feature will be of the form TAGGER-sourcetag-targetword
-   */  
-  @Override
-  public List<FeatureValue<String>> listFeaturize(Featurizable<IString, String> f) {
-    int problemId = f.sourceInputId;
-
-    CoreMap currentSentence = findSentence(sentences, problemId+1);
-
-    if (currentSentence == null) {
-      // TODO: now what do we do?  Return null or blow up?
-      System.err.println("WARNING: Given translation problem for sentence that wasn't cached, " + problemId + "; cached " + sentences.size() + " sentences");
-      return null;
-    }
-
-    List<FeatureValue<String>> features = Generics.newArrayList();
-    
-    List<CoreLabel> words = currentSentence.get(CoreAnnotations.TokensAnnotation.class);
-    
-    if (words.size() != f.sourceSentence.size()) {
-       if (!warnSet.contains(problemId)) {
-         System.err.printf("WARNING: tokenization mismatch for %d - Phrasal: <%s> CoreNLP <%s>\n", problemId, f.sourceSentence, words);
-         warnSet.add(problemId);
-       }
-       return null;
-    }
-    /*
-    */
-    PhraseAlignment reverseAlignment = 
-          PhraseAlignment.getPhraseAlignment(
-             f.option.abstractOption.alignment.s2tStr().replace(" ", ";"));
-
-    if (f.option.abstractOption.target.size() != 0) 
-    for (int i : f.option.sourceCoverage) {
-       int phraseI = i - f.sourcePosition;
-//       System.err.printf("phraseI: %d i: %d f.sourcePosition: %d\n", phraseI, i, f.sourcePosition); 
-       int phraseJs[] = reverseAlignment.t2s(phraseI);
-       if (phraseJs == null) {
-         // this word is not aligned to anything yet
-         continue;         
-       }
-       
-       for (int phraseJ : phraseJs) {
-         int j = f.targetPosition + phraseJ;
-         int sourceIndex = i;
-
-         String sourceTag = words.get(sourceIndex).tag();
-         String feature = "";
-         try {
-         String targetWord = f.option.abstractOption.target.get(phraseJ).toString();
-         feature = TAG_FEATURE_NAME + sourceTag + "-" + targetWord;
-         } catch (Exception e) {
-    System.err.printf("phraseJ: %d\n", phraseJ);
-    System.err.printf("phraseI: %d\n", phraseI);
-    System.err.printf("f.source: %s\n", f.option.abstractOption.source);
-    System.err.printf("f.target: %s\n", f.option.abstractOption.target);
-    System.err.printf("f.option.abstractOption: %s\n",  
-      f.option.abstractOption); 
-    System.err.printf("f.option.sourceCoverage: %s\n", f.option.sourceCoverage); 
-    System.err.printf("alignment: %s\n",  f.option.abstractOption.alignment);
-    System.err.printf("reverseAlignment: %s\n",  reverseAlignment);
-    System.err.printf("t-s\n"); 
-    if (f.option.abstractOption.alignment.t2s != null) {
-    for (int[] row : f.option.abstractOption.alignment.t2s) {
-       System.err.printf("\t%s\n", java.util.Arrays.toString(row));
-    }
-    } 
-  
-     System.err.printf("/t-s\n");
-
-    System.err.printf("t-s reverse\n"); 
-    if (reverseAlignment.t2s != null) {
-    for (int[] row : reverseAlignment.t2s) {
-       System.err.printf("\t%s\n", java.util.Arrays.toString(row));
-    }
-    }
-    System.err.printf("/t-s reverse\n");
-            System.exit(-1); 
-         }
-         
-         // no attempt to look for repeated features; 
-         // the system will find and sum those for us
-         features.add(new FeatureValue<String>(feature, 1.0));
-      }
-    }
-
-    SemanticGraph basicDependencies = currentSentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
-    if (f.option.abstractOption.target.size() != 0) 
-    for (SemanticGraphEdge edge : basicDependencies.edgeIterable()) {
-      String relation = edge.getRelation().toString();
-      int sourceIndex = edge.getSource().index() - 1; // IndexedWords are indexed from 1, not 0
-      if (sourceIndex < f.sourcePosition || sourceIndex >= f.sourcePosition + f.sourcePhrase.size()) {
-        continue;
-      }
-
-      if (reverseAlignment.t2s(sourceIndex-f.sourcePosition) == null) {
-        // this word is not aligned to anything yet
-        continue;
-      }
-
-      int[] targetAlignments = reverseAlignment.t2s(sourceIndex-f.sourcePosition);
-      for (int j = 0; j < targetAlignments.length; ++j) {
-        int phraseJ = targetAlignments[j];
-        String targetWord = f.option.abstractOption.target.get(phraseJ).toString();
-        String feature = DEP_FEATURE_NAME + relation + "-" + targetWord;
-        features.add(new FeatureValue<String>(feature, 1.0));
-      }
-    }
-
-    return features;
-  }
 }
