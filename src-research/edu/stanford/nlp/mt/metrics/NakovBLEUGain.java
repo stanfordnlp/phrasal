17136e2cfaf60dfc19df6df47cd2dee3f6fcfeef
Spence Green <spence@spencegreen.com>
2013-04-21 15:35:44 -0700

    Oops. Forgot to add the package. Fix build

---
+++ src-research/edu/stanford/nlp/mt/metrics/NakovBLEUGain.java
@@ -0,0 +1,47 @@
+package edu.stanford.nlp.mt.metrics;
+
+import java.util.List;
+
+import edu.stanford.nlp.mt.base.Sequence;
+
+/**
+ * Extension to Lin and Och (2004) described by Nakov et al. (2012) and evaluated
+ * by Kevin Gimpel in the addendum to his NAACL 2012 conference paper:
+ * 
+ * http://ttic.uchicago.edu/~kgimpel/papers/gimpel+smith.naacl12.addendum.pdf
+ * 
+ * @author Spence Green
+ *
+ * @param <TK>
+ * @param <FV>
+ */
+public class NakovBLEUGain<TK,FV> implements SentenceLevelMetric<TK, FV> {
+
+  private static final int DEFAULT_ORDER = 4;
+
+  private final int order;
+
+  public NakovBLEUGain() {
+    this(DEFAULT_ORDER);
+  }
+
+  public NakovBLEUGain(int order) {
+    this.order = order;
+  }
+
+  @Override
+  public double score(int sourceId, List<Sequence<TK>> references,
+      Sequence<TK> translation) {
+    return BLEUMetric.computeLocalSmoothScore(translation, references, order, true);
+  }
+
+  @Override
+  public void update(int sourceId, List<Sequence<TK>> references,
+      Sequence<TK> translation) {
+  }
+
+  @Override
+  public boolean isThreadsafe() {
+    return true;
+  }
+}
