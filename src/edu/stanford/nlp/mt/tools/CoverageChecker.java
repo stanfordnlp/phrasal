f70662418f37971a12116593ce560f56538e25d3
sonalg <sonalg@stanford.edu>
2013-06-05 14:22:50 -0700

    changed commons.lang-2.5 to commons.lang3-3.1; changed imports and escapehtml instances to escapeHtml4 in core and mt

--- src/edu/stanford/nlp/mt/tools/CoverageChecker.java
+++ src/edu/stanford/nlp/mt/tools/CoverageChecker.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import edu.stanford.nlp.mt.base.LineIndexedCorpus;
 import edu.stanford.nlp.stats.ClassicCounter;
