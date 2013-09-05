135b53e61569ba8becc6d940fedc8d7bd7c8a897
Thang Luong <lmthang@stanford.edu>
2013-09-04 17:39:22 -0700

    MooreLewisCorpusSelection: use selectionSize instead of threshold

--- src/edu/stanford/nlp/mt/tools/MooreLewisCorpusSelection.java
+++ src/edu/stanford/nlp/mt/tools/MooreLewisCorpusSelection.java
@@ -4,6 +4,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 import edu.stanford.nlp.mt.base.IString;
 import edu.stanford.nlp.mt.base.KenLanguageModel;
@@ -14,25 +16,24 @@ import edu.stanford.nlp.mt.base.SimpleSequence;
 /**
  * Moore-Lewis's "Intelligent Selection of Language Model Training Data" (ACL2010).
  * 
- * @author Thang Luong <lmthang@stanford.edu>
+ * @author Thang Luong <lmthang@stanford.edu>, 2013
  *
  */
 public class MooreLewisCorpusSelection {
-  static final int VERBOSE = 1; // Thang Aug13: change from boolean to int, more debugging messages.
   private KenLanguageModel inKenLM;
   private KenLanguageModel outKenLM;
   private LineIndexedCorpus data;
-
+  private double[] crossEntDiffScores; // cross-entropy diff scores
+  
   private IString startToken;
   private IString endToken;
   private int order;
   
   static public void usage() {
     System.err.println("Usage:\n\tjava ...MooreLewisCorpusSelection " +
-        "(inDomainKenLM) (outDomainKenLM) (data) (outPrefix) (thresholdString)");
-    System.err.println("  outPrefix: for each threshold we will output two files outPrefix.threshold (data) "
-        + "and outPrefix.threshold.line (line indices, 0-based)");
-    System.err.println("  thresholdString: multiple thresholds could be specified and separated by -, e.g. 0.1-0.2-0.3");
+        "(selectionSize) (inDomainKenLM) (outDomainKenLM) (data) (outPrefix)");
+    System.err.println("  outPrefix: for each selectSize we will output three files outPrefix.data, "
+        + "outPrefix.score (cross-entropy diff scores), and outPrefix.line (0-based line indices)");
   }
 
   public MooreLewisCorpusSelection(String inDomainKenLMFile, String outDomainKenLMFile, String dataFile){
@@ -45,14 +46,18 @@ public class MooreLewisCorpusSelection {
     outKenLM = new KenLanguageModel(outDomainKenLMFile);
 
     // data file
-    System.err.println("# Opening %s " + dataFile);
+    System.err.println("# Opening " + dataFile);
     try {
       data = new LineIndexedCorpus(dataFile);
     } catch (IOException e) {
       System.err.println("! Can't load data file " + dataFile);
       e.printStackTrace();
     }
+  
+    // cross-entropy diff scores
+    crossEntDiffScores = new double[data.size()];
     
+    // others
     startToken = inKenLM.getStartToken();
     endToken = inKenLM.getEndToken();
     order = inKenLM.order();
@@ -63,55 +68,70 @@ public class MooreLewisCorpusSelection {
     }
   }
 
-  public void select(String outPrefix, double[] thresholds) throws IOException {
-    int numThresholds = thresholds.length;
-    PrintWriter[] dataPWs = new PrintWriter[numThresholds];    // data out
-    PrintWriter[] linePWs = new PrintWriter[numThresholds];    // line out
-    int[] numSelecteds = new int[numThresholds]; 
-    for (int i = 0; i < numThresholds; i++) {
-      dataPWs[i] = new PrintWriter(new OutputStreamWriter(
-          new FileOutputStream(outPrefix + "." + thresholds[i]), "UTF-8"));  
-
-      linePWs[i] = new PrintWriter(new OutputStreamWriter(
-          new FileOutputStream(outPrefix + "." + thresholds[i] + ".line"), "UTF-8"));
-      
-      numSelecteds[i] = 0;
+  // smallest values first
+  class SentenceScoreComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {         
+       return (int)Math.signum(crossEntDiffScores[o1]-crossEntDiffScores[o2]);
+    }      
+  }
+  
+  public double computeCrossEntDiff(String line){
+    // build sequence of IString
+    String[] tokens = line.split("\\s+");
+    IString[] istrings = new IString[tokens.length+2];
+    istrings[0] = startToken; // start token
+    for (int i = 0; i < tokens.length; i++) {
+      istrings[i+1] = new IString(tokens[i]);
     }
-    
+    istrings[istrings.length-1] = endToken; // end token
+    Sequence<IString> sequence = new SimpleSequence<IString>(istrings);
+
+    // compute entropy diff = -1/N*log p_in - (-1/N*log p_out) 
+    int numNgrams = (istrings.length<order)?1 : (istrings.length-order+1); // N
+    return -inKenLM.score(sequence)/numNgrams + outKenLM.score(sequence)/numNgrams;
+  }
+  
+  public void select(String outPrefix, int selectionSize) throws IOException {
+    PriorityQueue<Integer> Q = new PriorityQueue<Integer>(crossEntDiffScores.length, new SentenceScoreComparator());
     int count=0;
     for (String line : data) {
-      // build sequence of IString
-      String[] tokens = line.split("\\s+");
-      IString[] istrings = new IString[tokens.length+2];
-      istrings[0] = startToken; // start token
-      for (int i = 0; i < tokens.length; i++) {
-        istrings[i+1] = new IString(tokens[i]);
-      }
-      istrings[istrings.length-1] = endToken; // end token
-      Sequence<IString> sequence = new SimpleSequence<IString>(istrings);
-
-      // compute entropy diff = -1/N*log p_in - (-1/N*log p_out) 
-      int numNgrams = istrings.length-order+1; // N
-      double entropyDiff = -inKenLM.score(sequence)/numNgrams + outKenLM.score(sequence)/numNgrams; 
-      for (int i = 0; i < numThresholds; i++) {
-        if(entropyDiff<thresholds[i]){
-          numSelecteds[i]++;
-          dataPWs[i].println(line);
-          linePWs[i].println(count); // 0-based index
-        }
-      }
+      crossEntDiffScores[count] = computeCrossEntDiff(line);
+      Q.add(count);
       
       if(++count % 100000 == 0){
         System.err.print(" (" + count/1000 + "K) ");
       }
+    }    
+    System.err.println("Done! Num lines = " + count);
+
+    // init print writers
+    System.err.println("# Output sorted cross-entropy diff scores ...");
+    PrintWriter selectedDataPW = new PrintWriter(new OutputStreamWriter(
+        new FileOutputStream(outPrefix + ".data"), "UTF-8"));  
+    PrintWriter selectedScorePW = new PrintWriter(new OutputStreamWriter(
+        new FileOutputStream(outPrefix + ".score"), "UTF-8"));  
+    PrintWriter selectedLinePW = new PrintWriter(new OutputStreamWriter(
+        new FileOutputStream(outPrefix + ".line"), "UTF-8"));
+
+    // picking up smallest cross-entropy diff values first
+    count = 0;
+    while(!Q.isEmpty()){
+      int lineId = Q.poll();
+      selectedDataPW.println(data.get(lineId));
+      selectedScorePW.println(crossEntDiffScores[lineId]);
+      selectedLinePW.println(lineId); // 0-based index
+      
+      count++;
+      
+      if(count==selectionSize){
+        break;
+      }
     }
     
-    System.err.println("Done! Num lines = " + count);
-    for (int i = 0; i < numThresholds; i++) {
-      dataPWs[i].close();
-      linePWs[i].close();
-      System.err.println("Threshold " + thresholds[i] + ": selected " + numSelecteds[i]);
-    }
+    selectedDataPW.close();
+    selectedScorePW.close();
+    selectedLinePW.close();
   }
   
   public static void main(String[] args) throws IOException {
@@ -123,24 +143,15 @@ public class MooreLewisCorpusSelection {
       System.exit(-1);
     }
 
-    String inDomainKenLMFile = args[0]; // in-domain
-    String outDomainKenLMFile = args[1]; // out-domain
-    String dataFile = args[2];
-    String outPrefix = args[3];
-    String thresholdString = args[4];
+    int selectionSize = Integer.parseInt(args[0]);
+    String inDomainKenLMFile = args[1]; // in-domain
+    String outDomainKenLMFile = args[2]; // out-domain
+    String dataFile = args[3];
+    String outPrefix = args[4];
+    
     MooreLewisCorpusSelection mlcs = new MooreLewisCorpusSelection(inDomainKenLMFile, outDomainKenLMFile, dataFile);
     
-    // get thresholds from a string, e.g. 0.1-0.2-0.3
-    String[] thresholdTokens = thresholdString.split("-");
-    double[] thresholds = new double[thresholdTokens.length];
-    System.err.print("# Selecting with thresholds:");
-    for (int i = 0; i < thresholdTokens.length; i++) {
-      thresholds[i] = Double.parseDouble(thresholdTokens[i]);
-      System.err.print(" " + thresholds[i]);
-    }
-    System.err.println();
-    
     // MooreLewis selection
-    mlcs.select(outPrefix, thresholds);
+    mlcs.select(outPrefix, selectionSize);
   }
 }
