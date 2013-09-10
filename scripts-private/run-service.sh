b6f24e04bb6a0cc8a769594ddf87fd309de12eda
Spence Green <spence@spencegreen.com>
2013-09-10 15:24:48 -0700

    Updates for cluster

--- scripts-private/run-service.sh
+++ scripts-private/run-service.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Local usage
-MEM=9g
-DECODER_OPTS="-Djava.library.path=/home/rayder441/sandbox/javanlp/projects/mt/src-cc"
+#MEM=9g
+#DECODER_OPTS="-Djava.library.path=/home/rayder441/sandbox/javanlp/projects/mt/src-cc"
 
 # NLP cluster
-#MEM=80g
-#DECODER_OPTS="-Djava.library.path=/scr/nlp/data/gale3/KENLM-JNI/${HOST}"
+MEM=80g
+DECODER_OPTS="-Djava.library.path=/scr/nlp/data/gale3/KENLM-JNI/${HOST}"
 
 JAVA_OPTS="-server -ea -Xmx${MEM} -Xms${MEM} -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:PermSize=256m -XX:MaxPermSize=256m"
 
