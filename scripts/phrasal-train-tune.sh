14ba0055f29dadbaa45183ab89e675a5263b8913
Spence Green <spence@spencegreen.com>
2013-09-25 09:16:49 -0700

    Let Phrasal remove OOVs instead of a preprocessing script

--- scripts/phrasal-train-tune.sh
+++ scripts/phrasal-train-tune.sh
@@ -12,13 +12,11 @@ if [ $# -ne 4 ]; then
     echo
     echo Step definitions:
     echo "  1  Extract phrases from dev set"
-    echo "  2  Pre-process the dev set"
-    echo "  3  Run tuning"
-    echo "  4  Extract phrases from test set"
-    echo "  5  Pre-process the test set"
-    echo "  6  Decode test set"
-    echo "  7  Output results file"
-    echo "  8  Generate a learning curve from an online run"
+    echo "  2  Run tuning"
+    echo "  3  Extract phrases from test set"
+    echo "  4  Decode test set"
+    echo "  5  Output results file"
+    echo "  6  Generate a learning curve from an online run"
     exit 0
 fi
 VAR_FILE=$1
@@ -63,16 +61,6 @@ function extract {
     PTABLEDIR=$2
     mkdir -p $PTABLEDIR
     java $JAVA_OPTS $EXTRACTOR_OPTS edu.stanford.nlp.mt.train.PhraseExtract -threads $THREADS_EXTRACT -extractors $EXTRACTORS $EXTRACT_SET -fFilterCorpus $FILTSET $LO_ARGS $OTHER_EXTRACT_OPTS -outputDir $PTABLEDIR 2> ${PTABLEDIR}/extract.gz.log
-}
-
-#
-# Prepare a file for decoding by removing OOVs. Also extract a vocabulary.
-#
-function prep-source {
-    INFILE=$1
-    OUTFILE=$2
-    rm -f $OUTFILE
-    ln -s $INFILE $OUTFILE
 }
 
 function tune-setup {
@@ -218,13 +206,13 @@ function bookmark {
 # Synthetic parameters and commands
 mkdir -p logs
 TUNE_PTABLE_DIR="$TUNE_SET_NAME".tables
-TUNE_FILE="$TUNE_SET_NAME".prep
+TUNE_FILE="$TUNE_SET"
 TUNERUNNAME="$TUNE_SET_NAME"."$SYS_NAME"
 TUNEDIR="$TUNERUNNAME".tune
 TUNE_INI_FILE="$TUNERUNNAME".ini
 
 DECODE_PTABLE_DIR="$DECODE_SET_NAME".tables
-DECODE_FILE="$DECODE_SET_NAME".prep
+DECODE_FILE="$DECODE_SET"
 
 RUNNAME="$DECODE_SET_NAME"."$TUNERUNNAME"
 
@@ -239,25 +227,17 @@ do
     fi
     if [ $step -eq 2 ]; then
 	step-status $step
-	prep-source $TUNE_SET $TUNE_FILE
-    fi
-    if [ $step -eq 3 ]; then
-	step-status $step
 	if [ $TUNE_MODE == "batch" ]; then
 	    tune-batch
 	else
 	    tune-online
 	fi
     fi
-    if [ $step -eq 4 ]; then
+    if [ $step -eq 3 ]; then
 	step-status $step
 	extract $DECODE_SET $DECODE_PTABLE_DIR
     fi
-    if [ $step -eq 5 ]; then
-	step-status $step
-	prep-source $DECODE_SET $DECODE_FILE
-    fi
-    if [ $step -eq 6 ]; then
+    if [ $step -eq 4 ]; then
 	step-status $step
 	if [ $TUNE_MODE == "batch" ]; then
 	    make-ini-from-batch-run
@@ -266,11 +246,11 @@ do
 	fi
 	decode
     fi
-    if [ $step -eq 7 ]; then
+    if [ $step -eq 5 ]; then
 	step-status $step
 	evaluate
     fi
-    if [ $step -eq 8 ]; then
+    if [ $step -eq 6 ]; then
 	step-status $step
 	create-learn-curve
     fi
