6135129cf98968b62f65eab0b80948accbfd807f
Kenneth Heafield <github@kheafield.com>
2013-11-07 17:08:24 -0800

    Path was wrong

--- src-cc/compile_JNI.sh
+++ src-cc/compile_JNI.sh
@@ -3,7 +3,7 @@
 export CXXFLAGS="$CXXFLAGS -O3 -fPIC -DHAVE_ZLIB -lz"
 
 if [ ${#NPLM} != 0 ]; then
-  extra_flags="kenlm/wrappers/*.o -DHAVE_NPLM -lneuralLM -L$NPLM/src -I$NPLM/src -fopenmp"
+  extra_flags="kenlm/lm/wrappers/*.o -DHAVE_NPLM -lneuralLM -L$NPLM/src -I$NPLM/src -fopenmp"
 fi
 
 (cd kenlm; ./compile_query_only.sh)
@@ -17,4 +17,3 @@ else
 fi
 
 g++ -I. -O3 -DNDEBUG -DHAVE_ZLIB -DKENLM_MAX_ORDER=6 -fPIC -lrt -I$JAVA_HOME/include -Ikenlm/ -I$JAVA_HOME/include/linux edu_stanford_nlp_mt_lm_KenLanguageModel.cc kenlm/lm/*.o kenlm/util/*.o kenlm/util/double-conversion/*.o -shared -o libPhrasalKenLM$SUFFIX -lz $extra_flags
-
