89a721a30084712016edc7e2ae04d9d3c8252347
Thang Luong <lmthang@stanford.edu>
2013-11-15 02:27:14 -0800

    Done mt/neural/PhraseTableNeuralScores that scores phrase table entries using word embeddings, update Embedding to handle different forms of unknown/start/end tokens, add cosine distance for SimpleMatrix into Utils.

--- src-research/edu/stanford/nlp/mt/neural/PhraseTableNeuralScores.java
+++ src-research/edu/stanford/nlp/mt/neural/PhraseTableNeuralScores.java
@@ -6,7 +6,10 @@ package edu.stanford.nlp.mt.neural;
 import java.io.File;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.io.PrintStream;
 import java.util.List;
+
+import org.ejml.simple.SimpleMatrix;
 
 import edu.stanford.nlp.mt.base.FlatPhraseTable;
 import edu.stanford.nlp.mt.base.IOTools;
@@ -14,36 +17,17 @@ import edu.stanford.nlp.mt.base.IString;
 import edu.stanford.nlp.mt.base.IStrings;
 import edu.stanford.nlp.mt.base.PhraseAlignment;
 import edu.stanford.nlp.mt.base.Sequence;
-import edu.stanford.nlp.mt.decoder.feat.RuleFeaturizer;
+import edu.stanford.nlp.neural.Embedding;
+import edu.stanford.nlp.neural.Utils;
 import edu.stanford.nlp.util.StringUtils;
 
 /**
+ * Score phrase table entries using word embeddings.
+ * 
  * @author Minh-Thang Luong <lmthang@stanford.edu>, created on Nov 11, 2013
  *
  */
-public class PhraseTableNeuralScores<FV> extends FlatPhraseTable<FV> {
-  /**
-   * @param filename
-   * @throws IOException
-   */
-  public PhraseTableNeuralScores(String filename) throws IOException {
-    super(filename);
-    // TODO Auto-generated constructor stub
-  }
-
-
-  /**
-   * @param phraseFeaturizer
-   * @param filename
-   * @throws IOException
-   */
-  public PhraseTableNeuralScores(
-      RuleFeaturizer<IString, FV> phraseFeaturizer,
-      String filename) throws IOException {
-    // default is not to do log rithm on the scores
-    super(phraseFeaturizer, filename, false);
-  }
-  
+public class PhraseTableNeuralScores {
   /**
    * Load the phrase table from file. 
    * 
@@ -52,13 +36,19 @@ public class PhraseTableNeuralScores<FV> extends FlatPhraseTable<FV> {
    * @return
    * @throws IOException
    */
-  protected int init(File f, boolean reverse) throws IOException {
+  public static void score(String inPhraseTableFile, Embedding srcEmbedding, Embedding tgtEmbedding, 
+      String outPhraseTableFile, int option) throws IOException {
     Runtime rt = Runtime.getRuntime();
     long prePhraseTableLoadMemUsed = rt.totalMemory() - rt.freeMemory();
     final long startTime = System.nanoTime();
 
-    LineNumberReader reader = IOTools.getReaderFromFile(f);
+    LineNumberReader reader = IOTools.getReaderFromFile(new File(inPhraseTableFile));
+    PrintStream writer = IOTools.getWriterFromFile(new File(outPhraseTableFile));
     int numScores = -1;
+    int srcEmbeddingSize = srcEmbedding.getEmbeddingSize();
+    int tgtEmbeddingSize = tgtEmbedding.getEmbeddingSize();
+    
+    int count = 0;
     for (String line; (line = reader.readLine()) != null;) {
       List<List<String>> fields = StringUtils.splitFieldsFast(line, FlatPhraseTable.FIELD_DELIM);
       
@@ -68,66 +58,72 @@ public class PhraseTableNeuralScores<FV> extends FlatPhraseTable<FV> {
       Sequence<IString> source = IStrings.toIStringSequence(fields.get(0));
       Sequence<IString> target = IStrings.toIStringSequence(fields.get(1));
 //      String sourceConstellation = fields[2];
-      String targetConstellation = StringUtils.join(fields.get(3));
       List<String> scoreList = fields.get(4);
-      
-      if (reverse) {
-        Sequence<IString> tmp = source;
-        source = target;
-        target = tmp;
-      }
 
       // Ensure that all rules in the phrase table have the same number of scores
       if (numScores < 0) {
         numScores = scoreList.size();
       } else if (numScores != scoreList.size()) {
         throw new RuntimeException(
-            String
-                .format(
+            String.format(
                     "Error (line %d): Each entry must have exactly the same number of translation\n"
                         + "scores per line. Prior entries had %d, while the current entry has %d:",
                     reader.getLineNumber(), numScores, scoreList.size()));
       }
-      float[] scores;
-      try {
-        scores = IOTools.stringListToNumeric(scoreList);
-      } catch (NumberFormatException e) {
-        e.printStackTrace();
-        throw new RuntimeException(String.format("Number format error on line %d",
-            reader.getLineNumber()));
-      }
 
-      if (targetConstellation.equals("")) {
-        addEntry(source, target, null, scores);
-      } else {
-        addEntry(source, target,
-            PhraseAlignment.getPhraseAlignment(targetConstellation), scores);
-      }
+      
+      double score = Double.NaN;
+      if (option==0){ // average word vectors on each side and compute a similarity scores
+        SimpleMatrix srcVector = new SimpleMatrix(srcEmbeddingSize, 1);
+        for(IString src : source){
+          srcVector = srcVector.plus(srcEmbedding.get(src.word()));
+//          System.err.println(src.word() + "\t" + srcEmbedding.get(src.word()).transpose());
+        }
+        
+        SimpleMatrix tgtVector = new SimpleMatrix(tgtEmbeddingSize, 1);
+        for(IString tgt : target){
+          tgtVector = tgtVector.plus(tgtEmbedding.get(tgt.word()));
+//          System.err.println(tgt.word() + "\t" + tgtEmbedding.get(tgt.word()).transpose());
+        }
 
-      if (source.size() > longestSourcePhrase) {
-        longestSourcePhrase = source.size();
+        score = Utils.cosine(srcVector, tgtVector);
+      } else if (option==1) {
+        String targetConstellation = StringUtils.join(fields.get(3));
+        score = 0;
+//        System.err.println(source + "\t" + target + "\t" + targetConstellation + "\t" + scoreList);
+        if (!targetConstellation.equals("")) {
+          PhraseAlignment tgtAlignment = PhraseAlignment.getPhraseAlignment(targetConstellation);
+          for(int i=0; i<target.size(); i++){
+            int[] alignments = tgtAlignment.t2s(i);
+            
+            // sum cosine scores of aligned words
+            if (alignments!=null && alignments.length>0){
+              SimpleMatrix tgtVector = tgtEmbedding.get(target.get(i).word());
+//              System.err.println(target.get(i).word() + "\t" + tgtVector.transpose());
+              for (int alignment : alignments){ // sum cosine(srcVector, tgtVector)
+//                System.err.println(source.get(alignment).word() + "\t" + srcEmbedding.get(source.get(alignment).word()).transpose());
+                score += Utils.cosine(srcEmbedding.get(source.get(alignment).word()), tgtVector);
+              }
+            }
+          }
+        }
       }
-      if (target.size() > longestTargetPhrase) {
-        longestTargetPhrase = target.size();
+      writer.println(line + " " + score);
+      
+      count++;
+      if(count % 100000 == 0){
+        System.err.print(" (" + count/1000 + "K) ");
       }
-
-      System.err.println(source + "\n" + target + "\n" + targetConstellation + "\n" + scoreList);
-      break;
     }
+    
     reader.close();
-
+    writer.close();
+    
     // print some status information
     long postPhraseTableLoadMemUsed = rt.totalMemory() - rt.freeMemory();
     double elapsedTime = ((double) System.nanoTime() - startTime) / 1e9;
-    System.err
-        .printf(
-            "Done loading phrase table: %s (mem used: %d MiB time: %.3f s)%n",
-            f.getAbsolutePath(),
-            (postPhraseTableLoadMemUsed - prePhraseTableLoadMemUsed)
-                / (1024 * 1024), elapsedTime);
-    System.err.println("Longest foreign phrase: " + longestSourcePhrase);
-    System.err.printf("Phrase table signature: %d%n", getSignature());
-    return numScores;
+    System.err.printf("Done loading phrase table: num phrase pairs = %d, mem used = %d MiB, time = %.3f s\n",
+            count, (postPhraseTableLoadMemUsed - prePhraseTableLoadMemUsed) / (1024 * 1024), elapsedTime);
   }
   
   
@@ -135,25 +131,35 @@ public class PhraseTableNeuralScores<FV> extends FlatPhraseTable<FV> {
    * @param args
    */
   public static void main(String[] args) throws Exception {
-    // TODO Auto-generated method stub
-    if (args.length != 1) {
-      System.out
-          .println("Usage:\n\tjava ...PhraseTableWithNLMScores (phrasetable file)");
+    if (args.length != 7) {
+      System.err.println("Usage:\n\tjava ...PhraseTableWithNLMScores (inPhraseTableFile) "
+              + "(srcWordFile) (srcVectorFile) (tgtWordFile) (tgtVectorFile) (outPhraseTableFile) (scoreOption)");
+      System.err.println("\t\tscoreOption: 0 sum vectors on each side and compute cosine similarity (default)"
+          + ", 1 -- sum cosine scores of aligned words");
+      
       System.exit(-1);
     }
 
-    String model = args[0];
-    //String phrase = args[1];
+    String inPhraseTableFile = args[0];
+    String srcWordFile = args[1];
+    String srcVectorFile = args[2];
+    String tgtWordFile = args[3];
+    String tgtVectorFile = args[4];
+    String outPhraseTableFile = args[5];
+    int option = Integer.parseInt(args[6]);
+    
+    Embedding srcEmbedding = new Embedding(srcWordFile, srcVectorFile);
+    Embedding tgtEmbedding = new Embedding(tgtWordFile, tgtVectorFile);
+    
     long startTimeMillis = System.currentTimeMillis();
-    System.out.printf("Loading phrase table: %s\n", model);
-    FlatPhraseTable.createIndex(false);
-    PhraseTableNeuralScores<String> ppt = new PhraseTableNeuralScores<String>(null, model);
+    
+    System.err.printf("# Loading phrase table ...\n  phrase table file = %s\n", inPhraseTableFile);
+    PhraseTableNeuralScores.score(inPhraseTableFile, srcEmbedding, tgtEmbedding, outPhraseTableFile, option);
+    
     long totalMemory = Runtime.getRuntime().totalMemory() / (1L << 20);
     long freeMemory = Runtime.getRuntime().freeMemory() / (1L << 20);
     double totalSecs = (System.currentTimeMillis() - startTimeMillis) / 1000.0;
-    System.err.printf(
-        "size = %d, secs = %.3f, totalmem = %dm, freemem = %dm\n",
-        FlatPhraseTable.sourceIndex.size(), totalSecs, totalMemory, freeMemory);
+    System.err.printf("secs = %.3f, totalmem = %dm, freemem = %dm\n", totalSecs, totalMemory, freeMemory);
 
   }
 
