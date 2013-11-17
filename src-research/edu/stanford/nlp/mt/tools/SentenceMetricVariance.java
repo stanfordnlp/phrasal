4717ef27abb0a4039d2c639df08302629d90fc1e
Spence Green <spence@spencegreen.com>
2013-11-16 21:02:12 -0800

    Add fields

--- src-research/edu/stanford/nlp/mt/tools/SentenceMetricVariance.java
+++ src-research/edu/stanford/nlp/mt/tools/SentenceMetricVariance.java
@@ -19,48 +19,70 @@ public class SentenceMetricVariance {
   private static final int ORDER = 4;
   
   public static void main(String[] args) throws IOException {
-    if (args.length < 2) {
-      System.err.printf("Usage: java %s name ref [ref] < translations%n", SentenceMetricVariance.class.getName());
+    if (args.length < 3) {
+      System.err.printf("Usage: java %s name source ref [ref] < translations%n", SentenceMetricVariance.class.getName());
       System.exit(-1);
     }
     
     String name = args[0];
-    String[] refFileNames = new String[args.length-1];
-    System.arraycopy(args, 1, refFileNames, 0, refFileNames.length);
+    String srcFile = args[1];
+    String[] refFileNames = new String[args.length-2];
+    System.arraycopy(args, 2, refFileNames, 0, refFileNames.length);
     List<List<Sequence<IString>>> referencesList = Metrics.readReferences(refFileNames);
-
+    List<Sequence<IString>> sourceList = IStrings.tokenizeFile(srcFile);
+    
     LineNumberReader reader = new LineNumberReader(new InputStreamReader(
         System.in));
+//    LineNumberReader reader = IOTools.getReaderFromFile("mt05.mt06.dense-eb-01.trans");
 
     // Output files
     final int numReferences = refFileNames.length;
     PrintStream shortFile = IOTools.getWriterFromFile(name + ".short.tsv");
-    shortFile.print("srcid");
+    shortFile.print("srcid\tsrclen\ttgtlen");
     for (int i = 0; i < numReferences; ++i) {
-      shortFile.printf("\tref%d", i);
+      shortFile.printf("\tref%d\treflen%d", i, i);
     }
-    shortFile.printf("\trefAll%n");
+    shortFile.printf("\trefAll\trefAllLen%n");
 
     PrintStream longFile = IOTools.getWriterFromFile(name + ".long.tsv");
-    longFile.println("srcid\trefid\tscore");
+    longFile.println("srcid\tsrclen\ttgtlen\trefid\treflen\tscore");
     for (String line; (line = reader.readLine()) != null; ) {
+      final int sourceId = reader.getLineNumber() - 1;
       Sequence<IString> translation = IStrings.tokenize(line);
-      int sourceId = reader.getLineNumber() - 1;
+      final int tgtLen = translation.size();
+      final int srcLen = sourceList.get(sourceId).size();
       List<Sequence<IString>> references = referencesList.get(sourceId);
-      shortFile.print(sourceId);
+      shortFile.printf("%d\t%d\t%d", sourceId, srcLen, tgtLen);
       double[] scoreArray = new double[numReferences];
       for (int i = 0; i < numReferences; ++i) {
+        List<Sequence<IString>> ref = references.subList(i, i+1);
+        assert ref.size() == 1;
+        final int refLength = ref.get(0).size();
         scoreArray[i] = 
-            BLEUMetric.computeLocalSmoothScore(translation, references.subList(i, i+1), ORDER, NAKOV_EXTENSION);
-        shortFile.printf("\t%.4f", scoreArray[i]);
-        longFile.printf("%d\t%d\t%.4f%n", sourceId, i, scoreArray[i]);
+            BLEUMetric.computeLocalSmoothScore(translation, ref, ORDER, NAKOV_EXTENSION);
+        shortFile.printf("\t%.4f\t%d", scoreArray[i], refLength);
+        longFile.printf("%d\t%d\t%d\t%d\t%d\t%.4f%n", sourceId, srcLen, tgtLen, i, refLength, scoreArray[i]);
       }
+      final int allLength = bestMatchLength(references, tgtLen);
       double scoreAllRefs = 
           BLEUMetric.computeLocalSmoothScore(translation, references, ORDER, NAKOV_EXTENSION);
-      shortFile.printf("\t%.4f%n", scoreAllRefs);
-      longFile.printf("%d\tall\t%.4f%n", sourceId, scoreAllRefs);
+      shortFile.printf("\t%.4f\t%d%n", scoreAllRefs, allLength);
+      longFile.printf("%d\t%d\t%d\tall\t%d\t%.4f%n", sourceId, srcLen, tgtLen, allLength, scoreAllRefs);
     }
     shortFile.close();
     longFile.close();
+  }
+  
+  // Copied from BLEUMetric.java
+  private static int bestMatchLength(List<Sequence<IString>> references, int candidateLength) {
+    int best = -1;
+    for (Sequence<IString> reference : references) {
+      int refLength = reference.size();
+      if (best < 0 || Math.abs(candidateLength - best) > Math.abs(candidateLength
+          - refLength)) {
+        best = refLength;
+      }
+    }
+    return best;
   }
 }
