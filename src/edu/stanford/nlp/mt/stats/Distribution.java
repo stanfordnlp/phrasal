e7c485b8f9305ec5f640a8416240831f9cd128b9
Spence Green <spence@spencegreen.com>
2013-12-13 09:43:08 -0800

    Add statistics packages

---
+++ src/edu/stanford/nlp/mt/stats/Distribution.java
@@ -0,0 +1,16 @@
+package edu.stanford.nlp.mt.stats;
+
+/**
+ * Interface for a probability distribution.
+ * 
+ * @author Spence Green
+ *
+ */
+public interface Distribution {
+
+  public double[] draw(double[] params, double[] hyperParams);
+
+  public double mean(double[] params, double[] hyperParams);
+
+  public double probOf(double[] instance);
+}
