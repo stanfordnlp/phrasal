28fe597fe49f08a8e16cb6f49c7d2ef0fbff76d0
Spence Green <spence@spencegreen.com>
2013-12-17 11:10:49 -0800

    Initial commit

---
+++ scripts-private/nist-domains.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# Extract genre information for each segment
+# in a NIST source SGML file.
+#
+# Requires package: beautifulsoup4
+#
+# Author: Spence Green
+#
+import sys
+import codecs
+from bs4 import BeautifulSoup
+import os
+from os.path import basename
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
+args = sys.argv[1:]
+if len(args) != 1:
+    sys.stderr.write('Usage: python %s file%s' % (basename(sys.argv[0]), os.linesep))
+    sys.exit(-1)
+
+soup = BeautifulSoup(codecs.open(args[0],encoding='utf-8'))
+
+n_segments = 0
+genre_dict = {}
+for doc in soup.find_all('doc'):
+    genre = doc['genre']
+    genre_dict[genre] = 1
+    segment_list = doc.find_all('seg')
+    for line in segment_list:
+        n_segments += 1
+        print genre
+
+sys.stderr.write('#segments: %d%s' % (n_segments, os.linesep))
+sys.stderr.write('genres: %s%s' % (','.join(genre_dict.keys()),
+                                   os.linesep))
+
