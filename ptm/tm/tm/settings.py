de15a44ad16cf68f01353b4a747a767a06d64fe7
Jason Chuang <jcchuang@cs.stanford.edu>
2014-01-24 11:20:10 -0800

    Removed PTM's dependencies on DatasetManager.

--- ptm/tm/tm/settings.py
+++ ptm/tm/tm/settings.py
@@ -17,8 +17,8 @@ TEMPLATE_DEBUG = True
 #
 # TODO: Point to server checkout of the UI repo
 #
-UI_DIR = '/home/rayder441/sandbox/translate/'
-#UI_DIR = '/u/jcchuang/workspace/TranslateOnGithub/'
+#UI_DIR = '/home/rayder441/sandbox/translate/'
+UI_DIR = '/u/jcchuang/workspace/TranslateUI_Github/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
@@ -154,7 +154,6 @@ PIPELINE_JS = {
             'js/jquery.js',
             'js/underscore.js',
             'js/backbone.js',
-            'js/DatasetManager.js',
             'js/QueryString.js',
             'js/ExperimentUI.js',
             'js/TranslateServer.js',
