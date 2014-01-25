2eec9a0c6d284263cca9bbd523de781be25b5632
Spence Green <spence@spencegreen.com>
2014-01-25 01:10:45 -0800

    Fix contention in ConcurrentHashIndex

--- src/edu/stanford/nlp/mt/base/IString.java
+++ src/edu/stanford/nlp/mt/base/IString.java
@@ -2,8 +2,8 @@ package edu.stanford.nlp.mt.base;
 
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.objectbank.ObjectBank;
-import edu.stanford.nlp.util.HashIndex;
 import edu.stanford.nlp.util.Index;
+import edu.stanford.nlp.util.concurrent.ConcurrentHashIndex;
 
 import java.io.Serializable;
 import java.io.Writer;
@@ -23,8 +23,7 @@ public class IString implements CharSequence, Serializable, HasIntegerIdentity,
     HasWord, Comparable<IString> {
   private static final long serialVersionUID = 2718L;
 
-  // TODO(spenceg) Not threadsafe. Revert to ConcurrentHashIndex when fixed.
-  public static final Index<String> index = new HashIndex<String>(100000);
+  public static final Index<String> index = new ConcurrentHashIndex<String>(100000);
 
   public final int id;
 
