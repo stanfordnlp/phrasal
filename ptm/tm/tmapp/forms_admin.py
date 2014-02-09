dcb92ccbc58c5de22daf8d2f1317398c1a026d39
Spence Green <spence@spencegreen.com>
2014-02-09 18:02:03 +0100

    Load experiments in one shot

--- ptm/tm/tmapp/forms_admin.py
+++ ptm/tm/tmapp/forms_admin.py
@@ -4,13 +4,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+from tmapp.models import TranslationSession,UserConfiguration,SourceDocument,Language
+
 logger = logging.getLogger(__name__)
 
 class UserCreationForm2(UserCreationForm):
     """
     User authentication model based on a json specification.
     """
-    json_spec = forms.CharField(label='Json user specification',widget=forms.Textarea)
+    json_spec = forms.CharField(label='Json experiment specification',widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
         super(UserCreationForm2, self).__init__(*args, **kwargs)
@@ -26,48 +28,59 @@ class UserCreationForm2(UserCreationForm):
     def clean(self):
         cleaned_data = self.cleaned_data
 
-        # Parse the json spec
-        json_str = cleaned_data['json_spec']
-        d = json.loads(json_str)
-
-        username = d['username']
-        cleaned_data['username'] = username
-        try:
-            User._default_manager.get(username=username)
-            raise forms.ValidationError(
-                self.error_messages['duplicate_username'],
-                code='duplicate_username',
-                )
-        except User.DoesNotExist:
-            # Ok. Username is unique
-            pass
-    
-        password1 = d['password']
-        cleaned_data['password1'] = password1
-        password2 = d['password']
-        cleaned_data['password2'] = password2
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(
-                self.error_messages['password_mismatch'],
-                code='password_mismatch',
-            )
-        
         return cleaned_data
 
     def save(self, commit=True):
+        """
+        Create and configure all Users and TranslationSessions
+        """
         cleaned_data = self.cleaned_data
-        user = super(UserCreationForm, self).save(commit=False)
-        user.username = cleaned_data['username']
-        user.set_password('password1')
-        if commit:
+        spec = json.loads(cleaned_data['json_spec'])
+        user = None
+        for username,spec in spec.iteritems():
+            logger.debug(username)
+            # Create the user object
+            #user = super(UserCreationForm, self).save(commit=False)
+            user = User.objects.create(username=username)
+            user.username = username
+            user.set_password(spec['password'].strip())
             user.save()
 
-            # Created the user. Now configure the user session
-            json_str = cleaned_data['json_spec']
-            d = json.loads(json_str)
+            # Create the user configuration object
+            src = spec['src_lang']
+            tgt = spec['tgt_lang']
+            src_lang = Language.objects.get(code=src)
+            tgt_lang = Language.objects.get(code=tgt)
+            user_configuration = UserConfiguration.objects.create(user=user,source_language=src_lang,
+                                                                  target_language=tgt_lang)
+            user_configuration.save()
             
-            
-            # TODO(spenceg): If we get this far, then we should create the
-            # other user data, including translation sessions and training records
+            # Create and save the translation sessions
+            for i,session_spec in enumerate(spec['sessions']):
+                doc_url = session_spec[0]
+                ui = session_spec[1]
+                try:
+                    src_doc = SourceDocument.objects.get(url=doc_url)
+                except SourceDocument.DoesNotExist:
+                    src_doc = SourceDocument.objects.create(url=doc_url,language=src_lang)
+                    src_doc.save()
+                session = TranslationSession.objects.create(user=user,src_document=src_doc,
+                                                            tgt_language=tgt_lang,interface=ui,
+                                                            order=i)
+                session.save()
+
+            # Create and save the training sessions
+            for i,session_spec in enumerate(spec['training']):
+                doc_url = session_spec[0]
+                ui = session_spec[1]
+                try:
+                    src_doc = SourceDocument.objects.get(url=doc_url)
+                except SourceDocument.DoesNotExist:
+                    src_doc = SourceDocument.objects.create(url=doc_url,language=src_lang)
+                    src_doc.save()
+                session = TranslationSession.objects.create(user=user,src_document=src_doc,
+                                                            tgt_language=tgt_lang,interface=ui,
+                                                            training=True,order=i)
+                session.save()
 
         return user
