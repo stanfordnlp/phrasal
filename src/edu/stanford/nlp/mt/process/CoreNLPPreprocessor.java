1257b28bdb90e37f278f5d901bdf346f90b63d52
Spence Green <spence@spencegreen.com>
2014-02-10 13:55:59 -0800

    Fix preprocessor; remove IString synchronization

--- src/edu/stanford/nlp/mt/process/CoreNLPPreprocessor.java
+++ src/edu/stanford/nlp/mt/process/CoreNLPPreprocessor.java
@@ -1,7 +1,6 @@
 package edu.stanford.nlp.mt.process;
 
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.List;
 
 import edu.stanford.nlp.ling.CoreLabel;
@@ -10,10 +9,10 @@ import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.mt.base.IString;
 import edu.stanford.nlp.mt.base.IStrings;
 import edu.stanford.nlp.mt.base.Sequence;
+import edu.stanford.nlp.mt.base.SimpleSequence;
 import edu.stanford.nlp.mt.train.SymmetricalWordAlignment;
 import edu.stanford.nlp.process.Tokenizer;
 import edu.stanford.nlp.process.TokenizerFactory;
-import edu.stanford.nlp.util.Characters;
 import edu.stanford.nlp.util.Generics;
 
 /**
@@ -40,28 +39,10 @@ public abstract class CoreNLPPreprocessor implements Preprocessor {
     tf.setOptions(options);
   }
   
-  /**
-   * Converts control characters to whitespace. The underlying CoreNLP tokenizers
-   * typically delete these characters. In MT bitexts they usually function as
-   * whitespace.
-   * 
-   * @param input
-   * @return
-   */
-  private String removeControlCharacters(String input) {
-    StringBuilder sb = new StringBuilder();
-    int inputLength = input.length();
-    for (int i = 0; i < inputLength; ++i) {
-      char c = input.charAt(i);
-      sb.append(Characters.isControl(c) ? " " : c);
-    }
-    return sb.toString();
-  }
-  
   @Override
   public Sequence<IString> process(String input) {
-    String uncased = removeControlCharacters(toUncased(input.trim()));
-    Tokenizer<CoreLabel> tokenizer = tf.getTokenizer(new StringReader(uncased));
+    String tokenizerInput = toUncased(input.trim());
+    Tokenizer<CoreLabel> tokenizer = tf.getTokenizer(new StringReader(tokenizerInput));
     List<String> outputStrings = Generics.newLinkedList();
     while (tokenizer.hasNext()) {
       String string = tokenizer.next().get(TextAnnotation.class);
@@ -72,62 +53,40 @@ public abstract class CoreNLPPreprocessor implements Preprocessor {
   
   @Override
   public SymmetricalWordAlignment processAndAlign(String input) {
-    String uncased = removeControlCharacters(toUncased(input.trim()));
-    Tokenizer<CoreLabel> tokenizer = tf.getTokenizer(new StringReader(uncased));
+    input = input.trim();
+
+    // Run through the tokenizer and convert to sequence
+    String tokenizerInput = toUncased(input);
+    Tokenizer<CoreLabel> tokenizer = tf.getTokenizer(new StringReader(tokenizerInput));
     List<CoreLabel> outputTokens = tokenizer.tokenize();
-    
-    // Convert tokenized string to sequence
-    List<String> originalStrings = Generics.newArrayList(outputTokens.size());
-    List<String> outputStrings = Generics.newArrayList(outputTokens.size());
-    for (CoreLabel token : outputTokens) {
-      String outputToken = token.get(TextAnnotation.class);
-      String inputToken = token.get(OriginalTextAnnotation.class);
-      if (inputToken == null) {
-        throw new RuntimeException("Invertible option not set in preprocessor/tokenizer");
-      }
-      
-      // Oooh. This is gross and slow. But some core tokenizers return whitespace
-      // in a CoreLabel. The MT system never does that.
-      List<String> inputFields = Arrays.asList(inputToken.split("\\s+"));
-      List<String> outputFields = Arrays.asList(outputToken.split("\\s+"));
-      if (inputFields.size() == outputFields.size()) {
-        outputStrings.addAll(outputFields);
-        originalStrings.addAll(inputFields);
-      
-      } else {
-        // Skip token
-        System.err.printf("%s: Skipping non-invertible token ||%s|| -> ||%s||%n", this.getClass().getName(),
-            inputToken, outputToken);
-      }
+    IString[] outputSequence = new IString[outputTokens.size()];
+    for (int i = 0; i < outputSequence.length; ++i) {
+      String outputToken = outputTokens.get(i).get(TextAnnotation.class);
+      outputSequence[i] = new IString(outputToken);
     }
     
+    // Whitespace tokenization of input, create alignment
     Sequence<IString> inputSequence = IStrings.tokenize(input);
-    Sequence<IString> outputSequence = IStrings.toIStringSequence(outputStrings);
-    SymmetricalWordAlignment alignment = new SymmetricalWordAlignment(inputSequence, outputSequence);
+    SymmetricalWordAlignment alignment = new SymmetricalWordAlignment(inputSequence, 
+        new SimpleSequence<IString>(true, outputSequence));
     
     // Generate the alignments
-    int j = 0;
-    for (int i = 0; i < inputSequence.size(); ++i) {
-      if (j >= outputTokens.size()) {
-        System.err.printf("%s: WARNING: Non-invertible input: %s%n", this.getClass().getName(), input);
+    StringBuilder inputToken = new StringBuilder();
+    for (int i = 0, j = 0, limit = outputTokens.size(); j < limit; ++j) {
+      CoreLabel tokenizedToken = outputTokens.get(j);
+      String inputTokenPart = toUncased(tokenizedToken.get(OriginalTextAnnotation.class));
+      alignment.addAlign(i, j);
+
+      inputToken.append(inputTokenPart);
+      if (i >= inputSequence.size()) {
+        System.err.println("WARNING: Non-invertible input: " + input);
         break;
       }
-      String uncasedInputToken = toUncased(inputSequence.get(i).toString());
-      List<Integer> targets = Generics.newLinkedList();
-      StringBuilder sb = new StringBuilder();
-      String original = originalStrings.get(j);
-      targets.add(j++);
-      sb.append(original);
-      while ( ! uncasedInputToken.equals(original) && j < outputSequence.size()) {
-        sb.append(originalStrings.get(j));
-        targets.add(j++);
-        original = sb.toString();
-      }
-      for (int targetIndex : targets) {
-        alignment.addAlign(i, targetIndex);
+      if (inputSequence.get(i).toString().equals(inputToken.toString())) {
+        ++i;
+        inputToken = new StringBuilder();
       }
     }
-    
     return alignment;
   }
 
