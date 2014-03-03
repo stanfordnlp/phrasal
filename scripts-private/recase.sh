57e42e0bee438b7d3fd96b9323fd9be13257c699
Spence Green <spence@spencegreen.com>
2014-03-02 18:51:00 -0800

    Update recaser script

--- scripts-private/recase.sh
+++ scripts-private/recase.sh
@@ -1,37 +1,20 @@
 #!/usr/bin/env bash
 #
-# Re-case French and German text. The procedures for converting MT
-# output to "readable" text differs for the two languages.
-#
-# German: 1) re-case 2) detokenize
-#
-# French: 1) re-case, 2) detokenize
+# Run the LM-based recaser.
 #
 # Author: Spence Green
 #
 
 if [ $# -ne 2 ]; then
-	echo Usage: $(basename $0) "[French|German]" file
+	echo Usage: $(basename $0) lm_file text_file
 	exit -1
 fi
 
-lang=$1
+lm_file=$1
 infile=$2
 
-model_path=/scr/nlp/data/WMT/recasers
-
-if [ $RECASE_MODEL ]; then
-  model=srilm:$RECASE_MODEL
-elif [ $lang == "French" ]; then
-	model=srilm:${model_path}/french.hmm.recaser.arpa
-else
-	model=kenlm:${model_path}/german.hmm.recaser.probing
-fi
-
 HOST=`hostname -s`
-JVM_OPTS="-server -Xmx20g -Xms2g -XX:+UseParallelGC"
-JNI_OPTS="-Djava.library.path=/scr/nlp/data/gale3/KENLM-JNI/${HOST}:/scr/nlp/data/gale3/SRILM-JNI/${HOST}"
+JVM_OPTS="-server -ea -Xmx2g -Xms2g -XX:+UseParallelGC -XX:+UseParallelOldGC"
+JNI_OPTS="-Djava.library.path=/scr/nlp/data/gale3/KENLM-JNI/${HOST}"
 
 java $JVM_OPTS $JNI_OPTS edu.stanford.nlp.mt.tools.LanguageModelTrueCaser $model < $infile 
-# 2>/dev/null
-
