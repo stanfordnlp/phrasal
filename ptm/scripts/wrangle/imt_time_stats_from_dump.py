1aa462542026352d581a83616a59376bbba97caf
Spence Green <spence@spencegreen.com>
2014-03-02 18:50:35 -0800

    Updates

--- ptm/scripts/wrangle/imt_time_stats_from_dump.py
+++ ptm/scripts/wrangle/imt_time_stats_from_dump.py
@@ -3,7 +3,7 @@ import sys
 import codecs
 import csv
 import dateutil.parser
-from os.path import basename
+from os.path import basename,split
 from datetime import datetime
 from collections import namedtuple,defaultdict
 from csv_unicode import UnicodeReader
@@ -15,6 +15,8 @@ str2bool = lambda x:True if x == '1' else False
 args = sys.argv[1:]
 dump_file = args[0]
 doc_to_timing = defaultdict(dict)
+session_id = 0
+print 'id\tuser\tgenre\tfile\tui\ttime'
 with open(dump_file) as in_file:
     r = UnicodeReader(in_file, delimiter='|', quoting = csv.QUOTE_NONE)
     for row in map(DumpRow._make, r):
@@ -25,13 +27,9 @@ with open(dump_file) as in_file:
         start = dateutil.parser.parse(row.start_time)
         end = dateutil.parser.parse(row.end_time)
         t_elapsed = end - start
-        doc_name = basename(row.src_doc)
-        doc_to_timing[doc_name][row.interface] = '%s:%s' % (row.username, str(t_elapsed.total_seconds()))
+        doc_name = row.src_doc.replace('/static/data/fren/','')
+        genre,file_name = split(doc_name)
+        print '%d\t%s\t%s\t%s\t%s\t%s' % (session_id, row.username, genre, file_name, row.interface, str(t_elapsed.total_seconds()))
+        session_id += 1
 
-print 'Doc\tInteractive\tPostEdit'
-for src_id in sorted(doc_to_timing.keys()):
-    time_str = ''
-    for ui in sorted(doc_to_timing[src_id].keys()):
-        time_str += '\t%s' % (doc_to_timing[src_id][ui])
-    print '%s%s' % (src_id,time_str)
 
