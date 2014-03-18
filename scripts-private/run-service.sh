8f326f40f5f5cda03f3ad28a0e77e4b59ec99770
Spence Green <spence@spencegreen.com>
2014-03-18 14:03:10 -0700

    Update path on script

--- scripts-private/run-service.sh
+++ scripts-private/run-service.sh
@@ -10,16 +10,14 @@ shift
 
 if [ $mode == "local" ]; then
     MEM=9g
-    DECODER_OPTS="-Djava.library.path=/home/rayder441/sandbox/javanlp/projects/mt/src-cc"
 elif [ $mode == "cluster" ]; then
     MEM=150g
-    HOST=`hostname -s`
-    DECODER_OPTS="-Djava.library.path=/scr/nlp/data/gale3/KENLM-JNI/${HOST}"
 else
     echo Usage: `basename $0` "[local|cluster]" options
     exit -1
 fi
 
+DECODER_OPTS="-Djava.library.path=${JAVANLP_HOME}/projects/mt/src-cc"
 JAVA_OPTS="-server -ea -Xmx${MEM} -Xms${MEM} -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:PermSize=256m -XX:MaxPermSize=256m"
 
 java $JAVA_OPTS $DECODER_OPTS edu.stanford.nlp.mt.service.PhrasalService $*
