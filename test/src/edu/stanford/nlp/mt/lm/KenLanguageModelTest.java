c87fae47d8465183321a22372447a099e70fd36b
Spence Green <spence@spencegreen.com>
2014-03-17 19:23:29 -0700

    Add KenLM ARPA test

--- test/src/edu/stanford/nlp/mt/lm/KenLanguageModelTest.java
+++ test/src/edu/stanford/nlp/mt/lm/KenLanguageModelTest.java
@@ -11,28 +11,42 @@ import edu.stanford.nlp.mt.base.Sequence;
 import edu.stanford.nlp.mt.lm.LanguageModelFactory;
 
 /**
- * Simple unit test for the KenLM loader.
+ * Simple unit test for the KenLM loader. Tests both
+ * the ARPA and binary loaders.
  * 
  * @author Spence Green
  */
 public class KenLanguageModelTest extends TestCase {
 
-  private static KenLanguageModel lm;
-  static {
+  private static double scoreTestSentence(KenLanguageModel lm) {
+    String sent = "This is a test sentence to be scored by the language model";
+    Sequence<IString> seq = IStrings.tokenize(sent.toLowerCase());
+    Sequence<IString> paddedSequence = 
+        new InsertedStartEndToken<IString>(seq, lm.getStartToken(), lm.getEndToken());
+    return lm.score(paddedSequence, 1, null).getScore();
+  }
+
+  public void testBinarized() {
+    KenLanguageModel lm;
     try {
       lm = (KenLanguageModel) LanguageModelFactory
           .load("kenlm:projects/mt/test/inputs/3gm-probing.bin");
     } catch (IOException e) {
       throw new RuntimeException(e);
-    }
+    }  
+    double score = scoreTestSentence(lm);
+    assertTrue(score == (double) -72.4647216796875);
   }
 
-  public void testScore() {
-    String sent = "This is a test sentence to be scored by the language model";
-    Sequence<IString> seq = IStrings.tokenize(sent.toLowerCase());
-    Sequence<IString> paddedSequence = 
-        new InsertedStartEndToken<IString>(seq, lm.getStartToken(), lm.getEndToken());
-    double score = lm.score(paddedSequence, 1, null).getScore();
+  public void testARPA() {
+    KenLanguageModel lm;
+    try {
+      lm = (KenLanguageModel) LanguageModelFactory
+          .load("kenlm:projects/mt/test/inputs/3gm-probing.arpa.gz");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }  
+    double score = scoreTestSentence(lm);
     assertTrue(score == (double) -72.4647216796875);
   }
 }
