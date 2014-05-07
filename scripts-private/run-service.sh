da585bfd6c64c347e85e8bb35544be19b695bf7a
Spence Green <spence@spencegreen.com>
2014-05-06 21:40:09 -0700

    Update for new location of KenLM

--- scripts-private/run-service.sh
+++ scripts-private/run-service.sh
@@ -8,7 +8,7 @@ fi
 MEM=$1
 shift
 
-DECODER_OPTS="-Djava.library.path=${JAVANLP_HOME}/projects/mt/src-cc"
+DECODER_OPTS="-Djava.library.path=${JAVANLP_HOME}/projects/more/src-cc"
 JAVA_OPTS="-server -ea -Xmx${MEM} -Xms${MEM} -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:PermSize=256m -XX:MaxPermSize=256m"
 
 java $JAVA_OPTS $DECODER_OPTS edu.stanford.nlp.mt.service.PhrasalService $*
