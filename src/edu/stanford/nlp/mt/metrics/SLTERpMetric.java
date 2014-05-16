2220ca92d395a7efa719628f9e9d49eaaa4e5ca2
Spence Green <spence@spencegreen.com>
2014-05-16 12:44:43 -0700

    Correct TER for multiple references

--- src/edu/stanford/nlp/mt/metrics/SLTERpMetric.java
+++ src/edu/stanford/nlp/mt/metrics/SLTERpMetric.java
@@ -53,31 +53,33 @@ public class SLTERpMetric<TK,FV> implements SentenceLevelMetric<TK, FV> {
     
     // uniq references to prevent (expensive) redundant calculation.
     Set<Sequence<TK>> uniqRefs = new HashSet<Sequence<TK>>(references);
-    
+
+    /**
+     * This implements TER with length scaling per Chiang's standard
+     * transformation of BLEU (see <code>BLEUGain</code>). We also follow
+     * Snover et al. (2009) recommendation (p. 3) for how to compute TER with multiple
+     * references, namely to select the reference for which the least number of
+     * absolute edits is required. Combining these two recommendations amounts to
+     * simply ignoring the denominator for the TER calculation.
+     */
     final String hyp = translation.toString();
     double bestTER = Double.POSITIVE_INFINITY;
-    int lengthScale = Integer.MAX_VALUE;
     for (Sequence<TK> refSeq : uniqRefs) {
-      double ter;
-      if (refSeq.size() == 0) {
-        ter = translation.size();
-      } else {
-        String ref = refSeq.toString();
-        TERalignment align = terCalc.TER(hyp, ref);
-        ter = align.numEdits / align.numWords;
-        if (VERBOSE) {
-          System.err.printf("ref: %s%n", ref);
-          System.err.printf("numEdits: %f%n", align.numEdits);
-          System.err.printf("numWords: %f%n", align.numWords);
-        }        
-      }
+      String ref = refSeq.toString();
+      TERalignment align = terCalc.TER(hyp, ref);
+      //        ter = align.numEdits / align.numWords;
+      double ter = align.numEdits;
       if (ter < bestTER) {
         bestTER = ter;
-        lengthScale = refSeq.size() == 0 ? 1 : refSeq.size();
       }
+      if (VERBOSE) {
+        System.err.printf("ref: %s%n", ref);
+        System.err.printf("numEdits: %f%n", align.numEdits);
+        System.err.printf("numWords: %f%n", align.numWords);
+      }        
     }
     
-    return -bestTER*lengthScale;
+    return -bestTER;
   }
 
   @Override
