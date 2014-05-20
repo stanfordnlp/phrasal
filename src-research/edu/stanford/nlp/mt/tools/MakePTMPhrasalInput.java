ceb0c1eca6c667f71a086f8f1f9e8a99554a9bf2
Spence Green <spence@spencegreen.com>
2014-05-19 17:41:33 -0700

    Update to match output format of db pipeline

--- src-research/edu/stanford/nlp/mt/tools/MakePTMPhrasalInput.java
+++ src-research/edu/stanford/nlp/mt/tools/MakePTMPhrasalInput.java
@@ -83,10 +83,10 @@ public class MakePTMPhrasalInput {
         seenHeader = true;
         continue;
       }
-      String segId = String.format("%s:%s", fields[0], fields[1]).replace(".src.json", ".tgt");
-      String tgtLine = fields[2];
-      String alignStr = fields[4];
-      String srcLine = fields[5];
+//      String segId = String.format("%s:%s", fields[0], fields[1]).replace(".src.json", ".tgt");
+      String tgtLine = fields[3];
+      String alignStr = extend(fields[5]);
+      String srcLine = fields[6];
       SymmetricalWordAlignment s2t = new SymmetricalWordAlignment(srcLine, tgtLine, alignStr);
       SymmetricalWordAlignment s2sPrime = srcPreproc.processAndAlign(srcLine);
       SymmetricalWordAlignment t2tPrime = tgtPreproc.processAndAlign(tgtLine);
@@ -106,8 +106,27 @@ public class MakePTMPhrasalInput {
           }
         }
       }
-      System.out.printf("%s\t%s\t%s\t%s\t%s%n", fields[0], fields[1], t2tPrime.e().toString(), userTextTok, Sentence.listToString(alignmentList));
+      System.out.printf("%s\t%s\t%s\t%s\t%s\t%s%n", fields[0], fields[1], fields[2], t2tPrime.e().toString(), userTextTok, Sentence.listToString(alignmentList));
     }
     reader.close();
+  }
+
+  private static String extend(String aStr) {
+    String[] alignments = aStr.split("\\s+");
+    StringBuilder sb = new StringBuilder();
+    for (String algn : alignments) {
+      if (algn.contains(",")) {
+        String[] fields = algn.split("-");
+        assert fields.length == 2;
+        String[] tIndices = fields[1].split(",");
+        for (String tIdx : tIndices) {
+          sb.append(String.format(" %s-%s", fields[0], tIdx));
+        }
+      } else {
+        if (sb.length() > 0) sb.append(" ");
+        sb.append(algn);
+      }
+    }
+    return sb.toString();
   }
 }
