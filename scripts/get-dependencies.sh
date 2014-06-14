a8a7f24db8f02b6f47505f8324d04737c8411f4b
Spence Green <spence@spencegreen.com>
2014-06-13 22:56:00 -0700

    Clean out lib directory

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -6,15 +6,15 @@
 # Download TERp, since BBN's license 
 # won't let us redistribute it with Phrasal 
 wget http://www.umiacs.umd.edu/~snover/terp/downloads/terp.v1.tgz
-tar -xvf terp.v1.tgz --wildcards --no-anchored '*.jar'
+tar -xzf terp.v1.tgz
 
 mkdir -p lib-nodistrib
 mv terp.v1/dist/lib/*.jar lib-nodistrib
 
+# Download jetty for the web service
+#
 JETTY_VERSION=9.2.1.v20140609
 wget http://download.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
-
-# Download jetty for the web service
 tar -xzf jetty-distribution-$JETTY_VERSION.tar.gz
 mv jetty-distribution-$JETTY_VERSION/lib/jetty-*jar lib-nodistrib
 
