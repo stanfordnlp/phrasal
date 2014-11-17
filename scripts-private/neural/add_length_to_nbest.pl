b5b97c7cb8724af9c36814c367d4784f35236aab
Michael Robert Kayser <mkayser@joni.stanford.edu>
2014-11-17 10:53:38 -0800

    Fix whitespace in add_length_to_nbest.pl

--- scripts-private/neural/add_length_to_nbest.pl
+++ scripts-private/neural/add_length_to_nbest.pl
@@ -6,10 +6,10 @@ use warnings;
 die "Usage: $0 <nbest >nbest_with_len" if scalar(@ARGV !=0);
 
 while(<STDIN>) {
-    if(m/^(.*?\|\|\|)(.+?)(\|\|\|\s+)(.*\s*)/) {
+    if(m/^(.*?\|\|\|)(.+?)(\|\|\|)(.*\s*)/) {
 	my ($left, $text, $middle, $right) = ($1,$2,$3,$4);
 	my $count = &count($text);
-	printf ("%s%s%s len: %d %s", $left, $text, $middle, $count, $right);
+	printf ("%s%s%s len: %d%s", $left, $text, $middle, $count, $right);
     }
     else {
 	die "Bad line: $_";
