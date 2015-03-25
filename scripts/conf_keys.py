cfa9fd03b01f67922bad5bb3fb090fe27c560b9c
Spence Green <spence@spencegreen.com>
2015-03-24 22:30:13 -0700

    Initial commit

---
+++ scripts/conf_keys.py
@@ -0,0 +1,53 @@
+SYSTEM_DIR = 'system-dir'
+
+# Experiment parameters
+EXPERIMENT = 'experiment'
+EXPERIMENT_NAME = 'name'
+
+# TASK: copy data
+TASK_COPY_DATA = 'copy-data'
+
+# Corpus
+CORPUS = 'corpus'
+CORPUS_SRC = 'source'
+CORPUS_TGT = 'target'
+CORPUS_ALIGN = 'align'
+
+# TASK: translation model
+TASK_TM = 'translation-model'
+TM_OUTPUT = 'file-path'
+TM_OPTIONS = 'options'
+
+# TASK: language model
+TASK_LM = 'language-model'
+LM_OUTPUT = 'file-path'
+LM_TYPE = 'type'
+LM_OPTIONS = 'compile-options'
+
+# TASK: decoder config
+TASK_DECODER_CONFIG = 'decoder-config'
+DECODER_OPTIONS = 'ini-options'
+
+# TASK: tune
+TASK_TUNE = 'tune'
+TUNE_SRC = 'source'
+TUNE_REFS = 'refs'
+TUNE_WTS = 'initial-wts'
+TUNE_OPTIONS = 'options'
+
+# TASK: evaluate
+TASK_EVAL = 'evaluate'
+EVAL_SRC = 'source'
+EVAL_METRIC = 'metric'
+EVAL_REFS = 'refs'
+
+# TASK: build repositories
+TASK_BUILD = 'build'
+BUILD_BRANCH = 'branch'
+BUILD_CMD = 'build-cmd'
+
+# TASK: create run script
+TASK_RUNTIME = 'runtime'
+RUNTIME_JVM = 'jvm-opts'
+RUNTIME_ENV = 'environment'
+
