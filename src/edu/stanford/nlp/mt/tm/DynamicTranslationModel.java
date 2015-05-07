02e145bfb286aa6d38e5150f6de52557e667a5cc
Spence Green <spence@spencegreen.com>
2015-05-07 00:03:28 -0700

    Cleanup import list

--- src/edu/stanford/nlp/mt/tm/DynamicTranslationModel.java
+++ src/edu/stanford/nlp/mt/tm/DynamicTranslationModel.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
