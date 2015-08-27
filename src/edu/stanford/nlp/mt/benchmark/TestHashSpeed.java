f1c5b4375f7d6b81c66d507162e9a64d64dd2cb0
Spence Green <spence@spencegreen.com>
2015-08-27 03:00:12 -0700

    Add more benchmarks

--- src/edu/stanford/nlp/mt/benchmark/TestHashSpeed.java
+++ src/edu/stanford/nlp/mt/benchmark/TestHashSpeed.java
@@ -2,20 +2,28 @@ package edu.stanford.nlp.mt.benchmark;
 
 import java.nio.charset.Charset;
 
-import edu.stanford.nlp.mt.util.MurmurHash3;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 
+import edu.stanford.nlp.mt.util.MurmurHash;
+import edu.stanford.nlp.mt.util.MurmurHash3;
+import edu.stanford.nlp.mt.util.TimingUtils;
+import edu.stanford.nlp.mt.util.TimingUtils.TimeKeeper;
 
 /**
- * @author yonik
+ * Adapted from Yonik's speed test.
+ * 
+ * @see {@link https://github.com/JohnLangford/vowpal_wabbit/wiki/murmur2-vs-murmur3}
+ * 
+ * @author Spence Green
+ *
  */
 public class TestHashSpeed {
   static  Charset utf8Charset = Charset.forName("UTF-8");
 
   public static void main(String[] args) throws Exception {
-    int arg = 0;
-    int size = Integer.parseInt(args[arg++]);
-    int iter = Integer.parseInt(args[arg++]);
-    String method = args[arg++];
+    int size = 1000;
+    int iter = 1000000;
 
     byte[] arr = new byte[size];
     for (int i=0; i<arr.length; i++) {
@@ -24,38 +32,59 @@ public class TestHashSpeed {
     String s = new String(arr, "UTF-8");
 
     int ret = 0;
-    long start = System.currentTimeMillis();
 
-    if (method == null || method.equals("murmur32"))  {
-      for (int i = 0; i<iter; i++) {
-        // change offset and len so internal conditionals aren't predictable
-        int offset = ret & 0x03;
-        int len = arr.length - offset - ((ret>>3)&0x03);
-        ret += MurmurHash3.murmurhash3_x86_32(arr, offset, len, i);
-      }
-    } else if (method.equals("slow_string")) {
-      for (int i = 0; i<iter; i++) {
-        // change offset and len so internal conditionals aren't predictable
-        int offset = ret & 0x03;
-        int len = arr.length - offset - ((ret>>3)&0x03);
-        byte[] utf8 = s.getBytes(utf8Charset);
-        ret += MurmurHash3.murmurhash3_x86_32(utf8, offset, len, i);
-      }
-    } else if (method.equals("fast_string")) {
-      for (int i = 0; i<iter; i++) {
-        // change offset and len so internal conditionals aren't predictable
-        int offset = ret & 0x03;
-        int len = arr.length - offset - ((ret>>3)&0x03);
-        ret += MurmurHash3.murmurhash3_x86_32(s, offset, len, i);
-      }
-    } else {
-      throw new RuntimeException("Unknown method " + method);
+    TimeKeeper timer = TimingUtils.start();
+    
+    for (int i = 0; i<iter; i++) {
+      // change offset and len so internal conditionals aren't predictable
+      int offset = ret & 0x03;
+      int len = arr.length - offset - ((ret>>3)&0x03);
+      ret += MurmurHash.hash32(arr, len, i);
     }
+    timer.mark("MurmurHash2");
 
-    long end = System.currentTimeMillis();
+    for (int i = 0; i<iter; i++) {
+      // change offset and len so internal conditionals aren't predictable
+      int offset = ret & 0x03;
+      int len = arr.length - offset - ((ret>>3)&0x03);
+      ret += MurmurHash.hash32(s);
+    }
+    timer.mark("MurmurHash2 string");
+    
+    for (int i = 0; i<iter; i++) {
+      // change offset and len so internal conditionals aren't predictable
+      int offset = ret & 0x03;
+      int len = arr.length - offset - ((ret>>3)&0x03);
+      ret += MurmurHash3.murmurhash3_x86_32(arr, offset, len, i);
+    }
+    timer.mark("MurmurHash3");
 
-    System.out.println("method="+method + " result="+ ret + " throughput=" + 1000 * ((double)size)*iter/(end-start) );
+    HashFunction hf = Hashing.murmur3_32();
+    for (int i = 0; i<iter; i++) {
+      // change offset and len so internal conditionals aren't predictable
+      int offset = ret & 0x03;
+      int len = arr.length - offset - ((ret>>3)&0x03);
+      ret += hf.hashBytes(arr, offset, len).asInt();
+    }
+    timer.mark("MurmurHash3 Google");
+    
+    for (int i = 0; i<iter; i++) {
+      // change offset and len so internal conditionals aren't predictable
+      int offset = ret & 0x03;
+      int len = arr.length - offset - ((ret>>3)&0x03);
+      byte[] utf8 = s.getBytes(utf8Charset);
+      ret += MurmurHash3.murmurhash3_x86_32(utf8, offset, len, i);
+    }
+    timer.mark("MurmurHash3 slow string");
 
+    for (int i = 0; i<iter; i++) {
+      // change offset and len so internal conditionals aren't predictable
+      int offset = ret & 0x03;
+      int len = arr.length - offset - ((ret>>3)&0x03);
+      ret += MurmurHash3.murmurhash3_x86_32(s, offset, len, i);
+    }
+    timer.mark("MurmurHash3 fast string");
+        
+    System.out.println("Timing: " + timer.toString());
   }
-
 }
