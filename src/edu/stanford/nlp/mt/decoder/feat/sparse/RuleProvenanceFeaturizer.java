c2b8462bf164800f972a95c9266d1e4d36124ab0
Joern Wuebker <joern@lilt.com>
2015-11-27 15:43:51 +0100

    added length features to rule provenance featurizer

--- src/edu/stanford/nlp/mt/decoder/feat/sparse/RuleProvenanceFeaturizer.java
+++ src/edu/stanford/nlp/mt/decoder/feat/sparse/RuleProvenanceFeaturizer.java
@@ -1,9 +1,12 @@
 package edu.stanford.nlp.mt.decoder.feat.sparse;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 import edu.stanford.nlp.mt.Phrasal;
+import edu.stanford.nlp.mt.decoder.feat.FeatureUtils;
 import edu.stanford.nlp.mt.decoder.feat.RuleFeaturizer;
 import edu.stanford.nlp.mt.util.FeatureValue;
 import edu.stanford.nlp.mt.util.Featurizable;
@@ -18,15 +21,37 @@ import edu.stanford.nlp.mt.util.IString;
 public class RuleProvenanceFeaturizer implements RuleFeaturizer<IString, String> {
 
   public static final String FEATURE_NAME = "PRV";
+  public static final String SOURCE_WORDS = "srcWrd";  
+  public static final String TARGET_WORDS = "tgtWrd";
 
+  private final boolean wordFeatures;
+  
+  public RuleProvenanceFeaturizer(String...args) {
+    Properties opts = FeatureUtils.argsToProperties(args);
+    wordFeatures = opts.containsKey("wordFeatures");
+  }
+  
+  public RuleProvenanceFeaturizer() {
+    wordFeatures = false;
+  }
+  
+  
   @Override
   public void initialize() {}
 
   @Override
   public List<FeatureValue<String>> ruleFeaturize(Featurizable<IString, String> f) {
-      return f.phraseTableName.equals(Phrasal.TM_BACKGROUND_NAME) ? null :
-        Collections.singletonList(new FeatureValue<>(
-            FEATURE_NAME + ":" + f.phraseTableName, 1.0));
+    if(f.phraseTableName.equals(Phrasal.TM_BACKGROUND_NAME)) return null;
+    
+    List<FeatureValue<String>> features = new ArrayList<>();
+    features.add(new FeatureValue<>(FEATURE_NAME + ":" + f.phraseTableName, 1.0));
+    
+    if(wordFeatures) {
+      features.add(new FeatureValue<>(FEATURE_NAME + ":" + SOURCE_WORDS + ":" + f.phraseTableName, f.sourcePhrase.size()));
+      features.add(new FeatureValue<>(FEATURE_NAME + ":" + TARGET_WORDS + ":" + f.phraseTableName, f.targetPhrase.size()));
+    }
+    
+    return features;
   }
 
   @Override
