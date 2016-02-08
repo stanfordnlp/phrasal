242d35c83d1ef67033741e26be514c9740a00122
Joern Wuebker <joern@lilt.com>
2016-02-08 12:13:00 +0100

    correct KSR computation

--- src/edu/stanford/nlp/mt/util/KSR.java
+++ src/edu/stanford/nlp/mt/util/KSR.java
@@ -15,53 +15,58 @@ public class KSR {
   
   public static KSR getNextPrefix(List<RichTranslation<IString, String>> translations, 
       int nbest_size, Sequence<IString> reference, int previousPrefixSize) {
-    
     int ksrTyped = 0;
     int ksrTotal = 0;
     Sequence<IString> nextPrefix = null;
 
-    Sequence<IString> bestMatch = getBestMatch(translations, nbest_size, reference, previousPrefixSize);
+    Match bestMatch = getBestMatch(translations, nbest_size, reference, previousPrefixSize);
     
-    if(bestMatch == null || bestMatch.size() < previousPrefixSize) {
+    if(bestMatch.fullMatch == null || bestMatch.fullMatch.size() < previousPrefixSize) {
       for(int i = previousPrefixSize; i < reference.size(); ++i) {
         // space after the word: + 1 keystroke
         ksrTotal += reference.get(i).length() + 1;
       }
       ksrTyped = ksrTotal;
-    } 
-    int next = previousPrefixSize;
-    
-    if(bestMatch.size() < previousPrefixSize) {
-      for(int i = previousPrefixSize; i < reference.size(); ++i) {
-        ksrTotal += reference.get(i).length() + 1;
-      }
-      ksrTyped = ksrTotal;
       return new KSR(ksrTyped, ksrTotal, nextPrefix);
-    }
+    } 
     
-    for( ; next < bestMatch.size(); ++next) {
-      // count 1 key stroke for accepting a word
-      ksrTyped += 1;
-      ksrTotal += bestMatch.get(next).length() + 1;
-    }
+    // count 1 key stroke for accepting a character sequence of any length
+    if(bestMatch.fullMatch.size() > previousPrefixSize || bestMatch.prefixMatchSize > 0)
+      ++ksrTyped;
     
+    int next = previousPrefixSize;
+    for( ; next < bestMatch.fullMatch.size(); ++next) {
+      ksrTotal += bestMatch.fullMatch.get(next).length() + 1;
+    }
+
     if(reference.size() > next) {
       // type the next word
       int length = reference.get(next).length() + 1;
-      ksrTyped += length;
+      ksrTyped += length - bestMatch.prefixMatchSize;
       ksrTotal += length;
-      if(reference.size() > next + 1) nextPrefix = bestMatch.concat(reference.subsequence(next, next + 1));// else we're finished
+      if(reference.size() > next + 1) nextPrefix = bestMatch.fullMatch.concat(reference.subsequence(next, next + 1));// else we're finished
     } 
     
     return new KSR(ksrTyped, ksrTotal, nextPrefix);
   }
-
-  private static Sequence<IString> getBestMatch(List<RichTranslation<IString, String>> translations, 
+  
+  private static class Match {
+    public Sequence<IString> fullMatch;
+    public int prefixMatchSize;
+    
+    public Match(Sequence<IString> fullMatch, int prefixMatchSize) {
+      this.fullMatch = fullMatch;
+      this.prefixMatchSize = prefixMatchSize;
+    }
+  }
+  
+  private static Match getBestMatch(List<RichTranslation<IString, String>> translations, 
       int nbest_size, Sequence<IString> reference, int previousPrefixSize) {
     if(translations == null || translations.size() == 0) return null;
       
     Sequence<IString> bestMatch = null;
     int bestMatchSize = -1;
+    int bestPrefixSize = -1;
     int n = 1;
     for(RichTranslation<IString, String> entry : translations) {
       int matchSize = previousPrefixSize;
@@ -71,19 +76,31 @@ public class KSR {
         ++matchSize;
       }
         
+      int prefixSize = 0;
+      if(entry.translation.size() > matchSize) {
+        String hypWord = entry.translation.get(matchSize).toString();
+        String refWord = reference.get(matchSize).toString();
+        while(prefixSize < hypWord.length() && prefixSize < refWord.length() && hypWord.charAt(prefixSize) == refWord.charAt(prefixSize))
+          ++prefixSize;
+      }
+      
       matchSize = Math.min(matchSize, entry.translation.size());
       
       if(matchSize > bestMatchSize) {
         bestMatchSize = matchSize;
         bestMatch = entry.translation.subsequence(0, matchSize);
+        bestPrefixSize = prefixSize;
+      }
+      else if(matchSize == bestMatchSize && prefixSize > bestPrefixSize) {
+        bestPrefixSize = prefixSize;
       }
       
       ++n;
       if(n > nbest_size) break;
     }
     
-    return bestMatch;
+    return new Match(bestMatch, bestPrefixSize);
   }
 
-
 }
+
