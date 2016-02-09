eb339a86a7980d8ee714c2e83b15036fc937e9e0
Joern Wuebker <joern@lilt.com>
2016-02-09 16:05:25 +0100

    create all prefixes

--- scripts/make-prefixes.py
+++ scripts/make-prefixes.py
@@ -18,7 +18,7 @@ import codecs
 import os
 from argparse import ArgumentParser
 
-def make_prefixes(src_file, tgt_file, samples_per_sent):
+def make_prefixes(src_file, tgt_file, samples_per_sent, allPrefixes):
     """
     Make a prefix file deterministically by selecting suffixes.
     """
@@ -28,16 +28,27 @@ def make_prefixes(src_file, tgt_file, samples_per_sent):
         tgt = [x.strip() for x in infile.readlines()]
     with codecs.open(src_file+'.out','w',encoding='utf-8') as srcout, codecs.open(tgt_file+'.out','w',encoding='utf-8') as tgtout, codecs.open(tgt_file+'.prefix','w',encoding='utf-8') as prfout:
         i = 1
-        for j in xrange(0,samples_per_sent):
+        if not allPrefixes:
+            for j in xrange(0,samples_per_sent):
+                for f,e in zip(src,tgt):
+                    e_tok = e.split()
+                    e_len = len(e_tok)
+                    # l := the suffix length
+                    l = i % e_len
+                    srcout.write(f + os.linesep)
+                    tgtout.write(' '.join(e_tok[0:e_len]) + os.linesep)
+                    prfout.write(' '.join(e_tok[0:e_len-l]) + os.linesep)
+                    i += 1
+        else
             for f,e in zip(src,tgt):
                 e_tok = e.split()
                 e_len = len(e_tok)
-                # l := the suffix length
-                l = i % e_len
-                srcout.write(f + os.linesep)
-                tgtout.write(' '.join(e_tok[0:e_len]) + os.linesep)
-                prfout.write(' '.join(e_tok[0:e_len-l]) + os.linesep)
-                i += 1
+                for l in xrange(1, e_len - 1):       
+                    srcout.write(f + os.linesep)
+                    tgtout.write(' '.join(e_tok[0:e_len]) + os.linesep)
+                    prfout.write(' '.join(e_tok[0:e_len-l]) + os.linesep)
+                    i += 1
+
         print '# examples:', i-1
 
 def main():
@@ -55,9 +66,14 @@ def main():
                         type=int,
                         default=1,
                         help='Number of samples per sentence (default: 1).')
+    parser.add_argument('-a','--all-prefixes',
+                        dest='all',
+                        type=bool,
+                        default=false,
+                        help='Create all prefixes. Overides --samples-per-sent.')
     args = parser.parse_args()
     
-    make_prefixes(args.src_file, args.tgt_file, args.nsamples)
+    make_prefixes(args.src_file, args.tgt_file, args.nsamples, args.all)
 
 if __name__ == '__main__':
     main()
