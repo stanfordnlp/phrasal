f5d75c1dcd0d89e2dd3d676c495bbfb0b21dc594
Aditya N Shastry <aditya@lilt.com>
2019-11-21 14:47:41 -0800

    Normalize unicode whitespaces (#4)
    * normalize unicode whitespaces

--- src/edu/stanford/nlp/mt/util/ParallelCorpus.java
+++ src/edu/stanford/nlp/mt/util/ParallelCorpus.java
@@ -109,7 +109,8 @@ public class ParallelCorpus implements Iterable<AlignedSentence>, Serializable {
    * @return
    */
   private int[] stringToArray(String string) {
-    return Arrays.asList(string.trim().split("\\s+")).stream().mapToInt(i -> vocabulary.add(i)).toArray();
+    String normalizedUnicodeWSString = string.replaceAll("[\\s\\p{Z}]+", " ").trim();
+    return Arrays.asList(normalizedUnicodeWSString.split(" ")).stream().mapToInt(i -> vocabulary.add(i)).toArray();
   }
   
   @SuppressWarnings("unchecked")
